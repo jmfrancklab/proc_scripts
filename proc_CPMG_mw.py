912be8efc0c442d8f1529907f9932ad6261987bd
aabeaton <aabeaton@syr.edu>
2020-01-08 12:59:43 -0500

    Functioning code to get enhancement from a CPMG-DNP dataset

--- proc_CPMG_mw.py
+++ proc_CPMG_mw.py
@@ -1,4 +1,208 @@
+from pyspecdata import *
+from scipy.optimize import leastsq,minimize,basinhopping,nnls
+fl = figlist_var()
+for date,id_string in [
+        ('200107','CPMG_DNP_1')
+        ]:
+    filename = date+'_'+id_string+'.h5'
+    nodename = 'signal'
+    s = nddata_hdf5(filename+'/'+nodename,
+            directory = getDATADIR(
+                exp_type = 'test_equip'))
+    SW_kHz = s.get_prop('acq_params')['SW_kHz']
+    nPoints = s.get_prop('acq_params')['nPoints']
+    nEchoes = s.get_prop('acq_params')['nEchoes']
+    nPhaseSteps = s.get_prop('acq_params')['nPhaseSteps']
+    nScans = s.get_prop('acq_params')['nScans']
+    s.set_units('t','s')
+    print ndshape(s)
+    orig_t = s.getaxis('t')
+    p90_s = s.get_prop('acq_params')['p90_us']*1e-6
+    transient_s = s.get_prop('acq_params')['deadtime_us']*1e-6
+    deblank = s.get_prop('acq_params')['deblank_us']*1e-6
+    acq_time_s = orig_t[nPoints]
+    tau_s = s.get_prop('acq_params')['tau_us']*1e-6
+    pad_s = s.get_prop('acq_params')['pad_us']*1e-6
+    tE_s = 2.0*p90_s + transient_s + acq_time_s + pad_s
+    print "ACQUISITION TIME:",acq_time_s,"s"
+    print "TAU DELAY:",tau_s,"s"
+    print "TWICE TAU:",2.0*tau_s,"s"
+    print "ECHO TIME:",tE_s,"s"
+    t2_axis = linspace(0,acq_time_s,nPoints)
+    tE_axis = r_[1:nEchoes+1]*tE_s
+    s.setaxis('t',None)
+    #s.setaxis('nScans',r_[0:nScans])
+    s.chunk('t',['ph1','tE','t2'],[nPhaseSteps,nEchoes,-1])
+    s.setaxis('ph1',r_[0.,2.]/4)
+    s.setaxis('tE',tE_axis)
+    s.setaxis('t2',t2_axis)
+    fl.next(id_string+'raw data - chunking')
+    fl.image(s)
+    s.ft('t2', shift=True)
+    fl.next(id_string+'raw data - chunking ft')
+    fl.image(s)
+    s.ft(['ph1'])
+    s = s['ph1',1].C
+    fl.next(id_string+' image plot coherence-- ft ')
+    fl.image(s)
+    s.ift('t2')
+    fl.next(id_string+' image plot coherence ')
+    fl.image(s)
+    #s.mean('nScans',return_error=False)
+    s.reorder('t2',first=True)
+    print ndshape(s)
+    t2_max = zeros_like(s.getaxis('power'))
+    for x in xrange(len(s.getaxis('power'))):
+        t2_max[x] = abs(s['power',x]['tE',0]).argmax('t2',raw_index=True).data
+    s.setaxis('t2',lambda t: t -s.getaxis('t2')[int(t2_max.mean())])
+    s.rename('tE','nEchoes').setaxis('nEchoes',r_[1:nEchoes+1])
+    print ndshape(s)
+    s.reorder('nEchoes',first=True)
+    print ndshape(s)
+    s.ft('t2')
+    # as of right now, this plot doesn't show anything meaningful
+    fl.next('check center')
+    fl.image(s)
+    f_axis = s.fromaxis('t2')
+    def costfun(p):
+        zeroorder_rad,firstorder = p
+        phshift = exp(-1j*2*pi*f_axis*(firstorder*1e-6))
+        phshift *= exp(-1j*2*pi*zeroorder_rad)
+        corr_test = phshift * s
+        return (abs(corr_test.data.imag)**2)[:].sum()
+    iteration = 0
+    def print_fun(x, f, accepted):
+        global iteration
+        iteration += 1
+        print (iteration, x, f, int(accepted))
+        return
+    sol = basinhopping(costfun, r_[0.,0.],
+            minimizer_kwargs={"method":'L-BFGS-B'},
+            callback=print_fun,
+            stepsize=100.,
+            niter=100,
+            T=1000.
+            )
+    zeroorder_rad, firstorder = sol.x
+    phshift = exp(-1j*2*pi*f_axis*(firstorder*1e-6))
+    phshift *= exp(-1j*2*pi*zeroorder_rad)
+    s *= phshift
+    print "RELATIVE PHASE SHIFT WAS {:0.1f}\us and {:0.1f}$^\circ$".format(
+            firstorder,angle(zeroorder_rad)/pi*180)
+    if s['nEchoes':0]['t2':0]['power',-4].item().real > 0:
+        print s['nEchoes':0]['t2':0]['power',-4].item().real
+        print "Sign correction"
+        s *= -1
+        print s['nEchoes':0]['t2':0]['power',-4].item().real
+    print ndshape(s)
+    s.reorder('power',first=True)
+    fl.next('after phased - real ft')
+    fl.image(s.real)
+    fl.next('after phased - imag ft')
+    fl.image(s.imag)
+    #data = s['t2':0]['nEchoes':0]
+    #fl.next('Fit decay')
+    #x = tE_axis
+    #ydata = data.data.real
+    #ydata /= max(ydata)
+    #fl.plot(x,ydata, '.', alpha=0.4, label='data', human_units=False)
+    #fl.show();quit()
+    enhancement = s['t2':0]['nEchoes':0].C
+    #enhanced = enhancement.data[1:]
+    enhanced = enhancement.data[1:].real
+    enhanced /= max(enhanced)
+    fl.next('enhancement curve')
+    power_axis_dBm = array(s.get_prop('meter_powers'))
+    power_axis_W = zeros_like(power_axis_dBm)
+    power_axis_W[:] = (1e-2*10**((power_axis_dBm[:]+10.)*1e-1))
+    fl.plot(power_axis_W[:-3],enhanced[:-3],'.',human_units=False)
+    #fl.plot(power_axis_W[-3:],enhanced[-3:],'o',human_units=False)
+    xlabel('Power (W)')
+    ylabel('Enhancement')
+fl.show();quit()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# break
+# break
+# break
+# break
+# break
 # coding: utf-8
 
 # 
