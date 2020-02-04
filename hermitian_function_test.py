d0b1407f951f1999f0f1f395a6271907e941a017
John Franck (laptop) <jmfranck@syr.edu>
2020-02-03 22:53:30 -0500

    think I'm part of the way to a solution for phasing inversion recovery curves

--- hermitian_function_test.py
+++ hermitian_function_test.py
@@ -1,7 +1,48 @@
 from pyspecdata import *
+def zeroth_order_ph(d, plot_name=None):
+    r'''determine the covariance of the datapoints
+    in complex plane, and use to phase the
+    zeroth-order even if the data is both negative
+    and positive'''
+    eigenValues, eigenVectors = eig(cov(c_[
+        d.data.real,
+        d.data.imag].T
+        ))
+    # next 3 lines from stackexchange -- sort by
+    # eigenvalue
+    idx = eigenValues.argsort()[::-1]   
+    eigenValues = eigenValues[idx]
+    eigenVectors = eigenVectors[:,idx]
+    # determine the phase angle from direction of the
+    # largest principle axis
+    ph0 = arctan2(eigenVectors[1,0],eigenVectors[0,0])
+    if plot_name:
+        eigenVectors *= (eigenValues.reshape(-1,2)*ones((2,1)))/eigenValues.max()*abs(d.data).max()
+        d_forplot = d.C
+        fl.next(plot_name)
+        fl.plot(
+                d_forplot.data.real,
+                d_forplot.data.imag,
+                '.',
+                alpha=0.25,
+                label='before'
+                )
+        d_forplot /= exp(1j*ph0)
+        fl.plot(
+                d_forplot.data.real,
+                d_forplot.data.imag,
+                '.',
+                alpha=0.25,
+                label='after'
+                )
+        fl.plot(0,0,'ko')
+        fl.plot(eigenVectors[0,0],eigenVectors[1,0],'o',
+                label='first evec')
+        fl.plot(eigenVectors[0,1],eigenVectors[1,1],'o')
+    return exp(1j*ph0)
 def hermitian_function_test(s, down_from_max=0.5):
     # {{{ determine where the "peak" of the echo is,
-    # center on it, and use it to determine the max
+    # and use it to determine the max
     # shift
     data_for_peak = abs(s).mean_all_but(['t2'])
     max_val = data_for_peak.data.max()
@@ -9,15 +50,23 @@ def hermitian_function_test(s, down_from_max=0.5):
             max_val*down_from_max)
     longest_pair = diff(pairs).argmax()
     peak_location = pairs[longest_pair,:]
-    s.setaxis('t2',lambda x: x-peak_location.mean())
+    peak_center = peak_location.mean()
+    s.setaxis('t2',lambda x: x-peak_center)
     s.register_axis({'t2':0})
     max_shift = diff(peak_location).item()/2
     # }}}
+    s_foropt = s.C
+    # {{{ make sure there's an odd number of points
+    s_foropt = s_foropt['t2':(-max_shift,max_shift)]
+    n_points = ndshape(s_foropt)['t2']
+    if n_points % 2 == 0:
+        s_foropt = s_foropt['t2',:-1]
+        n_points -= 1
+    # }}}
     # {{{ construct test arrays for T2 decay and shift
     shift_t = nddata(r_[-1:1:200j]*max_shift, 'shift')
-    t2_decay = exp(-s.fromaxis('t2')*nddata(r_[0:1e3:200j],'R2'))
+    t2_decay = exp(-s_foropt.fromaxis('t2')*nddata(r_[0:1e3:200j],'R2'))
     # }}}
-    s_foropt = s.C
     # {{{ time shift and correct for T2 decay
     s_foropt.ft('t2')
     s_foropt *= exp(1j*2*pi*shift_t*
@@ -25,19 +74,22 @@ def hermitian_function_test(s, down_from_max=0.5):
     s_foropt.ift('t2')
     s_foropt /= t2_decay
     # }}}
-    # {{{ make sure there's and odd number of poings
-    # and set phase of center point to 0
-    s_foropt = s_foropt['t2':(-max_shift,max_shift)]
-    n_points = ndshape(s_foropt)['t2']
-    if n_points % 2 == 0:
-        s_foropt = s_foropt['t2',:-1]
-        n_points -= 1
+    # the problem here is that we need to do this separately for each choice
+    # of decay and shift, but allow zeroth_order_ph to operate over any
+    # additional dimensions, so the following doesn't work correctly right now
+    # {{{ phase the center point to predominantly 0 or pi
     center_point = s_foropt['t2',n_points//2+1]
-    s_foropt /= center_point/abs(center_point)
+    if len(center_point.dimlabels) > 0:
+        assert len(center_point.dimlabels) == 1, repr(ndshape(center_point))+" has too many dimensions"
+        ph0 = zeroth_order_ph(center_point)
+        s_foropt /= ph0
+    else:
+        ph0 = center_point/abs(center_point)
+        s_foropt /= ph0
     # }}}
     residual = abs(s_foropt - s_foropt['t2',::-1].runcopy(conj)).sum('t2')
     residual.reorder('shift')
     minpoint = residual.argmin()
     best_shift = minpoint['shift']
     best_R2 = minpoint['R2']
-    return residual,best_shift, best_R2
+    return residual,ph0,best_shift+peak_center,best_R2
--- proc_IR.py
+++ proc_IR.py
@@ -1,89 +1,110 @@
 from pyspecdata import *
 from scipy.optimize import leastsq,minimize
 from hermitian_function_test import hermitian_function_test
+from sympy import symbols
 fl = figlist_var()
-date = '200131'
-id_string = 'IR_pR_1_2'
-filename = date+'_'+id_string+'.h5'
+t2 = symbols('t2')
+# {{{ input parameters
+date = '200203'
+id_string = 'IR_pR_1_3'
+clock_correction = 1.0829/998.253
 nodename = 'signal'
+filter_bandwidth = 1e3
+# }}}
+filename = date+'_'+id_string+'.h5'
 s = nddata_hdf5(filename+'/'+nodename,
         directory = getDATADIR(exp_type = 'test_equip' ))
-s.rename('t','t2').set_units('t2','s')
+# it doesn't make sense to do any type of FT until the
+# data is in the correct shape --- another reason why
+# chunking should be done before the file is saved
 print(s.getaxis('vd'))
-fl.next('raw data - no clock correction')
-fl.image(s)
-s.ft('t2',shift=True)
-clock_correction = 0 # radians per second
-clock_correction = 1.0829/998.253
-#clock_correction = -0.58434/9.88461 # for IR_1
-#clock_correction = -4.33/9.72
-s *= exp(-1j*s.fromaxis('vd')*clock_correction)
+s.chunk('t',['ph2','ph1','t2'],[4,2,-1]).set_units('t2','s')
+s.setaxis('ph1',r_[0.:4.:2.]/4)
+s.setaxis('ph2',r_[0.:4.]/4)
+s.reorder(['ph1','ph2'])
+fl.next('raw data')
+s.ft('t2', shift=True)
+s = s['t2':(-filter_bandwidth/2,filter_bandwidth/2)]
+fl.image(s,interpolation='bicubic')
+fl.next('filtered data')
 s.ift('t2')
-fl.next('raw data - clock correction')
-fl.image(s)
-nPoints = 1024*2
-t2_axis = s.getaxis('t2')
-s.setaxis('t2',None)
-s.chunk('t2',['ph2','ph1','t2'],[4,2,-1])
-s.setaxis('t2',t2_axis[nPoints])
-s.setaxis('ph1',r_[0.,2.]/4)
-s.setaxis('ph2',r_[0.,1.,2.,3.]/4)
-print(ndshape(s))
-fl.next('image')
-fl.image(s)
-s.ft(['ph2','ph1'])
+rough_center = abs(s).convolve('t2',0.01).mean_all_but('t2').argmax('t2').item()
+s.setaxis(t2-rough_center)
+s = s['t2':(-25e-3,25e-3)] # select only 50 ms in time domain, because it's so noisy
+fl.image(s,interpolation='bicubic')
+fl.next('before clock correction')
 s.ft('t2')
+fl.image(s)
+fl.next('after clock correction')
+s *= exp(-1j*s.fromaxis('vd')*clock_correction)
+fl.image(s)
+## My attempts to align the frequencies here do not work -- we should rather
+## use the method from the YQ PNAS paper?
+#fl.next('align frequencies')
+#center_freqs = abs(s).argmax('t2')
+#s.ift('t2')
+#s *= exp(-1j*2*pi*center_freqs*s.fromaxis('t2'))
+#s.ft('t2')
+#fl.image(s)
 fl.next('image coherence')
-fl.image(s['vd',0])
-s = s['ph2',1]['ph1',0].C
-slice_f = (-3e3,3e3)
-s = s['t2':slice_f].C
+s.ft(['ph2','ph1'])
+fl.image(s)
+fl.next('select coherence and phase')
 s.ift('t2')
-residual,best_shift, best_R2 = hermitian_function_test(s)
+residual,ph0,best_shift, best_R2 = hermitian_function_test(s['ph2',1]['ph1',0])
 # {{{ slice out the FID appropriately and phase correct
 # it
 s.ft('t2')
 s *= exp(1j*2*pi*best_shift*s.fromaxis('t2'))
-s.ift('t2')
-ph0 = s['t2':0.0]
-ph0 /= abs(ph0)
 s /= ph0
+s.ift('t2')
 s = s['t2':(0,None)]
 s['t2',0] *= 0.5
+fl.image(s)
+fl.next('select coherence and phase -- frequency domain')
 s.ft('t2')
 # }}}
-min_vd = s.getaxis('vd')[abs(s).sum('t2').argmin('vd',raw_index=True).item()]
-est_T1 = min_vd/log(2)
-for x in range(len(s.getaxis('vd'))):
-    if s.getaxis('vd')[x] < min_vd:
-        s['vd',x] *= -1
-print("Estimated T1 is:",est_T1,"s")
+fl.image(s)
+fl.next('signal vs. vd')
+s_sliced = s['ph2',1]['ph1',0]
+s_forerror = s['ph2',r_[0,2,3]]['ph1',1]
+s_forerror.run(lambda x: abs(x)**2).mean_all_but(['vd']).run(sqrt)
+s_sliced.mean('t2').set_error(s_forerror.data)
+fl.plot(s_sliced,'o')
+fl.plot(s_sliced.imag,'o')
+#min_vd = s.getaxis('vd')[abs(s).sum('t2').argmin('vd',raw_index=True).item()]
+#est_T1 = min_vd/log(2)
+#for x in range(len(s.getaxis('vd'))):
+#    if s.getaxis('vd')[x] < min_vd:
+#        s['vd',x] *= -1
+#print("Estimated T1 is:",est_T1,"s")
 fl.next('Spectrum - freq domain')
-fl.plot(s.reorder('vd',first=False), alpha=0.5)#, label='%s'%label_str)
-fl.next('Spectrum - waterfall')
-s_sliced.waterfall()
-s_sliced.sum('t2')
-s_sliced /= max(abs(s_sliced.data))
-fl.next('sum along t2')
-fl.plot(s.real,'o-')
-fl.plot(s.imag,'o-')
-# perform fit for T1
-fl.next('T1 fit')
-x_data = s.getaxis('vd')
-y_data = s.data.real
-y_data /= max(abs(y_data))
-fl.plot(x_data,y_data,'o-',human_units=False)
-fitfunc = lambda p,x: p[0]*(1-2*exp(-x_data*p[1]))
-errfunc = lambda p_arg,x_arg,y_arg: fitfunc(p_arg,x_arg) - y_arg
-p_ini = [1.0,10.0]
-p1,success = leastsq(errfunc, p_ini[:], args=(x_data,y_data))
-assert success == 1, "Fit did not succeed"
-T1 = 1./p1[1]
-print("T1:",T1,"s")
-fl.show();quit()
-x_fit = linspace(x_data.min(),x_data.max(),5000)
-fl.next('ext')
-fl.plot(x_fit, fitfunc(p1, x_fit),':', label='fit (T1 = %0.2f ms)'%(T1*1e3), human_units=False)
+fl.plot(s['ph2',1]['ph1',0])
+#fl.plot(s.reorder('vd',first=False), alpha=0.5)#, label='%s'%label_str)
+##fl.next('Spectrum - waterfall')
+##s_sliced.waterfall()
+##s_sliced.sum('t2')
+##s_sliced /= max(abs(s_sliced.data))
+#fl.next('sum along t2')
+#fl.plot(s.real,'o-')
+#fl.plot(s.imag,'o-')
+## perform fit for T1
+#fl.next('T1 fit')
+#x_data = s.getaxis('vd')
+#y_data = s.data.real
+#y_data /= max(abs(y_data))
+#fl.plot(x_data,y_data,'o-',human_units=False)
+#fitfunc = lambda p,x: p[0]*(1-2*exp(-x_data*p[1]))
+#errfunc = lambda p_arg,x_arg,y_arg: fitfunc(p_arg,x_arg) - y_arg
+#p_ini = [1.0,10.0]
+#p1,success = leastsq(errfunc, p_ini[:], args=(x_data,y_data))
+#assert success == 1, "Fit did not succeed"
+#T1 = 1./p1[1]
+#print("T1:",T1,"s")
+#fl.show();quit()
+#x_fit = linspace(x_data.min(),x_data.max(),5000)
+#fl.next('ext')
+#fl.plot(x_fit, fitfunc(p1, x_fit),':', label='fit (T1 = %0.2f ms)'%(T1*1e3), human_units=False)
 
 
 
