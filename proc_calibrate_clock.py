e2449c6450a02ffab8c8d52fb629611d0c935837
Alec Beaton (SU Network) <aabeaton@syr.edu>
2020-02-06 20:12:43 -0500

    Trying to process calibrate clock

--- proc_calibrate_clock.py
+++ proc_calibrate_clock.py
@@ -1,8 +1,8 @@
 from pyspecdata import *
 fl = figlist_var()
-date = '191121'
+date = '200206'
 for id_string in [
-        ('calibrate_clock_2'),
+        ('calibrate_clock_1'),
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
@@ -10,22 +10,23 @@ for id_string in [
             directory = getDATADIR(exp_type = 'test_equip' ))
     s.rename('t','t2').set_units('t2','s')
     clock_correction = 0
-    #clock_correction = -4.33/10.0
     fl.next('image raw')
     acq_time = s.getaxis('t2')[-1]
     manual_taxis_zero = acq_time/2.0
     s.setaxis('t2',lambda x: x-manual_taxis_zero)
     s.ft('t2', shift=True)
+    data = s.C
     fl.image(s)
     fl.next('phase error vs vd')
     fl.plot(s.sum('t2').angle, 'o')
     fl.next('phase error, unwrapped vs vd')
     s = s['vd',1:]/s['vd',:-1]
     s = s.angle.name('signal phase').set_units('rad')
-    s.data = s.data.cumsum()
+    s.data = s.C.data.cumsum()
     fl.plot(s,'o')
     print((s.data[-1]-s.data[-2])/(s.getaxis('vd')[-1]-s.getaxis('vd')[-2]))
-    #fl.next('image corrected')
-    #s *= exp(-1j*s.fromaxis('vd')*clock_correction)
-    #fl.image(s)
+    clock_correction = 9.35
+    fl.next('image corrected')
+    data *= exp(1j*data.fromaxis('vd')*clock_correction)
+    fl.image(data)
 fl.show()
