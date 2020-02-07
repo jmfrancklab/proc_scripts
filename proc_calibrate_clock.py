329db1325d5fb9151d72aab61fb50a595d83f60a
Alec Beaton (SU Network) <aabeaton@syr.edu>
2020-02-07 12:10:18 -0500

    Pushing processing of earliest clock calibration dataset (jan 2019)

--- proc_calibrate_clock.py
+++ proc_calibrate_clock.py
@@ -1,8 +1,8 @@
 from pyspecdata import *
 fl = figlist_var()
-date = '200206'
+date = '190103'
 for id_string in [
-        ('calibrate_clock_1'),
+        ('calibrate_clock'),
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
@@ -11,22 +11,13 @@ for id_string in [
     s.rename('t','t2').set_units('t2','s')
     clock_correction = 0
     fl.next('image raw')
-    acq_time = s.getaxis('t2')[-1]
-    manual_taxis_zero = acq_time/2.0
-    s.setaxis('t2',lambda x: x-manual_taxis_zero)
-    s.ft('t2', shift=True)
-    data = s.C
     fl.image(s)
     fl.next('phase error vs vd')
     fl.plot(s.sum('t2').angle, 'o')
     fl.next('phase error, unwrapped vs vd')
     s = s['vd',1:]/s['vd',:-1]
     s = s.angle.name('signal phase').set_units('rad')
-    s.data = s.C.data.cumsum()
+    s.data = s.data.cumsum()
     fl.plot(s,'o')
-    print((s.data[-1]-s.data[-2])/(s.getaxis('vd')[-1]-s.getaxis('vd')[-2]))
-    clock_correction = 9.35
-    fl.next('image corrected')
-    data *= exp(1j*data.fromaxis('vd')*clock_correction)
-    fl.image(data)
+    print (s.data[-1]-s.data[-2])/(s.getaxis('vd')[-1]-s.getaxis('vd')[-2])
 fl.show()
