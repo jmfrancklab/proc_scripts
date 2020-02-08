bde62c764fbcaad568790c87a39d383e58ff91e7
John Franck (laptop) <jmfranck@syr.edu>
2020-02-08 00:41:29 -0500

    change the peak alignment to just use the max of a convolved function

--- align_slice.py
+++ align_slice.py
@@ -8,28 +8,15 @@ def align_and_slice(s, dimension='t2', convwidth=500, threshold=0.05, fl=None):
     s.ift(dimension)
     R = pi*convwidth # Lorentzian FWHM formula -- R = 1/T2, FWHM = R/pi
     s_conv = s.C*exp(-R * abs(s.fromaxis(dimension)))
+    s_conv_save = s.C
     s_conv.ft(dimension)
-    first_time = True
-    total_center_frq = 0
-    j = 0
-    while first_time or any(center_freq.data > 0.5):
-        print("iterating")
-        fl.next("iteration %d"%j)
-        temp = abs(s_conv)
-        fl.image(temp)
-        temp /= temp.C.sum(dimension)
-        temp *= temp.fromaxis(dimension)
-        center_freq = temp.sum(dimension)
-        print("center frequencies:",center_freq)
-        s_conv.ift(dimension)
-        s_conv *= exp(-1j*2*pi*center_freq*s.fromaxis(dimension))
-        s_conv.ft(dimension)
-        total_center_frq += center_freq
-        j += 1
-        if j > 10: break
-    print("total frq:",total_center_frq)
-    s *= exp(-1j*2*pi*total_center_frq*s.fromaxis(dimension))
+    fl.next('check for center')
+    fl.image(abs(s_conv))
+    center_frq = abs(s_conv).argmax(dimension)
+    s *= exp(-1j*2*pi*center_frq*s.fromaxis(dimension))
+    s_conv = s_conv_save * exp(-1j*2*pi*center_frq*s.fromaxis(dimension))
     s.ft(dimension)
+    s_conv.ft(dimension)
     s_conv.mean_all_but([dimension])
     slices = s_conv.contiguous(lambda x:
             abs(x)>threshold*abs(s_conv.data).max())
