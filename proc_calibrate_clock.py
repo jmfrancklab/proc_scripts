33f4b97b760627e7d3e38f13c6dbdc6af91a9fde
Alec Beaton (laptop) <aabeaton@syr.edu>
2020-02-11 17:34:18 -0500

    Pushing successful run of the calibration, rough

--- proc_calibrate_clock.py
+++ proc_calibrate_clock.py
@@ -1,8 +1,9 @@
 from pyspecdata import *
 from align_slice import align_and_slice
+from scipy.optimize import leastsq
 fl = figlist_var()
 for date,id_string in [
-        ('200206','calibrate_clock_1'),
+        ('200210','calibrate_clock_1'),
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
@@ -10,22 +11,37 @@ for date,id_string in [
             directory = getDATADIR(exp_type = 'test_equip' ))
     s.rename('t','t2').set_units('t2','s')
     clock_correction = 0
-    centerpoint = abs(s).mean('vd').argmax('t2').item()
+    #clock_correction = (1.46491/999104e-6)
+    clock_correction = 1.54862309
+    centerpoint = abs(s).mean('nScans').mean('vd').argmax('t2').item()
     s.setaxis('t2', lambda x: x-centerpoint)
     fl.next('image raw -- time domain')
     fl.image(s, interpolation='bicubic')
     s.ft('t2', shift=True)
     s = s['t2':(-5e3,5e3)]
+    s *= exp(-1j*s.fromaxis('vd')*1e-6*clock_correction)
     fl.next('image raw')
     fl.image(s)
     fl.next('image shifted and sliced')
-    s = align_and_slice(s,fl=fl)
+    s = align_and_slice(s,convwidth=0,fl=fl)
+    s.mean('nScans')
     fl.image(s)
+    fl.show();quit()
     fl.next('phase error vs vd')
     fl.plot(s.sum('t2').angle, 'o')
     fl.next('phase error, unwrapped vs vd - sig avg')
     s = s['vd',1:]/s['vd',:-1]
     s = s.angle.name('signal phase').set_units('rad')
     s.data = s.data.cumsum()
-    fl.plot(s,'o')
+    s.setaxis('vd',s.getaxis('vd')*1e-6)
+    fl.plot(s,'o',human_units=False)
+    # begin fit to return clock correction
+    x = s.getaxis('vd')
+    fitfunc = lambda p, x: p[0]*x
+    errfunc = lambda p_arg,x_arg,y_arg: fitfunc(p_arg, x_arg) - y_arg
+    p_ini = [1.0]
+    p1,success = leastsq(errfunc,p_ini[:],args=(x,s.data))
+    x_fit = linspace(x.min(),x.max(),5000)
+    fl.plot(x_fit,fitfunc(p1,x_fit),':',human_units=False)
+    print(p1)
 fl.show()
