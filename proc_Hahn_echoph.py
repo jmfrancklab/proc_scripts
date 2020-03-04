dd2012f5f4c1da376f94bf6c71acaaf3ec6017d8
Alec Beaton (laptop) <aabeaton@syr.edu>
2020-03-04 18:39:06 -0500

    Updating legend

--- proc_Hahn_echoph.py
+++ proc_Hahn_echoph.py
@@ -8,7 +8,8 @@ t2 = symbols('t2')
 filter_bandwidth = 5e3
 color_choice = True
 for date,id_string,label_str,color_str in [
-        ('200113','echo_TEMPOL_1','microwaves off','blue'),
+        ('191007','echo_1','microwaves off','blue'),
+        ('191007','echo_2','microwaves on (2 W)','red'),
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
@@ -34,13 +35,8 @@ for date,id_string,label_str,color_str in [
     s.ift('t2')
     rough_center = abs(s).convolve('t2',0.01).mean_all_but('t2').argmax('t2').item()
     s.setaxis(t2-rough_center)
-    s.mean('nScans')
-    s.ft('t2')
     fl.image(s)
-    #k = s.C*exp(-1j*s.fromaxis('t2')*0.9*2*pi)
-    k = s.C*exp(-1j*0.9*2*pi)
-    k *= exp(-1j*k.fromaxis('t2')*2*pi*0.005)
-    s = k.C
+    s.ft('t2')
     s.ift('t2')
     residual,best_shift = hermitian_function_test(s[
         'ph2',-2]['ph1',1])
@@ -65,21 +61,16 @@ for date,id_string,label_str,color_str in [
         ph0 = ph0/abs(ph0)
     s /= ph0
     fl.next('frequency domain -- after hermitian function test and phasing')
+    s_ = s.C.ft('t2',pad=524)
     s.ft('t2')
     fl.image(s)
-    s.ift('t2')
-    k.ift('t2')
-    s = s['t2':(0,None)]
-    k = k['t2':(0,None)]
-    fl.next('phased - time')
-    fl.plot(s['ph2',-2]['ph1',1])
-    s.ft('t2')
-    k.ft('t2')
-    #s.convolve('t2',7)
-    fl.next('')
-    s.name('')
-    k.rename('t2','Offset').set_units('Offset','Hz')
+    fl.next('phased')
     s.rename('t2','Offset').set_units('Offset','Hz')
-    fl.plot(k['ph2',-2]['ph1',1],label='before phasing',c='k')
-    fl.plot(s['ph2',-2]['ph1',1],label='after phasing',c='r')
+    s_.rename('t2','Offset').set_units('Offset','Hz')
+    if label_str == 'microwaves on (2 W)':
+        s *= -1
+        s_ *= -1
+    fl.plot(s['ph2',-2]['ph1',1],color='%s'%color_str,label='%s'%label_str)
+    fl.next('phased, pad')
+    fl.plot(s_['ph2',-2]['ph1',1],color='%s'%color_str,label='%s'%label_str)
 fl.show();quit()
