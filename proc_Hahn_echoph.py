1bf20c49369e84fc0bfda5b2379251cc22f991a1
Alec Beaton (laptop) <aabeaton@syr.edu>
2020-03-04 16:16:02 -0500

    Data for phase cycling slides (ENC)

--- proc_Hahn_echoph.py
+++ proc_Hahn_echoph.py
@@ -6,17 +6,8 @@ rcParams["savefig.transparent"] = True
 fl = figlist_var()
 t2 = symbols('t2')
 filter_bandwidth = 5e3
-color_choice = False
-for date,id_string,label_str,color_str in [
-        #('191007','echo_1','microwaves off','red',),
-        #('191007','echo_2','microwaves on','red'),
-        #('191031','echo_5_2','microwaves on','red'),
-        #('191031','echo_5_mw_36dBm_2','microwaves on','red'),
-        ('200304','echo_AER_1','microwaves on','red'),
-        ('200304','echo_AER_4','microwaves on','red'),
-        ('200304','echo_AER_2','microwaves on','red'),
-        ('200304','echo_AER_5','microwaves on','red'),
-        #('200304','echo_AER_3','microwaves on','red'),
+for date,id_string,label_str in [
+        ('191007','echo_2','n'),
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
@@ -32,12 +23,20 @@ for date,id_string,label_str,color_str in [
     s.chunk('t',['ph2','ph1','t2'],[2,4,-1])
     s.setaxis('ph2',r_[0.,2.]/4)
     s.setaxis('ph1',r_[0.,1.,2.,3.]/4)
+    s.set_units('t2','s')
     s.reorder('t2',first=False)
-    fl.next('raw data -- coherence channels')
-    s.ft(['ph2','ph1'])
+    fl.next('raw data')
+    s *= exp(-s.getaxis('t2')/10e-3)
     fl.image(s)
+    fl.next('raw data -- coherence pathways')
+    #s = s['power',0].C
+    s.ft(['ph2','ph1'])
+    #s *= exp(-s.getaxis('t2')/10e-3)
+    fl.image(s)
+    fl.show();quit()
     fl.next('filtered + rough centered data')
     s.ft('t2', shift=True)
+    s *= exp(-1j*2*pi*10.3)
     s = s['t2':(-filter_bandwidth/2,filter_bandwidth/2)]
     s.ift('t2')
     rough_center = abs(s).convolve('t2',0.01).mean_all_but('t2').argmax('t2').item()
@@ -53,7 +52,6 @@ for date,id_string,label_str,color_str in [
     print("best shift is",best_shift)
     # {{{ slice out the FID appropriately and phase correct
     # it
-    s.mean('nScans')
     s.ft('t2')
     s *= exp(1j*2*pi*best_shift*s.fromaxis('t2'))
     s.ift('t2')
@@ -70,15 +68,16 @@ for date,id_string,label_str,color_str in [
         ph0 = ph0/abs(ph0)
     s /= ph0
     fl.next('frequency domain -- after hermitian function test and phasing')
-    s.ft('t2')
+    s.ft('t2',pad=512)
     fl.image(s)
     s.ift('t2')
     s = s['t2':(0,None)]
     fl.next('phased - time')
-    fl.plot(s['ph2',-2]['ph1',1])
+    fl.plot(s)
     s.ft('t2')
-    #s.convolve('t2',7)
-    fl.next('')
+    fl.next('phased')
     s.name('')
-    fl.plot(s['ph2',-2]['ph1',1])#['nScans',0])
-fl.show();quit()
+    fl.plot(k['ph2',-2]['ph1',1],c='k')
+    fl.plot(s['ph2',-2]['ph1',1])
+    fl.plot(s.imag['ph2',-2]['ph1',1])
+    fl.show();quit()
