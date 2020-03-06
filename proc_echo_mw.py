e3941cc2692bfdb94c662823102fecf88f19847f
Alec Beaton (laptop) <aabeaton@syr.edu>
2020-03-06 16:39:57 -0500

    Tried processing some of AG data for possible inclusion in ENC

--- proc_echo_mw.py
+++ proc_echo_mw.py
@@ -82,15 +82,18 @@ for date,id_string in [
     fl.plot(s['ph2',-2]['ph1',1])
     s = s['t2':(0,None)]
     s['t2',0] *= 0.5
-    #s *= exp(-s.getaxis('t2')/35e-3)
+    s *= exp(-s.getaxis('t2')/10e-3)
     #s['t2':(35e-3,None)] = 0
     fl.next('FID slice, time domain -- after hermitian function test and phasing')
     fl.plot(s['ph2',-2]['ph1',1])
     fl.next('FID slice, freq domain -- after hermitian function test and phasing')
     s.ft('t2')
     s *= -1
-    #s.convolve('t2',3)
-    fl.plot(s['power',:-3]['ph2',-2]['ph1',1])
+    s *= 
+    #s.convolve('t2',5)
+    fl.plot(s['power',0]['ph2',-2]['ph1',1])
+    fl.plot(s['power',-4]['ph2',-2]['ph1',1])
+    fl.show();quit()
     s = s['ph2',-2]['ph1',1]
     enhancement = s['t2':(-50,50)].C
     #enhancement = s.C
