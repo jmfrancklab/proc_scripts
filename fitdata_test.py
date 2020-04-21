a6ec73c01748c456f8ee24eb6b6af9bf71e8b8a4
John Franck (desktop) <jmfranck@syr.edu>
2020-04-21 01:08:31 -0400

    Jf stopping with debug -- see slack

--- fitdata_test.py
+++ fitdata_test.py
@@ -1,5 +1,4 @@
 from pyspecdata import *
-from nmrfit import *
 
 #{{{ code bit from proc_IR
 from scipy.optimize import leastsq,minimize
@@ -9,19 +8,62 @@ fl = figlist_var()
 t2 = symbols('t2')
 #}}}
 
-def __init__(self,*args,**kwargs):
-    fitdata.__init__(self,*args,**kwargs)
-    self.symbol_list = [r'M(\infty)',r'M(0)',r'T_1']
-    self.starting_guesses = map(double,[r_[1,1,1],r_[0,0,1],r_[-100,100,0.03],r_[0.001,0.001,0.001],r_[1,-1,4.0]])
-    self.guess_lb = r_[-inf,-inf,1e-4]
-    self.guess_ub = r_[+inf,+inf,20.]
-    self.gen_symbols(r'M(t)')
-
-def fitfunc_raw(self,p,x):
-    return p[0]+(p[1]-p[0])*exp(-x/p[2])
-
-def fitfunc_raw_symb(self,p,x):
-    return p[0]+(p[1]-p[0])*sympy.exp(-x/p[2])
+class t1curve(fitdata):
+    def fitfunc_raw(self,p,x):
+        '''just the actual fit function to return the array y as a function of p and x'''
+        return p[0]+(p[1]-p[0])*exp(-x/p[2])
+    def fitfunc_raw_symb(self,p,x):
+        '''if I'm using a named function, I have to define separately in terms of sympy rather than numpy functions'''
+        return p[0]+(p[1]-p[0])*sympy.exp(-x/p[2])
+    def linfunc(self,x,y,xerr = None,yerr = None):
+        '''just the actual fit function to return the pair of arrays x',y' that should be linear
+        it accepts as inputs x and y, and it uses the output from the fit, where necessary
+        also optionally propagates the error based on yerr and xerr, which can be passed in to it
+        For the case of T1, we want to return ln(y-M(\infty)) = ln(M(0)-M(\infty)) - t/T_1
+        '''
+        #print 'DEBUG: y is',y
+        #print 'DEBUG: M(\infty) is',self.output(r'M(\infty)')
+        temp = self.output(r'M(\infty)')-y # the argument for log
+        #print 'DEBUG: temp is',temp
+        # note that there is some error associated with m(\infty) that I'm just taking for granted
+        rety = log(temp)
+        if yerr != None:
+            reterr = yerr/abs(temp)
+        mask = isfinite(rety)
+        retx = x # for instance, in emax, this is not just x
+        xname = self.fit_axis # same as the fit axis
+        yname = r'$ln(M(\infty)-M(t))$'
+        #{{{ this should be pretty standardized
+        retval = nddata(rety,
+                [size(rety),1],
+                [xname,yname])
+        retval.labels([self.fit_axis],
+                [retx.copy()])
+        if yerr != None:
+            retval.set_error(reterr)
+        #}}}
+        return retval
+    def linerror(self,x,y):
+        '''propagate the error for linfunc
+        '''
+        rety = log(y-self.output(r'M(\infty)'))
+        mask = isfinite(rety)
+        x_axis_of_linear_plot = x # for instance, in emax, this is not just x
+        retval = nddata(rety,
+                [size(rety),1],
+                [self.fit_axis,r'$ln(M(t)-M(\infty))$'])
+        retval.labels([self.fit_axis],
+                [x_axis_of_linear_plot.copy()])
+        return retval
+    def __init__(self,*args,**kwargs):
+        '''here, we give the particular latex representation and list of symbols for this particular child class'''
+        fitdata.__init__(self,*args,**kwargs)
+        self.symbol_list = [r'M(\infty)',r'M(0)',r'T_1'] # note that it must notbe possible to find part of one of the later strings by searching for one of the earlier strings
+        self.starting_guesses = map(double,[r_[1,1,1],r_[0,0,1],r_[-100,100,0.03],r_[0.001,0.001,0.001],r_[1,-1,4.0]])
+        self.guess_lb = r_[-inf,-inf,1e-4]
+        self.guess_ub = r_[+inf,+inf,20.]
+        self.gen_symbolic(r'M(t)')
+        return
 
 #{{{ code for import, pre-fit proc
 # {{{ input parameters
