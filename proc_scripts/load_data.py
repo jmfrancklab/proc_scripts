4f950900004fb7f8d7255a8dfaee0a6a2bb47c43
Alex <guinneak198@github.com>
2020-04-27 09:57:59 -0400

    working on loading data

--- proc_scripts/load_data.py
+++ proc_scripts/load_data.py
@@ -1,4 +1,9 @@
 from pyspecdata import *
 
 def load_data(searchstr,freq_range,time_range):
+    filename = date+'_'+id_string+'.h5'
+    nodename = 'signal'
+    s = nddata_hdf5(filename+'/'+nodename,
+            directory = getDATADIR(
+                exp_type = 'test_equip'))
 
