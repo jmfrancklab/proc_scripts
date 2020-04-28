7f94ecfde849f3be172b73e07a27d37683cfd8d2
Alex <guinneak198@github.com>
2020-04-28 15:00:42 -0400

    updated load data for nmr files must be in test_equip

--- proc_scripts/__init__.py
+++ proc_scripts/__init__.py
@@ -2,6 +2,8 @@ from .hermitian_function_test import *
 from .align_slice import *
 from .load_data import *
 from .load_data_capture import *
+from .load_data_nmr import *
 from .side_by_side import *
 from .short_echo_FID import *
-__all__ = ['zeroth_order_ph','hermitian_function_test','align_and_slice','load_data','load_data_cap','side_by_side','fl_mod','FID']
+
+__all__ = ['zeroth_order_ph','hermitian_function_test','align_and_slice','load_data','load_data_cap','load_data_nmr','side_by_side','fl_mod','FID']
--- proc_scripts/load_data_nmr.py
+++ proc_scripts/load_data_nmr.py
@@ -1,5 +1,5 @@
 from pyspecdata import *
-def load_data_nmr(searchstr,exp_no):
+def load_data_nmr(searchstr,expno):
     files = search_filename(searchstr, 'test_equip')
     s = find_file(searchstr, exp_type='test_equip', dimname = 'indirect', expno=expno)
     return s
