ac1d0aab3f860101923c630752434d742bde0068
Alex <guinneak198@github.com>
2020-06-03 13:27:47 -0400

    switching branches

--- proc_scripts/load_data.py
+++ proc_scripts/load_data.py
@@ -3,6 +3,7 @@ from .Utility import dBm2power
 #to use type s = load_data("nameoffile")
 def proc_bruker_deut_IR_withecho_mancyc(s):
     raise RuntimeError("this is where postprocessing would be implemented -- not implemented yet")
+
 def proc_bruker_deut_IR_mancyc(s):
     fl = figlist_var()
     s.chunk('indirect',['indirect','ph1','ph2'],[-1,4,2]) #expands the indirect dimension into indirect, ph1, and ph2. inner most dimension is the inner most in the loop in pulse sequence, is the one on the farthest right. Brackets with numbers are the number of phase cycle steps in each one. the number of steps is unknown in 'indirect' and is therefore -1.
