5fc2316328273e819ef676356c503493eeb2f432
Alex <guinneak198@github.com>
2020-06-14 12:26:38 -0400

    needed for cavanagh fig

--- proc_IR.py
+++ proc_IR.py
@@ -32,6 +32,11 @@ for searchstr,exp_type,nodename, postproc in [
     s.setaxis(t2-rough_center)
     #}}}
     #{{{hermitian function test and apply best shift
+    fl.next('time domain before')
+    fl.image(s)
+    fl.next('frequency domain before')
+    s.ft('t2')
+    fl.image(s)
     best_shift = hermitian_function_test(s[
         'ph2',coh_sel['ph2']]['ph1',coh_sel['ph1']])
     logger.info(strm("best shift is",best_shift))
@@ -41,6 +46,10 @@ for searchstr,exp_type,nodename, postproc in [
     s.ift('t2')
     fl.next('time domain after hermitian test')
     fl.image(s)
+    fl.next('frequency domain after')
+    s.ft('t2')
+    fl.image(s)
+    fl.show();quit()
     #}}}
     #{{{zeroth order phase correction
     ph0 = s['t2':0]['ph2',coh_sel['ph2']]['ph1',coh_sel['ph1']]
