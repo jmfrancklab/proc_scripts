2d3f7b51938c42cb0b459c0b454540a3e0551a9b
Alex <guinneak198@github.com>
2020-06-19 12:40:03 -0400

    proc_nutation updated

--- proc_nutation.py
+++ proc_nutation.py
@@ -7,7 +7,7 @@ fl = fl_mod()
 t2 = symbols('t2')
 logger = init_logging("info")
 for searchstr,exp_type,nodename,postproc in [
-    ['200219_nutation_alex_probe','test_equip','spincore_nutation_v1','spincore_nutation']
+    ['200219_nutation_alex_probe','test_equip','nutation','spincore_nutation_v1']
     ]:
     s = find_file(searchstr,exp_type=exp_type,expno=nodename,postproc=postproc,
             lookup=postproc_dict) 
