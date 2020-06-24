8249461e64545092d4c0573f46ce69866ce82609
Alex <guinneak198@github.com>
2020-06-24 13:49:40 -0400

    wrong branch`:w :Q

--- proc_CPMG.py
+++ proc_CPMG.py
@@ -4,32 +4,35 @@ from proc_scripts import *
 from proc_scripts import postproc_dict
 fl = figlist_var()
 logger = init_logging('info')
-
 for searchstr, exp_type, nodename, postproc, label_str in [
-        #('200221_CPMG_TEMPOLgel_3p0_1','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
-        ('200221_CPMG_TEMPOLgel_2p9_1','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
-        #('200304_CPMG_2p6_1','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
-        #('200305_CPMG_3p5_2','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
-        #('200305_CPMG_3p6_2','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
-        #('200305_CPMG_3p7_2','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
-        #('200305_CPMG_3p7_3','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
-        #('200305_CPMG_3p8_2','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
-        #('200305_CPMG_3p9_2','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
-        #('200305_CPMG_4p0_1','test_equip','signal','spincore_CPMG_v1','deadtime=5'),
+        #('200221','CPMG_TEMPOLgel_3p0_1','deadtime=5'),
+        ('200221_CPMG_TEMPOLgel_2p9_1','test_equip','signal','CPMG','deadtime=5'),
+        #('200304','CPMG_2p6_1','deadtime=5'),
+        #('200305','CPMG_3p5_2','deadtime=5'),
+        #('200305','CPMG_3p6_2','deadtime=5'),
+        #('200305','CPMG_3p7_2','deadtime=5'),
+        #('200305','CPMG_3p7_3','deadtime=5'),
+        #('200305','CPMG_3p8_2','deadtime=5'),
+        #('200305','CPMG_3p9_2','deadtime=5'),
+        #('200305','CPMG_4p0_1','deadtime=5'),
         ]:
-    ###{{{loading in data and displaying raw data
-    s = find_file(searchstr, exp_type=exp_type,
+    s =  find_file(searchstr, exp_type=exp_type,
             expno=nodename, postproc=postproc, lookup=postproc_dict)
-    #}}}
+    nEchoes = s.get_prop('acq_params')['nEchoes']
     #{{{select and display coherence channel centered
-    echo_center = abs(s['tE',0]).argmax('t2').data.item()
+    echo_center = abs(s)['tE',0].argmax('t2').data.item()
     s.setaxis('t2', lambda x: x-echo_center)
+    
+    
     best_shift = hermitian_function_test(s)
     logger.info(strm("best shift is",best_shift))
-    s.setaxis('t2', lambda x: x-best_shift)
+    s.setaxis('t2',lambda x:x-best_shift)
     fl.next('time domain after hermitian test')
     fl.image(s)
     s.register_axis({'t2':0})
+    fl.next('centered')
+    fl.image(s)
+    fl.show();quit()
     #}}}
     #{{{cost function phase correction
     s.ft('t2')
@@ -69,6 +72,7 @@ for searchstr, exp_type, nodename, postproc, label_str in [
     #{{{select echo decay fit function
     s.ift('t2')
     s = s['t2':(0,None)]
+    #s *= -1
     s['t2',0] *= 0.5
     s.ft('t2')
     data = s['t2':(0,None)].sum('t2')
