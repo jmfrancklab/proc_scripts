4e41e4e05e0ae463c3a2116f93355932788c44fa
Alex <guinneak198@github.com>
2020-08-11 13:30:38 -0400

    processing 90 pulse

--- proc_scripts/load_data.py
+++ proc_scripts/load_data.py
@@ -5,10 +5,21 @@ from sympy import symbols
 import logging
 fl=figlist_var()
 #to use type s = load_data("nameoffile")
-def proc_bruker_90_pulse(d,fl=fl):
-    s.ft('t2', shift=True) # taking FT of the data along t2 axis
-    fl.next('raw data')
-    fl.plot(s)
+def proc_bruker_90_pulse(s,fl=fl):
+    dw = diff(s.getaxis('t2')[0:2]).item()
+    s.ft('t2',shift=True)
+    s = ph1_real_Abs(s,dw)
+    s_conv = s.C
+    s_conv.ift('t2')
+    s_conv *=exp(-8*pi*s_conv.fromaxis('t2'))
+    s_conv.ft('t2')
+    if fl is not None:
+        fl.next('raw-frequency domain')
+        fl.image(s)
+    if fl is not None:
+        fl.next('raw-time domain')
+        s.ift('t2')
+        fl.image(s)
     return s
 def proc_bruker_deut_IR_withecho_mancyc(s,fl=fl):
     s.chunk('indirect',['indirect','ph1','ph2','ph3'],[-1,4,2,2]) #expands the indirect dimension into indirect, ph1, and ph2. inner most dimension is the inner most in the loop in pulse sequence, is the one on the farthest right. Brackets with numbers are the number of phase cycle steps in each one. the number of steps is unknown in 'indirect' and is therefore -1.
