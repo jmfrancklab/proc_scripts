e6c1e9830201bbb05a696c09baab811a7049511f
Alex <guinneak198@github.com>
2020-10-02 15:01:17 -0400

    processing nmr data

--- proc_CPMG.py
+++ proc_CPMG.py
@@ -9,10 +9,10 @@ dwdel1=6.5e-6
 TD=8
 tau_extra=20e-6
 for searchstr, exp_type, nodename, postproc, label_str, f_range, spincore in [
-        ('freeSL_201001','test_equip',9,'ag_CPMG_strob','free SL',(-500,500),False),
+        #('freeSL_201001','test_equip',7,'ag_CPMG_strob','free SL',(-500,500),False),
         #('200221_CPMG_TEMPOLgel_2p9_1','test_equip','signal','spincore_CPMG_v1','deadtime=5',(-500,500),True),
         #('w8_200731','test_equip',3,'ag_CPMG_strob','water loading 8',(-500,500)),
-        #('w8_200917','test_equip',6,'ag_CPMG_strob','water loading 8',(-500,500)),
+        ('w8_200917','test_equip',6,'ag_CPMG_strob','water loading 8',(-300,300),False),
         #('200305_CPMG_3p5_2','test_equip','signal','spincore_CPMG_v1','deadtime=5',(-500,500)),
         #('200305_CPMG_3p6_2','test_equip','signal','spincore_CPMG_v1','deadtime=5',(-500,500)),
         #('200305_CPMG_3p7_2','test_equip','signal','spincore_CPMG_v1','deadtime=5',(-500,500)),
@@ -23,7 +23,6 @@ for searchstr, exp_type, nodename, postproc, label_str, f_range, spincore in [
         ]:
     s = find_file(searchstr, exp_type=exp_type,
             expno=nodename, postproc=postproc, lookup=postproc_dict, fl=fl)
-    fl.show();quit()
     s.ift('t2')
     if spincore:
         s.reorder('nScans',first=True)
@@ -40,6 +39,7 @@ for searchstr, exp_type, nodename, postproc, label_str, f_range, spincore in [
     s.ft('t2')
     fl.next('before fitting')
     fl.image(s)
+    fl.show();quit()
     f,T2 = decay(s, f_range, indirect='tE')
     fl.plot_curve(f,'T2 relaxation decay')
     #}}}
