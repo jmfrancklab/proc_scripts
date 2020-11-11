5653782df61de88c756af8ec6348289494dc1bec
Alex <guinneak198@github.com>
2020-11-11 15:50:05 -0500

    ILT option added but not clean

--- proc_IR.py
+++ proc_IR.py
@@ -21,7 +21,7 @@ for searchstr,exp_type,nodename, postproc, clock_correction,this_l in [
         #('free4AT_201014','test_equip',3,'ag_IR2H',None)
         #('free4AT100mM_201104', 'test_equip',2,'ab_ir2h',None),
         #('ag_oct182019_w0_8','test_equip',3,'ab_ir2h',None)
-        ('w3_201111','test_equip',2,'ab_ir2h',None,None),
+        ('w20_201111','test_equip',2,'ab_ir2h',None,None),
         ]:
     fl.basename = searchstr
     if clock_correction is None:
@@ -75,10 +75,84 @@ for searchstr,exp_type,nodename, postproc, clock_correction,this_l in [
     s = s['t2':(0,None)]
     s['t2',0] *= 0.5
     s.ft('t2')
+    fl.next('where to cut')
+    s = s['ph2',coh_sel['ph2']]['ph1',coh_sel['ph1']]*-1 
+    fl.plot(s)
+    #fl.show();quit()
+    #{{{If visualizing via ILT
+    fl.next('Plotting phased spectra')
+    for j in range(ndshape(s)['indirect']):
+        fl.plot(s['indirect',j]['t2':(-20,20)],
+            alpha=0.5,
+            label='vd=%g'%s.getaxis('indirect')[j])
+
+    #exponential curve
+    rec_curve = s['t2':(-20,20)].C.sum('t2')
+    fl.next('recovery curve')
+    fl.plot(rec_curve,'o')
+    #fl.show();quit()
+    #attempting ILT plot with NNLS_Tikhonov_190104
+
+    T1 = nddata(logspace(-3,3,150),'T1')
+    l = sqrt(logspace(-6.0,0.05,35)) #play around with the first two numbers to get good l curve,number in middle is how high the points start(at 5 it starts in hundreds.)
+    plot_Lcurve = False
+    if plot_Lcurve:
+        def vec_lcurve(l):
+            return s.C.nnls('indirect',T1,lambda x,y: 1.0-2*exp(-x/y), l=l)
+
+        x=vec_lcurve(l) 
+
+        x_norm = x.get_prop('nnls_residual').data
+        r_norm = x.C.run(linalg.norm,'T1').data
+
+        with figlist_var() as fl:
+            fl.next('L-Curve')
+            figure(figsize=(15,10))
+            fl.plot(log10(r_norm[:,0]),log10(x_norm[:,0]),'.')
+            annotate_plot = True
+            show_lambda = True
+            if annotate_plot:
+                if show_lambda:
+                    for j,this_l in enumerate(l):
+                        annotate('%0.3f'%this_l, (log10(r_norm[j,0]),log10(x_norm[j,0])),
+                                ha='left',va='bottom',rotation=45)
+                else:
+                    for j,this_l in enumerate(l):
+                        annotate('%d'%j, (log10(r_norm[j,0]),log10(x_norm[j,0])),
+                                ha='left',va='bottom',rotation=45)
+        d_2d = s*nddata(r_[1,1,1],r'\Omega')
+    #fl.show();quit()
+    sfo1 = 289.69
+    arbitrary_reference = s.get_prop('acq')['BF1'] # will eventually be 
+    print("SFO1 is",sfo1)
+    s.setaxis('t2',lambda x:x + sfo1 - arbitrary_reference)
+    this_l = 0.033 #pick number in l curve right before it curves up
+    soln = s.real.C.nnls('indirect',T1, lambda x,y: 1.0-2.*exp(-x/y),l=this_l)
+    soln.reorder('t2',first=False)
+    soln.rename('T1','log(T1)')
+    soln.setaxis('log(T1)',log10(T1.data))
+    fl.next('water loading 20')
+    fl.image(soln['t2':(100,300)])
+
+
+    #fl.show();quit()
+    print("SAVING FILE")
+    np.savez(searchstr+'_'+str(nodename)+'_ILT_inv',
+            data=soln.data,
+            logT1=soln.getaxis('log(T1)'),
+            t2=soln.getaxis('t2'))               
+    print("FILE SAVED")
+    quit()
+
+
     #}}}
     #{{{recovery curve and fitting
     s_sliced = s['ph2',coh_sel['ph2']]['ph1',coh_sel['ph1']]*-1 # bc inverted at high powers
     # below, f_range needs to be defined
+    s_sliced.ift('t2')
+    fl.next('recovery curve')
+    fl.plot(s_sliced)
+    fl.show();quit()
     M0,Mi,R1,vd = symbols("M_0 M_inf R_1 indirect",real=True)
     f,T1 = recovery(s_sliced, (-50,50),guess=None)
     fl.plot_curve(f,'inversion recovery curve')
