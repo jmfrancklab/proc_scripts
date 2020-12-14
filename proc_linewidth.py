f0528b8d30873e387feb483b4956f6e92f32d214
Alex <guinneak198@github.com>
2020-12-14 10:46:21 -0500

    calculated linewidths for 4-AT

--- proc_linewidth.py
+++ proc_linewidth.py
@@ -1,13 +1,14 @@
 from pyspecdata import *
-from proc_Scripts import *
+from proc_scripts import *
 from proc_scripts import postproc_dict
 fl = fl_mod()
 for searchstr,exp_type,postproc in [
-        ["201118_100mM4AT",'ESR','ESR_linewidth']
+        ["201118_1mM4AT",'ESR','ESR_linewidth']
         ]:
-    s = find_file('searchstr.DSC',exp_type=exp_type,postproc=postproc,
+    s = find_file(searchstr+'.DSC',exp_type=exp_type,postproc=postproc,
             lookup=postproc_dict)
     fl.next('linewidth for 100 uM 4AT')
     fl.plot(s)
+    
     fl.show();quit()
 
--- proc_scripts/load_data.py
+++ proc_scripts/load_data.py
@@ -368,19 +368,22 @@ def proc_DOSY_CPMG(s):
     # }}}
     return s
 def proc_ESR_linewidth(s):
-    logging.infor("loading preprocessing for ESR linewidth calculation")
-    s.chunk_auto('harmonic','phase')
-    s = s['phase',0]
-    s -= s['$B_0$',:50].c.mean('$B_0$')
-    s_integral = s['harmonic',0].C.run_nopop(cumsum,'$B_0$')
+    logging.info("loading preprocessing for ESR linewidth calculation")
+    print(ndshape(s))
+    #s.chunk_auto(['$B_0$'],'phase')
+    #s = s['phase',0]
+    s -= s['$B_0$',:50].C.mean('$B_0$')
+    s_integral = s.C.run_nopop(cumsum,'$B_0$')
     x1,x2 = s_integral.getaxis('$B_0$')[r_[5,-5]]
     y1 = s_integral.data[:5].mean()
     y2 = s_integral.data[-5:].mean()
     straight_baseline = (s.fromaxis('$B_0$')-x1)*(y2-y1)/(x2-x1)
-    s_integral -= straigh_baseline
+    s_integral -= straight_baseline
     s_integral /= s_integral.data.mean()
     center_field = (s_integral * s.fromaxis('$B_0$')).mean('$B_0$').item()
     s.setaxis('$B_0$',lambda x: x-center_field)
+    s_integral = s.C.run_nopop(cumsum,'$B_0$')
+    print(s_integral)
     return s
     
 
