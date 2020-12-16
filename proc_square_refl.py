53a8a5780960771517b7f498e36e7d838f9fedc0
Alex <guinneak198@github.com>
2020-12-16 11:48:04 -0500

    displaying solenoid and capillary probe real and phase angles

--- proc_square_refl.py
+++ proc_square_refl.py
@@ -5,19 +5,24 @@ from sympy import symbols
 
 init_logging("debug")
 d = find_file(
-    "201208_Ni_sol_probe_6", exp_type="ODNP_NMR_comp/test_equipment", expno="capture1"
+    "201207_sqwv_cap_probe_1", exp_type="ODNP_NMR_comp/test_equipment", expno="capture1"
+)
+s = find_file(
+    "201208_sqwv_sol_probe_1",exp_type="ODNP_NMR_comp/test_equipment", expno="capture1"
 )
 d.setaxis("ch", r_[1, 2])
+s.setaxis("ch", r_[1,2])
+s.set_units("t","s")
 d.set_units("t", "s")
-print(d.get_prop())
-quit()
+#print(d.get_prop())
+#quit()
 
 class fl_ext(figlist_var):
     def next(self, *arg, **kwargs):
         kwargs.update({"figsize": (9, 6), "legend": True})
         super().next(*arg, **kwargs)
 
-    def abs_re_plot(fl, d, add_text="", show_angle=False):
+    def abs_re_plot(fl, d, add_text="", show_angle=True):
         for j in range(ndshape(d)["ch"]):
             chlabel = d.getaxis("ch")[j]
             l = fl.plot(
@@ -26,21 +31,21 @@ class fl_ext(figlist_var):
                 alpha=0.5,
                 label="CH%d abs " % chlabel + add_text,
             )
-            fl.plot(
-                d["ch", j].real,
-                linewidth=1,
-                color=l[0].get_color(),
-                alpha=0.5,
-                label="CH%d real " % chlabel + add_text,
-            )
-            fl.plot(
-                d["ch", j].imag,
-                "--",
-                linewidth=1,
-                color=l[0].get_color(),
-                alpha=0.5,
-                label="CH%d imag " % chlabel + add_text,
-            )
+            #fl.plot(
+            #    d["ch", j].real,
+            #    linewidth=1,
+            #    color=l[0].get_color(),
+            #    alpha=0.5,
+            #    label="CH%d real " % chlabel + add_text,
+            #)
+            #fl.plot(
+            #    d["ch", j].imag,
+            #    "--",
+            #    linewidth=1,
+            #    color=l[0].get_color(),
+            #    alpha=0.5,
+            #    label="CH%d imag " % chlabel + add_text,
+            #)
             if show_angle:
                 fl.twinx(orig=False)
                 fl.plot(
@@ -62,56 +67,93 @@ class fl_ext(figlist_var):
 
 
 with fl_ext() as fl:
+    s.ft("t")
     d.ft("t")#, shift=True)
+    s = 2*s['t':(0,None)]
     d = 2*d['t':(0,None)]
     #in the previous version we had multiplied data by 2 because the euation 1/2a*exp(iwt)+aexp(-iwt) and the 2
     #negated the half.... this is not done here. before we did it because the demodulation looked weird w/o it.
     #I am assuming this is not needed here as the demodulation either won't be used or is not affected anymore.
+    s = s["t":(0,50e6)]
     d = d["t":(0, 50e6)]
     fl.next("show the frequency distribution")
     forplot = d.C
+    forplot_s = s.C
     forplot[lambda x: abs(x) < 1e-10] = 0 #filter low frequency noise out
+    forplot_s[lambda x: abs(x) < 1e-10] = 0
     frq_guess = abs(d["ch", 1]).argmax("t").item() #this is to find the peak? argmax on reflection ch would be the peaks
+    frq_guess_s = abs(s['ch',1]).argmax("t").item()
     frq_range = r_[-10,10]*1e6 + frq_guess
+    frq_range_s = r_[-10,10]*1e6 +frq_guess_s
     d["t":(0, frq_range[0])] = 0 #again filtering out noise outside of blips
     d["t":(frq_range[1], None)] = 0
+    s["t":(0,frq_range_s[0])] = 0
+    s["t":(frq_range_s[1],None)] = 0
     # {{{ shouldn't have to do it this way, but something weird going on w/ aligndata
     tukey_filter = d.fromaxis("t")["t":tuple(frq_range)].run(lambda x: tukey(len(x))) #what does tukey mean? why are we using this nomenclature?
+    tukey_filter_s = s.fromaxis("t")["t":tuple(frq_range)].run(lambda x:tukey(len(x)))
     d["t":tuple(frq_range)] *= tukey_filter
+    s["t":tuple(frq_range)] *= tukey_filter_s
     for j in d.getaxis('ch'):
         fl.plot(abs(forplot)['ch':j], alpha=0.5, plottype="semilogy", label=f"CH{j} orig")
         fl.plot(abs(d)['ch':j][lambda x: abs(x) > 1e-10], alpha=0.5, plottype="semilogy", label=f"CH{j} filtered") 
     fl.grid()
+    for j in s.getaxis('ch'):
+        fl.plot(abs(forplot_s)['ch':j],alpha=0.5,plottype='semilogy',label=f"CH{j} orig")
+        fl.plot(abs(s)['ch':j][lambda x: abs(x) > 1e-10], alpha=0.5, plottype="semilogy",label=f"CH{j} filtered")
+    fl.grid     
     df = diff(d.getaxis("t")[r_[0, 1]]).item()
+    sf = diff(s.getaxis("t")[r_[0,1]]).item()
+    s.ift("t")
     d.ift("t")
     # {{{ determine the frequency from the phase gradient during the pulse
+    st = diff(s.getaxis("t")[r_[0,1]]).item()
     dt = diff(d.getaxis("t")[r_[0, 1]]).item()
     pulse_slice = d["ch", 0].contiguous(lambda x: abs(x) > 0.5*abs(x).data.max())[0] #defines pulse slice based on control signal
+    pulse_slice_s = s["ch",0].contiguous(lambda x: abs(x) > 0.5*abs(x).data.max())[0]
     d.setaxis("t", lambda x: x - pulse_slice[0]).register_axis({"t": 0}) #resets t axis around pulse slice
+    s.setaxis("t",lambda x: x - pulse_slice_s[0]).register_axis({"t":0})
+    pulse_slice_s -= pulse_slice_s[0]
     pulse_slice -= pulse_slice[0]
     #{{{ Not sure what this portion is doing...is this similar to a time shift? or first order phase correction?
+    s = s["t":tuple(pulse_slice_s + r_[-0.5e-6,2e-6])]
     d = d["t" : tuple(pulse_slice + r_[-0.5e-6, 2e-6])]
+    pulse_middle_s = s["ch",0]["t":tuple(pulse_slice_s +r_[+0.5e-6,-0.5e-6])]
     pulse_middle = d["ch", 0]["t" : tuple(pulse_slice + r_[+0.5e-6, -0.5e-6])]
+    ph_diff_s = pulse_middle_s["t",1:]/pulse_middle_s["t",:-1]
     ph_diff = pulse_middle["t", 1:] / pulse_middle["t", :-1]
+    ph_diff_s.sum("t")
     ph_diff.sum("t")
+    ph_diff_s = ph_diff_s.angle.item()
     ph_diff = ph_diff.angle.item()
+    frq_s = ph_diff_s/st/2/pi
     frq = ph_diff/dt/2/pi
     #}}}
     # }}}
     print("frq:", frq)
+    s *= exp(-1j*2*pi*frq*s.fromaxis("t"))
     d *= exp(-1j*2*pi*frq*d.fromaxis("t")) #convolution
+    ph0_s = s["ch",0].C.sum("t").item()
     ph0 = d["ch", 0].C.sum("t").item() #pseudo 0th order phase correction
     ph0 /= abs(ph0)
+    ph0_s /= abs(ph0_s)
     d /= ph0
+    s /= ph0_s
     fl.next("analytic signal -- phase plot", twinx=True)
     for j in range(ndshape(d)["ch"]):
         fl.twinx(orig=True)
         l = fl.plot(abs(d["ch", j]))
         fl.twinx(orig=False)
         fl.plot(d["ch", j].angle, ".", color=l[0].get_color(), alpha=0.1)
+    for j in range(ndshape(s)["ch"]):
+        fl.twinx(orig=True)
+        fl.twinx(orig=False)
+        fl.plot(s["ch",j].angle,".",color=fl.plot(abs(s["ch",j]))[0].get_color(),alpha=0.1)
     fl.grid()
     fl.next("analytic signal -- abs,re")
-    fl.abs_re_plot(d)
+    fl.abs_re_plot(d,add_text="capillary probe")
+    fl.abs_re_plot(s,add_text="solenoid probe")
+    fl.show();quit()
     scalar_refl = d["ch", 1]["t":(1e-6, 2e-6)].mean("t").item()
     fl.next("blips")
     blip_range = r_[-0.1e-6, 1e-6] #defining decay slice
