85b966314643fc23afe68ea099ae19b7d93be0a3
Alex <guinneak198@github.com>
2020-12-29 11:45:11 -0500

    processing input reffered data

--- proc_spincore_raw.py
+++ proc_spincore_raw.py
@@ -2,17 +2,17 @@ from pyspecdata import *
 from scipy.optimize import leastsq,minimize,basinhopping
 fl = figlist_var()
 for date,id_string in [
-        ('201228','Ni_sol_probe_1'),
+        ('201229','input_referred_3'),
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
     s = nddata_hdf5(filename+'/'+nodename,
             directory = getDATADIR(exp_type = 'test_equip'))
     print(ndshape(s))
-    fl.next('raw without tune limiter')
+    fl.next('Spincore with tuned limiter')
     fl.plot(s)
     s.ft('t')
-    fl.next('FTed abs')
-    fl.plot(s)
+    #fl.next('FTed abs')
+    #fl.plot(s)
     fl.show();quit()
 
