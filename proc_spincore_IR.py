4d068d593874147637437a642e5fec709b126c56
Alex <guinneak198@github.com>
2021-01-28 13:12:26 -0500

    integrate limits was at wrong line

--- proc_spincore_IR.py
+++ proc_spincore_IR.py
@@ -87,7 +87,8 @@ s_signal = s['ph2',coh_sel['ph2']]['ph1',coh_sel['ph1']]
 s_forerror = s['ph2',coh_err['ph2']]['ph1',coh_err['ph1']]
 # variance along t2 gives error for the mean, then average it across all other dimension, then sqrt for stdev
 logger.info(strm(ndshape(s_forerror)))
-
+s_signal = integrate_limits(s_signal)
+s_forerror = integrate_limits(s_forerror)
 s_forerror.run(lambda x:abs(x)**2).mean_all_but(['vd','t2']).integrate('t2').run(sqrt)
 s_signal.integrate('t2').set_error(s_forerror.data)
 # }}}
@@ -105,7 +106,6 @@ p_opt,success = leastsq(errfunc, p_ini[:])
 assert success in [1,2,3], "Fit did not succeed"
 T1 = 1./p_opt[1]
 logger.info(strm("T1:",T1,"s"))
-s_signal = integrate_limits(s_signal)
 f = fitdata(s_signal)
 error = fitdata(s_forerror)
 M0,Mi,R1,vd = symbols("M_0 M_inf R_1 vd", real=True)
