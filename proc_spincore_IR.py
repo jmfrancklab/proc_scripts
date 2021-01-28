b9bc223a210056ca3f038ae3fb2c5e14c5bd620d
John Franck (laptop) <jmfranck@syr.edu>
2021-01-28 12:47:44 -0500

    get error to show up correctly

--- proc_spincore_IR.py
+++ proc_spincore_IR.py
@@ -6,6 +6,7 @@ from matplotlib import *
 import matplotlib.pyplot as plt
 import numpy as np
 from sympy import exp as s_exp
+init_logging('debug')
 fl = figlist_var()
 t2 = symbols('t2')
 # {{{ input parameters
@@ -15,7 +16,9 @@ nodename = 'signal'
 filter_bandwidth = 5e3
 coh_sel = {'ph1':0,
         'ph2':1}
-coh_err = {'ph1':1,# coherence channels to use for error
+coh_err = {'ph1':1,# coherence pathways to use for error -- note that this
+        #             should ideally be pathways that do NOT include any known
+        #             artifacts
         'ph2':r_[0,2,3]}
 # }}}
 filename = date+'_'+id_string+'.h5'
@@ -50,9 +53,9 @@ ph0 = s['t2':0]['ph2',coh_sel['ph2']]['ph1',coh_sel['ph1']]
 if len(ph0.dimlabels) > 0:
     assert len(ph0.dimlabels) == 1, repr(ndshape(ph0.dimlabels))+" has too many dimensions"
     ph0 = zeroth_order_ph(ph0)
-    print('phasing dimension as one')
+    logger.info(strm('phasing dimension as one'))
 else:
-    print("there is only one dimension left -- standard 1D zeroth order phasing")
+    logger.info(strm("there is only one dimension left -- standard 1D zeroth order phasing"))
     ph0 = ph0/abs(ph0)
 s /= ph0
 fl.next('frequency domain -- after hermitian function test and phasing')
@@ -79,17 +82,20 @@ fl.image(s.C.setaxis(
 'vd','#').set_units('vd','scan #'))
 fl.next('recovery curve')
 s_signal = s['ph2',coh_sel['ph2']]['ph1',coh_sel['ph1']]
+# {{{ here we use the inactive coherence pathways to determine the error
+#     associated with the data
 s_forerror = s['ph2',coh_err['ph2']]['ph1',coh_err['ph1']]
 # variance along t2 gives error for the mean, then average it across all other dimension, then sqrt for stdev
-print(ndshape(s_forerror))
+logger.info(strm(ndshape(s_forerror)))
 s_forerror.run(lambda x:abs(x)**2).mean_all_but(['vd','t2']).integrate('t2').run(sqrt)
 s_signal.integrate('t2').set_error(s_forerror.data)
+# }}}
+logger.info(strm("here is what the error looks like",s_signal.get_error()))
 fl.plot(s_signal,'o',label='real')
-fl.plot(s_sliced.imag,'o',label='imaginary')
+fl.plot(s_signal.imag,'o',label='imaginary')
 fl.next('Spectrum - freq domain')
 s = s['ph2',coh_sel['ph2']]['ph1',coh_sel['ph1']]
 fl.plot(s)
-#s_sliced *= -1
 fitfunc = lambda p, x: p[0]*(1-2*np.exp(-x*p[1]))
 x = s_signal.fromaxis('vd')
 errfunc = lambda p: fitfunc(p,x).data - s_signal.data.real
@@ -97,7 +103,7 @@ p_ini = [1.0,10.0]
 p_opt,success = leastsq(errfunc, p_ini[:])
 assert success in [1,2,3], "Fit did not succeed"
 T1 = 1./p_opt[1]
-print("T1:",T1,"s")
+logger.info(strm("T1:",T1,"s"))
 
 f = fitdata(s_signal)
 error = fitdata(s_forerror)
@@ -106,14 +112,13 @@ error.functional_form = Mi + (M0-Mi)*s_exp(-vd*R1)
 f.functional_form = Mi + (M0-Mi)*s_exp(-vd*R1)
 error.fit()
 f.fit()
-print("output:",f.output())
-print("latex:",f.latex())
+logger.info(strm("output:",f.output()))
+logger.info(strm("latex:",f.latex()))
 T1 = 1./f.output('R_1')
 fl.next('fit')
 fl.plot(s_signal,'o', label='actual data')
-fl.plot(s_signal.imag,'--',label='actual imaginary')
+fl.plot(s_signal.imag,'o',label='actual imaginary')
 fl.plot(f.eval(100),label='fit')
-fl.plot(s_forerror,'o',label='error')
 fl.plot(error.eval(100),label='fit error')
 plt.text(0.75, 0.25, f.latex(), transform=plt.gca().transAxes,size='large',
         horizontalalignment='center',color='k')
