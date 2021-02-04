58eb5d44e9805d041607933c681292c8dc5e20ed
Alex <guinneak198@github.com>
2021-02-04 13:46:46 -0500

    captured p90 varied nutation

--- proc_capture_nutation.py
+++ proc_capture_nutation.py
@@ -3,9 +3,12 @@ from pylab import *
 
 with figlist_var() as fl:
     for filename, folder_name, nodename in [
-            ('210202_gds_amp_vary_4', 'test_equip', 'capture1')
+            ('210204_gds_p90_vary_1', 'nutation', 'capture1')
             ]:
         d = find_file(filename, exp_type=folder_name, expno=nodename)
+        d.rename('amplitudes','p90')
+        print(d['p90',1].get_prop())
+        quit()
         fl.next('raw data')
         fl.plot(d)
         d.ft('t',shift=True)
@@ -16,7 +19,14 @@ with figlist_var() as fl:
 
         fl.next('freq domain')
         fl.plot(d)
+        d['t':(None,1.4e7)]=0
+        d['t':(1.6e7,None)]=0
         d.ift('t')
         fl.next('analytic signal')
-        for j in range(ndshape(d)['amplitudes']):
-            fl.plot(abs(d['amplitudes',j]),alpha=0.5, linewidth=1,label = "amplitude%d"%j) 
+        for j in range(ndshape(d)['p90']):
+            fl.plot(abs(d['p90',j]),alpha=0.5, linewidth=1,label = "p90%d"%j)
+        fl.next('analytic signal in frequency domain')
+        d.ft('t')
+        for j in range(ndshape(d)['p90']):
+            fl.plot(abs(d['p90',j]),alpha=0.5, linewidth=1,label = "p90%d"%j)
+
