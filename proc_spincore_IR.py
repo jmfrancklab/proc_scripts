9aae5a372734a3b1ac6f66462f21aa831cee8f30
Alex <guinneak198@github.com>
2021-02-10 10:04:26 -0500

    correlation alignment works?

--- proc_spincore_IR.py
+++ proc_spincore_IR.py
@@ -55,46 +55,72 @@ fl.image(s.C.setaxis('vd','#').set_units('vd','scan #'))
 for j in range(ndshape(s)['vd']):
     if s['vd',j].C.sum('t2')<1:
         s['vd',j] *= -1
-s.ft('t2')
-s.ift(['ph2','ph1'])
-s.ift('t2')
-s.ft('t2')
-fl.next('prior to alignment')
-fl.image(s.C.setaxis('vd','#').set_units('vd','scan #'), interpolation = 'bilinear')
-fl.basename='first pass'
 s_final = s.C * 0
+s.ft('t2')
+fl.next('before splitting and correl align')
+fl.image(s.C.setaxis('vd','#').set_units('vd','scan #'))
 s_before = s['vd',:2]
 s_after = s['vd',3:]
-opt_shift_after,sigma_after = correl_align(s_after,indirect_dim='vd',ph1_selection=ph1_val,ph2_selection=ph2_val,fl=fl)
+fl.next('prior to alignment s_before- coherence dimension')
+fl.image(s_before)
+fl.next('prior to alignment s_after- coherence dimension')
+fl.image(s_after.C.setaxis('vd','#').set_units('vd','scan #'))
+s_before.ift(['ph2','ph1'])
+s_before.ift('t2')
+s_before.ft('t2')
+fl.next('prior to alignment')
+fl.image(s_before, interpolation = 'bilinear')
+fl.basename='first pass'
+#opt_shift_after,sigma_after = correl_align(s_after,indirect_dim='vd',ph1_selection=ph1_val,ph2_selection=ph2_val,fl=fl)
 opt_shift_before,sigma_before = correl_align(s_before,indirect_dim='vd',ph1_selection=ph1_val,ph2_selection=ph2_val,fl=fl)
 fl.basename= None
-s.ift('t2')
+s_before.ift('t2')
 s_before *= np.exp(-1j*2*pi*opt_shift_before*s_before.fromaxis('t2'))
+#s_after *= np.exp(-1j*2*pi*opt_shift_after*s_after.fromaxis('t2'))
+fl.next('out of correl alignment')
+fl.image(s_before)
+s_before.reorder('ph1',first=True)
+s_before.reorder('ph2',first=True)
+s_before.reorder('t2',first=False)
+s_before.ft(['ph1','ph2'])
+
+s_after.ift(['ph2','ph1'])
+s_after.ift('t2')
+s_after.ft('t2')
+fl.next('prior to alignment')
+fl.image(s_after.C.setaxis('vd','#').set_units('vd','scan #'), interpolation = 'bilinear')
+fl.basename='first pass'
+opt_shift_after,sigma_after = correl_align(s_after,indirect_dim='vd',ph1_selection=ph1_val,ph2_selection=ph2_val,fl=fl)
+fl.basename= None
+s_after.ift('t2')
 s_after *= np.exp(-1j*2*pi*opt_shift_after*s_after.fromaxis('t2'))
+fl.next('out of correl alignment')
+fl.image(s_after.C.setaxis('vd','#').set_units('vd','scan #'))
+s_after.reorder('ph1',first=True)
+s_after.reorder('ph2',first=True)
+s_after.reorder('t2',first=False)
+s_after.ft(['ph1','ph2'])
 s_final['vd',:2] = s_before
 s_final['vd',3:] = s_after
 s = s_final
-fl.next('out of correl alignment')
+
+fl.next('coherence domain-after corr')
+s.ft('t2')
 fl.image(s.C.setaxis('vd','#').set_units('vd','scan #'))
-s.reorder('ph1',first=True)
-s.reorder('ph2',first=True)
-s.reorder('t2',first=False)
-s.ft(['ph1','ph2'])
-fl.next('time domain-after corr')
-fl.image(s.C.setaxis('vd','#').set_units('vd','scan #'))
+s.ift('t2')
 s *= -1
 fl.next('after alignment')
 fl.image(s.C.setaxis('vd','#').set_units('vd','scan #'))
 #}}}
 #{{{phasing the aligned data
-s.ift('t2')
+#s.ift('t2')
 best_shift = hermitian_function_test(s[
     'ph2',1]['ph1',0].C.mean('vd'))
 logger.info(strm("best shift is", best_shift))
 s.setaxis('t2', lambda x: x-best_shift).register_axis({'t2':0})
-#fl.next('time domain after hermitian test')
-#fl.image(s.C.setaxis(
-#'vd','#').set_units('vd','scan #'))
+fl.next('time domain after hermitian test')
+fl.image(s.C.setaxis(
+'vd','#').set_units('vd','scan #'))
 
 ph0 = s['t2':0]['ph2',ph2_val]['ph1',ph1_val]
 if len(ph0.dimlabels) > 0:
@@ -114,7 +140,6 @@ fl.image(s.C.setaxis(
 'vd','#').set_units('vd','scan #'))
 fl.next('phased and FID sliced -- frequency domain')
 s.ft('t2')
-# }}}
 fl.image(s.C.setaxis(
 'vd','#').set_units('vd','scan #'))
 #}}}
