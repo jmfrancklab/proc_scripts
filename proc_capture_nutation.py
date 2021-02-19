98495e496a13885282fcc9859c905e551d44bf1c
Alex <guinneak198@github.com>
2021-02-19 15:15:49 -0500

    time varied nutation integrated

--- proc_capture_nutation.py
+++ proc_capture_nutation.py
@@ -3,7 +3,7 @@ from pylab import *
 
 with figlist_var() as fl:
     for filename, folder_name, nodename in [
-            ('210202_gds_amp_vary_2', 'nutation', 'capture1')
+            ('210204_gds_p90_vary_3', 'nutation', 'capture1')
             ]:
         d = find_file(filename, exp_type=folder_name, expno=nodename)
         #print(ndshape(d))
@@ -20,27 +20,25 @@ with figlist_var() as fl:
         fl.next('freq domain')
         fl.plot(d)
         #fl.show();quit()
-        d['t':(None,1.18e7)]=0
-        d['t':(1.8e7,None)]=0
+        d['t':(None,1.4e7)]=0
+        d['t':(1.6e7,None)]=0
         d.ift('t')
         fl.next('analytic signal')
         #took out for loop and hard coding p90 times because only GDS parameters saved over
         # the pp parameters
-        fl.plot(abs(d['amplitudes',0]),alpha=0.5, linewidth=1,label = "p90 = 0 amp")
-        fl.plot(abs(d['amplitudes',1]),alpha=0.5,linewidth=1,label = "p90=0.056 amp")
-        fl.plot(abs(d['amplitudes',2]),alpha=0.5,linewidth=1,label = "p90=0.111 amp")
-        fl.plot(abs(d['amplitudes',3]),alpha=0.5,linewidth=1,label = "p90=0.167 amp")
-        fl.plot(abs(d['amplitudes',4]),alpha=0.5,linewidth=1,label = "p90=0.222 amp")
-        fl.plot(abs(d['amplitudes',5]),alpha=0.5,linewidth=1,label = "p90=0.278 amp")
-        fl.plot(abs(d['amplitudes',6]),alpha=0.5,linewidth=1,label = "p90=0.333 amp")
-        fl.plot(abs(d['amplitudes',7]),alpha=0.5,linewidth=1,label = "p90=0.389 amp")
-        fl.plot(abs(d['amplitudes',8]),alpha=0.5,linewidth=1,label = "p90=0.444 amp")
-        fl.plot(abs(d['amplitudes',9]),alpha=0.5,linewidth=1,label = "p90=0.5 amp")
+        fl.plot(abs(d['p90',0]),alpha=0.5, linewidth=1,label = "p90 = 0.1 us")
+        fl.plot(abs(d['p90',1]),alpha=0.5,linewidth=1,label = "p90=1.76 us")
+        fl.plot(abs(d['p90',2]),alpha=0.5,linewidth=1,label = "p90=3.41 us")
+        fl.plot(abs(d['p90',3]),alpha=0.5,linewidth=1,label = "p90=5.07 us")
+        fl.plot(abs(d['p90',4]),alpha=0.5,linewidth=1,label = "p90=6.72 us")
+        fl.plot(abs(d['p90',5]),alpha=0.5,linewidth=1,label = "p90=8.38 us")
+        fl.plot(abs(d['p90',6]),alpha=0.5,linewidth=1,label = "p90=10.03 us")
+        fl.plot(abs(d['p90',7]),alpha=0.5,linewidth=1,label = "p90=11.69 us")
+        fl.plot(abs(d['p90',8]),alpha=0.5,linewidth=1,label = "p90=13.34 us")
+        fl.plot(abs(d['p90',9]),alpha=0.5,linewidth=1,label = "p90=15 us")
         d = abs(d)
- 
-        ninety_pulse = d['t':(2.7e-5,4e-5)]
-        one_eightypulse = d['t':(5.696e-5,8e-5)]
-        #{{{integrating 90 pulse
+        ninety_pulse = d['t':(1.237e-5,3.09e-5)]
+        one_eightypulse = d['t':(5.311e-5,8.8e-5)]
         ninety_pulse = ninety_pulse.sum('t')
         fl.next('integrate 90 pulse')
         fl.plot(ninety_pulse,'o')
