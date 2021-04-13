102c2a4cae2817502b4b67a43454b1cd43ce7d31
John Franck (laptop) <jmfranck@syr.edu>
2021-04-12 21:05:11 -0400

    comments while reviewing

--- proc_scripts/integral_w_error.py
+++ proc_scripts/integral_w_error.py
@@ -6,14 +6,15 @@ def select_pathway(s,pathway):
     for k,v in pathway.items():
         retval = retval[k,v]
     return retval    
-def integral_w_errors(self,sig_path,error_path, indirect='vd'):
+def integral_w_errors(self,sig_path,error_path, indirect='vd', direct='t2'):
+    """need a docstring!!!"""
      frq_slice = integrate_limits(self)
-     s = self['t2':frq_slice]
-     t = self.getaxis('t2')
+     s = self[direct:frq_slice]
+     t = self.getaxis(direct)
      dt = t[1]-t[0]
      errors = []
      all_labels = set(self.dimlabels)
-     all_labels -= set([indirect])
+     all_labels -= set([indirect,direct])
      extra_dims = [j for j in all_labels if not j.startswith('ph')]
      if len(extra_dims) > 0:
          raise ValueError("You have extra (non-phase cycling, non-indirect) dimensions: "
@@ -21,10 +22,14 @@ def integral_w_errors(self,sig_path,error_path, indirect='vd'):
      collected_variance = ndshape(
              [ndshape(s)['vd'],len(error_path)],['vd','pathways']).alloc()
      for j in range(len(error_path)):
+         # calculate N₂ Δt² σ², which is the variance of the integral (by error propagation)
+         # where N₂ is the number of points in the indirect dimension
          s_forerror = select_pathway(s,error_path[j])
-         s_forerror.run(lambda x: abs(x)**2).mean_all_but([indirect,'t2']).integrate('t2')
-         s_forerror *= dt
+         # mean divides by N₁ (indirect), integrate multiplies by Δt, and the
+         # mean sums all elements (there are N₁N₂ elements)
+         s_forerror.run(lambda x: abs(x)**2).mean_all_but([indirect,direct]).integrate(direct)
+         s_forerror *= dt # Δt
          collected_variance['pathways',j] = s_forerror
-     collected_variance.mean('pathways')
+     collected_variance.mean('pathways') # mean the variance above across all pathways
      s = select_pathway(s,sig_path)
-     return s.integrate('t2').set_error(collected_variance.data)
+     return s.integrate(direct).set_error(sqrt(collected_variance.data))
