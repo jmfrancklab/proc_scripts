ccc36192aeb592acc7f6eeacb516bda6a1684a4a
John Franck (laptop) <jmfranck@syr.edu>
2021-04-14 12:07:23 -0400

    get everything on one plot and set a constant seed

--- examples/integration_with_error.py
+++ examples/integration_with_error.py
@@ -1,6 +1,8 @@
 from pylab import *
 from pyspecdata import *
 from proc_scripts import integrate_limits, integral_w_errors
+from numpy.random import seed
+seed(2021) # so the same random result is generated every time -- 2021 is meaningless
 init_logging(level='debug')
 fl=figlist_var()
 t2 = nddata(r_[0:1:1024j], 't2')
@@ -31,11 +33,11 @@ fl.plot(just_noise, '.', capsize=6)
 data /= 0.5*0.25 # the dt in the integral for both dims
 data /= sqrt(ndshape(data)['ph1']*ndshape(data)['ph2']) # normalization
 # }}}
-print("check the std after FT",std(data['ph1',0]['ph2',0].data.real))
+logger.debug(strm("check the std after FT",std(data['ph1',0]['ph2',0].data.real)))
 # the sqrt on the next line accounts for the var(real)+var(imag)
 std_off_pathway = data['ph1',0]['ph2',0].C.run(lambda x: abs(x)**2).mean_all_but('t2').mean('t2').run(sqrt)/sqrt(2)
-print("check the std after FT -- manually",std_off_pathway)
-print("check the std after FT -- manually don't collapse vd",data['ph1',0]['ph2',0].C.run(lambda x: abs(x)**2).mean_all_but(['t2','vd']).mean('t2').run(sqrt)/sqrt(2))
+logger.debug(strm("check the std after FT -- manually",std_off_pathway))
+logger.debug(strm("check the std after FT -- manually don't collapse vd",data['ph1',0]['ph2',0].C.run(lambda x: abs(x)**2).mean_all_but(['t2','vd']).mean('t2').run(sqrt)/sqrt(2)))
 dt = diff(data.getaxis('t2')[r_[0,1]]).item()
 data.ft('t2', shift=True)
 # {{{ 
@@ -46,31 +48,30 @@ fl.image(data, alpha=0.5)
 fl.next('manual limits', legend=True)
 # here I plot w/ manually chosen integration bounds:
 for bounds in [(0,200), # seem reasonable to me
-    (-95.90625,295.7109375) # what's currently picked by the automatic routine
+    (-90.91113281,307.69921875) # what's currently picked by the automatic routine
     ]:
     manual_bounds = data['ph1',0]['ph2',1]['t2':bounds]
     std_off_pathway = data['ph1',0]['ph2',0]['t2':bounds].C.run(lambda x: abs(x)**2).mean_all_but('t2').mean('t2').run(sqrt)
-    print("here is the std calculated from an off pathway",std_off_pathway,"does it match",fake_data_noise_std,"?")
+    logger.debug(strm("here is the std calculated from an off pathway",std_off_pathway,"does it match",fake_data_noise_std,"?"))
     N = ndshape(manual_bounds)['t2']
     df = diff(data.getaxis('t2')[r_[0,1]]).item()
-    print(ndshape(manual_bounds),"df is",df,"N is",N,"N*df is",N*df)
+    logger.debug(strm(ndshape(manual_bounds),"df is",df,"N is",N,"N*df is",N*df))
     manual_bounds.integrate('t2')
     # N terms that have variance given by fake_data_noise_std**2 each multiplied by df
     # the 2 has to do w/ real/imag/abs -- see check_integration_error
     propagated_variance = N * df**2 * fake_data_noise_std**2 * 2
-    print("manually calculated integral error is",sqrt(propagated_variance))
+    logger.debug(strm("manually calculated integral error is",sqrt(propagated_variance)))
     manual_bounds.set_error(sqrt(propagated_variance))
     fl.plot(manual_bounds, '.', capsize=6,
             label=r'bounds $%4g\rightarrow%4g$'%bounds,
             alpha=0.5)
 # Now you need to run your code that automatically chooses integration bounds
 # and also assigns error
-fl.next('automatic limits and error')
 error_pathway = (set(((j,k) for j in range(2) for k in range(4)))
             - set(excluded_pathways)
             - set([(signal_pathway['ph1'],signal_pathway['ph2'])]))
 error_pathway = [{'ph1':j,'ph2':k} for j,k in error_pathway]
 data = integral_w_errors(data,signal_pathway,error_pathway)
-fl.plot(data,'.',label='real', capsize=6)
-fl.plot(data.imag,'.',label='imaginary', capsize=6)
+fl.plot(data,'.',label='auto: real', capsize=6)
+fl.plot(data.imag,'.',label='auto: imaginary', capsize=6)
 fl.show()
