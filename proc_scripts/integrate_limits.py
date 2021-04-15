a7c2a69ec3492bf3117a941b45a7d1f42ac3b02f
Alec Beaton (laptop) <aabeaton@syr.edu>
2021-04-15 11:24:51 -0400

    Add in code to determine convolution width

--- proc_scripts/integrate_limits.py
+++ proc_scripts/integrate_limits.py
@@ -7,6 +7,20 @@ import logging
 def integrate_limits(s, axis="t2", fwhm=100, fl=None):
     signal_sign = s.C.sum(axis).run(np.real).run(np.sign)
     temp = s.real * signal_sign
+
+    # pulled from apodization code
+    sigma = nddata(np.linspace(1e-5,1e3,1000),'sigma').set_units('sigma','s')
+    s_avg = s.C.mean_all_but('t2')
+    gaussians = np.exp(-s_avg.C.fromaxis('t2')**2/2/sigma**2)
+    signal_E = (abs(s_avg * gaussians)**2).sum('t2')
+    signal_E /= signal_E.data.max()
+    filter_width = abs(signal_E-1/sqrt(2)).argmin('sigma').item()
+    if fl is not None:
+        fl.next('signal Energy')
+        fl.plot(signal_E, human_units=False)
+        fl.plot(signal_E['sigma':(filter_width,filter_width+1e-6)],'o', human_units=False)
+    fwhm = filter_width
+
     fl.push_marker()
     if fl is not None:
         fig, (ax1,ax2) = subplots(2,1)
