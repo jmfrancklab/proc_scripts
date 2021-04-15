8f6262aea64388fb010e8be448a0526a08995609
John Franck (laptop) <jmfranck@syr.edu>
2021-04-14 20:40:00 -0400

    much better automatic bounds

--- proc_scripts/integrate_limits.py
+++ proc_scripts/integrate_limits.py
@@ -1,6 +1,15 @@
 from pyspecdata import *
 import numpy as np
+import logging
 
-def integrate_limits(s, axis='t2', convwidth=100):
-    frq_slice = s.C.mean_all_but(axis).convolve(axis,convwidth).contiguous(lambda x: abs(x)>0.5*abs(x).data.max())[0]
-    return frq_slice 
+
+def integrate_limits(s, axis="t2", convwidth=50):
+    signal_sign = s.C.sum(axis).run(np.real).run(np.sign)
+    logging.debug(strm("the signal sign", signal_sign))
+    frq_slice = (
+        (s.real * signal_sign)
+        .mean_all_but(axis)
+        .convolve(axis, convwidth)
+        .contiguous(lambda x: abs(x) > 0.5 * abs(x).data.max())[0]
+    )
+    return frq_slice
