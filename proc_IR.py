411f91bdb8d732ff263f710eb08454f0b3822b7d
Alex <guinneak198@github.com>
2021-05-25 16:18:35 -0400

    higher level processing for IR

--- proc_IR.py
+++ proc_IR.py
@@ -6,27 +6,16 @@ fl = fl_mod()
 # {{{ input parameters
 save_npz = False
 #}}}
-T1_list = []
 for thisfile,exp_type,nodename,postproc,f_range,t_range,IR,ILT in [
-       ('210525_TEMPOL7uM_cap_probe_DNP','ODNP_NMR_comp/test_equipment','0p001_Watts','spincore_IR_v1',
-           (-0.5e3,1e3),(None,83e-3),True,False),
-        ('210525_TEMPOL7uM_cap_probe_DNP','ODNP_NMR_comp/test_equipment','0p5_Watts','spincore_IR_v1',
-           (-0.5e3,1e3),(None,83e-3),True,False),
-        ('210525_TEMPOL7uM_cap_probe_DNP','ODNP_NMR_comp/test_equipment','1_Watts','spincore_IR_v1',
-           (-0.5e3,1e3),(None,83e-3),True,False),
-        ('210525_TEMPOL7uM_cap_probe_DNP','ODNP_NMR_comp/test_equipment','1p5_Watts','spincore_IR_v1',
-           (-0.5e3,1e3),(None,83e-3),True,False),
-        ('210525_TEMPOL7uM_cap_probe_DNP','ODNP_NMR_comp/test_equipment','2_Watts','spincore_IR_v1',
-           (-0.5e3,1e3),(None,83e-3),True,False),
-        ('210525_TEMPOL7uM_cap_probe_DNP','ODNP_NMR_comp/test_equipment','2p5_Watts','spincore_IR_v1',
-           (-0.5e3,1e3),(None,83e-3),True,False),
-
+       ('210512_water_cap_probe_IR_33dBm','inv_rec','signal','spincore_IR_v1',
+           (-0.291e3,0.615e3),(None,83e-3),True,False),
+        # ('210322_water_control_FIR_noPower','inv_rec','signal','spincore_IR_v1',
+        #    (-0.146e3,-0.06e3),(None,83e-3),False,False),
+        #('210517_4OHTempo_TempControl_probe_FIR_34dBm.','odnp_nmr_comp/inv_rec','signal','spincore_IR_v1',
+        #    (-0.4e3,0.4e3),(None,20e-3),False,False),
+        #('w3_201111','test_equip',2,'ag_IR2H',(-600,600),(0,None),True)
         ]:
     s = find_file(thisfile,exp_type=exp_type,expno=nodename,
             postproc=postproc,lookup=postproc_dict,fl=fl)
-    #fl.show();quit()
-    T1 = process_IR(s,label=thisfile,W=5,f_range=f_range,fl=fl) 
-    T1_list.append(T1)
-print(T1_list)
-quit()
+    process_IR(s,label=thisfile,fl=fl) 
 
