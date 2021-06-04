209defa4563a008763da1189f5140e568320c594
John Franck (laptop) <jmfranck@syr.edu>
2021-06-04 13:35:04 -0400

    roll back AG's garbage in check integration errors

--- examples/check_integration_error.py
+++ examples/check_integration_error.py
@@ -30,6 +30,7 @@ all_results = ndshape(clean_data) + (n_repeats, "repeats")
 all_results.pop("t2").pop("ph1").pop("ph2")
 all_results = all_results.alloc()
 all_results.setaxis("vd", clean_data.getaxis("vd"))
+print("shape of all results", ndshape(all_results))
 for j in range(n_repeats):
     data = clean_data.C
     data.add_noise(fake_data_noise_std)
@@ -50,7 +51,7 @@ for j in range(n_repeats):
     manual_bounds.integrate("t2")
     # N terms that have variance given by fake_data_noise_std**2 each multiplied by df
     all_results["repeats", j] = manual_bounds
-    #print("#%d"%j)
+    print("#%d"%j)
 std_off_pathway = (
     data["ph1", 0]["ph2", 0]["t2":bounds]
     .C.run(lambda x: abs(x)**2)
@@ -62,11 +63,8 @@ print(
     "off-pathway std", std_off_pathway / sqrt(2), "programmed std", fake_data_noise_std
 )
 propagated_variance_from_inactive = N * df ** 2 * std_off_pathway ** 2
-
 # the 2 here has to do w/ real/imag/abs I believe, and is needed to get the
 # variance to match the actual std
-print(manual_bounds)
-quit()
 propagated_variance = N * df**2 * fake_data_noise_std**2 * 2
 fl.next("different types of error")
 manual_bounds.set_error(sqrt(propagated_variance))
@@ -82,9 +80,5 @@ fl.plot(manual_bounds, ".", capsize=6, label=r"propagated from inactive std", al
 all_results.mean("repeats", std=True)
 # by itself, that would give error bars, but the data would be averaged -- better to put the data in the same position
 manual_bounds.set_error(all_results.get_error())
-print(type(all_results))
-print(type(all_results.get_error()))
-print(type(manual_bounds))
-quit()
 fl.plot(manual_bounds, ".", capsize=6, label=r"std from repeats", alpha=0.5)
 fl.show()
