f4b372a8d6bba66ce3b1df3c5d463545437f9b41
Alex <guinneak198@github.com>
2021-06-17 11:16:42 -0400

    Revert "now there is only ONE script for correlation alignment that works for"
    This reverts commit 4b71fba7cc3fe1fd361880e01d536e8cf891e645.

--- pyspecProcScripts/third_level/process_enhancement.py
+++ pyspecProcScripts/third_level/process_enhancement.py
@@ -2,7 +2,7 @@ from pyspecdata import *
 from scipy.optimize import leastsq,minimize,basinhopping,nnls
 from pyspecProcScripts import *
 from pyspecProcScripts import postproc_dict
-from pyspecProcScripts.correlation_alignment import correl_align
+from pyspecProcScripts.correlation_alignment_ODNP import correl_align
 from sympy import symbols
 from matplotlib import *
 import numpy as np
@@ -35,10 +35,11 @@ def process_enhancement(s, searchstr='', signal_pathway = {'ph1':1},
         excluded_pathways = [(0,0)], freq_range=(None,None),
         t_range=(0,0.083),sign=None,fl=None):
     s *= sign
-    if fl is not None:
-        fl.push_marker()
-        fl.side_by_side('show frequency limits\n$\\rightarrow$ use to adjust freq range',
-                s,thisrange=freq_range) # visualize the frequency limits
+    #if fl is not None:
+    #    fl.push_marker()
+    #    fl.side_by_side('show frequency limits\n$\\rightarrow$ use to adjust freq range',
+    #            s,thisrange=freq_range) # visualize the frequency limits
+    #fl.show();quit()
     s.ift('t2')
     s.reorder(['ph1','power','t2'])
     rcParams.update({
@@ -65,10 +66,18 @@ def process_enhancement(s, searchstr='', signal_pathway = {'ph1':1},
         fl.next('time domain before hermitian')
         fl.image(s)
     #{{{Applying phasing corrections
+    #s.ift('t2') # inverse fourier transform into time domain
     best_shift,max_shift = hermitian_function_test(select_pathway(s,signal_pathway).C.convolve('t2',0.01))
     best_shift = 0.033e-3
     s.setaxis('t2',lambda x: x-best_shift).register_axis({'t2':0})
     logger.info(strm("applying zeroth order correction"))
+    #if fl is not None:
+    #    fl.next('After hermitian-t domain')
+    #    fl.image(s)
+    #    s.ft('t2')
+    #    fl.next('after hermitian-freq domain')
+    #    fl.image(s)
+    #fl.show();quit()    
     s.ift(['ph1'])
     phasing = s['t2',0].C
     phasing.data *= 0
@@ -76,7 +85,7 @@ def process_enhancement(s, searchstr='', signal_pathway = {'ph1':1},
     phasing['ph1',1] = 1
     phasing.ift(['ph1'])
     s /= phasing
-    ph0 = s['t2':0]/phasing #if I don't divide by phasing here my signal moves from ph1:1 to ph1:0 I do not know why or how to fix this without dividing by phasing
+    ph0 = s['t2':0]
     ph0 /= abs(ph0)
     s /= ph0
     s.ft(['ph1'])
@@ -85,22 +94,32 @@ def process_enhancement(s, searchstr='', signal_pathway = {'ph1':1},
     if fl is not None:
         fl.next('After zeroth order phase correction')
         fl.image(as_scan_nbr(s))
+        #s.ift('t2')
+        #fl.next('time domain after zeroth order')
+        #fl.image(s)
+    #fl.show();quit()    
     s.reorder(['ph1','power','t2'])
     logger.info(strm("zero corssing at",zero_crossing))
     #}}}
     #{{{Applying correlation alignment
-    s_aligned,opt_shift,sigma = correl_align(s,indirect_dim='power',
+    s.ift(['ph1'])
+    opt_shift,sigma = correl_align(s,indirect_dim='power',
             ph1_selection=1,sigma=0.001)
-    s = s_aligned
+    s.ift('t2')
+    s *= np.exp(-1j*2*pi*opt_shift*s.fromaxis('t2'))
+    s.ft('t2')
     fl.basename= None
     if fl is not None:
         fl.next(r'after correlation, $\varphi$ domain')
         s.set_units('t2','Hz')
-        fl.image(as_scan_nbr(s))
+        #s.C.reorder(['power','ph1']).smoosh(['power','ph1'])
+        #s /= phasing
+        fl.image(s,human_units=False)
     s.ift('t2')
     if fl is not None:
         fl.next('t domain after correlation')
-        fl.image(as_scan_nbr(s))
+        fl.image(s)
+    #fl.show();quit()    
     s.ft(['ph1'])
     if fl is not None:
         fl.next('after correlation alignment FTed ph')
@@ -115,42 +134,48 @@ def process_enhancement(s, searchstr='', signal_pathway = {'ph1':1},
         fl.image(as_scan_nbr(s))
     #}}}
     s.ift('t2')
-    s = s['t2':(0,t_range[-1])]
-    s['t2':0] *= 0.5
-    s.ft('t2')
+    d=s.C
+    d.ft('t2')
+    d.ift('t2')
+    d = d['t2':(0,t_range[-1])]
+    d['t2':0] *= 0.5
+    d.ft('t2')
     if fl is not None:
         fl.next('FID sliced')
-        fl.image(as_scan_nbr(s))
-    s *= sign
-    if 'nScans' in s.dimlabels:
-        s.mean('nScans')
+        fl.image(d)
+        #d.ift('t2')
+        #fl.next('FID sliced')
+        #fl.image(d,human_units=False)
+        #d.ft('t2')
+    d *= sign
     # {{{ this is the general way to do it for 2 pulses I don't offhand know a compact method for N pulses
-    error_pathway = (set(((j) for j in range(ndshape(s)['ph1'])))
+    error_pathway = (set(((j) for j in range(ndshape(d)['ph1'])))
             - set(excluded_pathways)
             - set([(signal_pathway['ph1'])]))
     error_pathway = [{'ph1':j} for j in error_pathway]
     # }}}
     #{{{ integrating with error bar calculation
-    s_int,frq_slice,std = integral_w_errors(s,signal_pathway,error_pathway,
+    d_,frq_slice,std = integral_w_errors(d,signal_pathway,error_pathway,
             indirect='power', fl=fl, return_frq_slice=True)
-    x = s_int.get_error()
+    x = d_.get_error()
     x[:] /= sqrt(2)
-    s = s_int.C
+    d = d_.C
     #}}}
     
     #{{{Normalizing by max 
-    s /= max(s.data)
+    idx_maxpower = np.argmax(s.getaxis('power'))
+    d /= max(d.data)
     #}}}
     power_axis_dBm = array(s.get_prop('meter_powers'))
     power_axis_W = zeros_like(power_axis_dBm)
     power_axis_W[:] = (1e-2*10**((power_axis_dBm[:]+10.)*1e-1))
     power_axis_W = r_[0,power_axis_W]
-    s.setaxis('power',power_axis_W)
+    d.setaxis('power',power_axis_W)
     thiscolor = next(thesecolors)
     if fl is not None:
         fl.next('E(p)')
-        fl.plot(s['power',:-3], 'ko', capsize=6, alpha=0.3)
-        fl.plot(s['power',-3:],'ro',capsize=6, alpha=0.3)
+        fl.plot(d['power',:-3], 'ko', capsize=6, alpha=0.3)
+        fl.plot(d['power',-3:],'ro',capsize=6, alpha=0.3)
         fl.pop_marker()
-    enhancement = s['power',:-3]
-    return enhancement
+    enhancement = d['power',:-3]
+    return enhancement,idx_maxpower
