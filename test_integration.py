1e6b96b902258b556f00ee4eab264edcc51c9064
Alex <guinneak198@github.com>
2021-06-17 14:10:29 -0400

    for paper

--- test_integration.py
+++ test_integration.py
@@ -2,13 +2,13 @@ from pyspecdata import *
 from pylab import *
 from matplotlib import *
 from pyspecProcScripts import *
-from pyspecProcScripts.correlation_alignment_ODNP import correl_align
+from pyspecProcScripts.correlation_alignment import correl_align
 import numpy as np
 fl = figlist_var()
 signal_pathway = {'ph1': 1, 'ph2':0}
 excluded_pathways = [(0,0),(0,3)]
 for thisfile,exp_type,nodename in [
-        ('210409_Ni_cap_probe_echo_1024','ODNP_NMR_comp/test_equipment','signal')
+        ('201113_TEMPOL_capillary_probe_16Scans_noModCoil','ODNP_NMR_comp/test_equipment','signal')
         ]:
     s = find_file(thisfile,exp_type=exp_type,expno=nodename)
     nPoints = s.get_prop('acq_params')['nPoints']
@@ -24,18 +24,21 @@ for thisfile,exp_type,nodename in [
     s.setaxis('nScans',r_[0:nScans])
     s.set_units('t2','s')
     s.reorder('t2',first=False)
-    s.chunk('nScans',['repeats','nScans'],[32,-1])
+    #s.chunk('nScans',['repeats','nScans'],[32,-1])
     s.ft('t2',shift=True)
     s.ft(['ph1','ph2'])
-    s = s.mean('repeats')
+    #s = s.mean('repeats')
     fl.next('raw data')
     fl.image(s)
     s.ift('t2')
     fl.next('raw data time domain')
-    t_range=(0,0.06)
-    f_range = (-1e3,1e3)
+    fl.image(s) 
+    #fl.show();quit()
+    t_range=(0,0.05)
+    f_range = (-0.1e3,0.13e3)
+    t_rx = (t_range[-1]/4)*3
     s.ift(['ph1','ph2'])
-    rx_offset_corr = s['t2':(0.045,None)]
+    rx_offset_corr = s['t2':(t_rx,None)]
     rx_offset_corr = rx_offset_corr.data.mean()
     s -= rx_offset_corr
     s.ft('t2')
@@ -74,12 +77,14 @@ for thisfile,exp_type,nodename in [
     #{{{alignment
     #s.ift(['ph1','ph2'])
     #s.ft('t2')
-    #opt_shift,sigma = correl_align(s,indirect_dim='nScans',
+    #s_aligned,opt_shift,sigma = correl_align(s,indirect_dim='nScans',
     #        ph1_selection = signal_pathway['ph1'],
     #        ph2_selection = signal_pathway['ph2'],sigma=50)
+    #s = s_aligned
     #s.ift('t2')
     #s *= np.exp(-1j*2*pi*opt_shift*s.fromaxis('t2'))
     #s.ft('t2')
+    #s = s_aligned
     #fl.basename=None
     #fl.next(r'after correlation, $\varphi$ domain')
     #fl.image(s)
@@ -112,6 +117,7 @@ for thisfile,exp_type,nodename in [
     fl.next('comparison of std')
     avg_s_int = s_int.get_error().mean().item()
     s_int1 = avg_s_int
+    s_int1 /= s_int.data.argmax().item()
     fl.plot(s_int.get_error(),'o',label='returned error from integral_w_errors')
     axhline(y=avg_s_int,c='red',linestyle=":",label='averaged returned error from integral_w_errors')
     data1 =data.C
@@ -121,6 +127,9 @@ for thisfile,exp_type,nodename in [
     data2 = data1.C
     data1=select_pathway(data1,signal_pathway)
     data2=select_pathway(data2,{'ph1':0,'ph2':0})
+    data2 /= data2.argmax('t2').item()
+    data1 /= data1.argmax('t2').item()
+
     data2.integrate('t2')
     data1.integrate('t2')
     after_slice_error = data1.real.run(np.std,'nScans')
@@ -129,8 +138,11 @@ for thisfile,exp_type,nodename in [
             c='k',linestyle=":",label='error associated with CT pathway of signal')
     axhline(y=float(off_CT_error.data),
             c='blue',linestyle=":",label='error associated with inactive CT pathway')
-    plt.ylim(0,160) 
+    plt.ylim(0,450) 
     plt.legend()
+    print("error from integral w errors",avg_s_int)
+    print("error associated with CT of signal", float(after_slice_error.data))
+    print("error associated with inactive CT pathway",float(off_CT_error.data))
     fl.show();quit()
     data1.integrate('t2')
     data_on = select_pathway(data1,signal_pathway)
