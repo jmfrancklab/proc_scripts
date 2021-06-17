b1f52efb7aed0585991cc8ffecb5acc3983f0eb8
Alex <guinneak198@github.com>
2021-06-17 16:04:00 -0400

    FOR SAM FIXES ON IR AND ENHANCEMENT

--- proc_IR.py
+++ proc_IR.py
@@ -8,14 +8,20 @@ save_npz = False
 #}}}
 coherence_pathway = {'ph1':0,'ph2':1}
 for thisfile,exp_type,nodename,postproc,f_range,t_range,clock_correction,IR,ILT in [
-       ('210615_S175R1a_pR_DDM_ODNP','odnp',
+       ('210616_F195R1a_pR_DDM_ODNP','ODNP_NMR_comp/test_equipment',
            'FIR_0dBm','spincore_IR_v1',
-           (-0.5e3,0.5e3),(None,50e-3),True,False,False),
+           (-0.35e3,0.23e3),(None,40e-3),True,True,False),
+        # ('210322_water_control_FIR_noPower','inv_rec','signal','spincore_IR_v1',
+        #    (-0.09e3,-0.06e3),(None,83e-3),False,False),
+        #('210517_4OHTempo_TempControl_probe_FIR_34dBm.','odnp_nmr_comp/inv_rec','signal','spincore_IR_v1',
+        #    (-0.4e3,0.4e3),(None,20e-3),False,False),
+        #('w3_201111','test_equip',2,'ag_IR2H',(-600,600),(0,None),True)
         ]:
     s = find_file(thisfile,exp_type=exp_type,expno=nodename,
             postproc=postproc,lookup=postproc_dict,fl=fl)
+    #fl.show();quit()
     myslice = s['t2':f_range]
-    mysgn = determine_sign(select_pathway(myslice, coherence_pathway), fl=fl)
-    T1 = process_IR(s,label=thisfile,W=7,f_range=f_range,t_range=t_range,
+    mysgn = determine_sign(select_pathway(myslice, coherence_pathway), fl=None)
+    T1 = process_IR(s,label=thisfile,W=1,f_range=f_range,t_range=t_range,
             clock_correction=clock_correction,IR=IR,flip=True,sign=mysgn,fl=fl) 
     fl.show()
--- proc_echo_mw.py
+++ proc_echo_mw.py
@@ -9,8 +9,7 @@ from sympy import symbols, Symbol, latex,limit,init_printing
 #from pylab import *
 #from sympy import exp as s_exp
 #from scipy.optimize import leastsq,minimize,basinhopping,nnls
-import os
-os.chdir('C:/Users/saman/proc_scripts/')
+
 #{{{input parameters
 plt.rcParams.update({
     "figure.facecolor":  (1.0, 1.0, 1.0, 0.0),  # clear
@@ -20,21 +19,20 @@ plt.rcParams.update({
 logger = init_logging("info")
 t2 = symbols('t2')
 signal_pathway = {'ph1':1}
+freq_range= (-0.5e3,0.5e3)
+t_range = (0,0.083)
 fl = fl_mod()
 #}}}
-for filename,nodename,file_location,postproc,freq_range,t_range in [
+for filename,nodename,file_location,postproc in [
         ('210616_F195R1a_pR_DDM_ODNP','enhancement',
-            'odnp','spincore_ODNP_v1',
-            (-0.4e3,0.4e3),(0,50e-3)),
-#        ('210615_S175R1a_pR_DDM_ODNP','enhancement',
-#            'odnp','spincore_ODNP_v1'),
-#            (-5e3,5e3)),
+            'ODNP_NMR_comp/test_equipment','spincore_ODNP_v1'),
         ]:
     s = find_file(filename,exp_type=file_location,expno=nodename,
             postproc=postproc,lookup=postproc_dict,fl=fl)
+    #fl.show();quit()
     myslice = s['t2':freq_range]
     mysign = determine_sign(select_pathway(myslice,signal_pathway,mult_ph_dims=False))
     enhancement,idx_maxpower = process_enhancement(s,searchstr = filename,
-            freq_range=freq_range, t_range=t_range,flip=True,sign=mysign,fl=fl)
+            freq_range=freq_range, t_range=t_range,sign=mysign,fl=fl)
     fl.show()
 
