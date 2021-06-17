4b71fba7cc3fe1fd361880e01d536e8cf891e645
Alex <guinneak198@github.com>
2021-06-17 10:27:18 -0400

    now there is only ONE script for correlation alignment that works for different phase cycles. It also accomodates data that has nScans

--- proc_IR.py
+++ proc_IR.py
@@ -10,7 +10,7 @@ coherence_pathway = {'ph1':0,'ph2':1}
 for thisfile,exp_type,nodename,postproc,f_range,t_range,clock_correction,IR,ILT in [
        ('210614_TEMPOL_100mM_cap_probe_DNP','inv_rec',
            'FIR_nopower','spincore_IR_v1',
-           (-0.213e3,0.25e3),(None,30e-3),False,False,False),
+           (-0.213e3,0.25e3),(None,30e-3),False,True,False),
         # ('210322_water_control_FIR_noPower','inv_rec','signal','spincore_IR_v1',
         #    (-0.09e3,-0.06e3),(None,83e-3),False,False),
         #('210517_4OHTempo_TempControl_probe_FIR_34dBm.','odnp_nmr_comp/inv_rec','signal','spincore_IR_v1',
@@ -19,9 +19,8 @@ for thisfile,exp_type,nodename,postproc,f_range,t_range,clock_correction,IR,ILT 
         ]:
     s = find_file(thisfile,exp_type=exp_type,expno=nodename,
             postproc=postproc,lookup=postproc_dict,fl=fl)
-    #fl.show();quit()
     myslice = s['t2':f_range]
     mysgn = determine_sign(select_pathway(myslice, coherence_pathway), fl=None)
     T1 = process_IR(s,label=thisfile,W=1,f_range=f_range,t_range=t_range,
-            clock_correction=clock_correction,IR=IR,flip=True,sign=mysgn,fl=fl) 
+            clock_correction=clock_correction,IR=IR,sign=mysgn,fl=fl) 
     fl.show()
