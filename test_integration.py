0596358b30bd075f7c4ab9423e838e8c2c7fe91f
Alex <guinneak198@github.com>
2021-06-18 17:12:53 -0400

    AG normalize

--- test_integration.py
+++ test_integration.py
@@ -2,13 +2,13 @@ from pyspecdata import *
 from pylab import *
 from matplotlib import *
 from pyspecProcScripts import *
-from pyspecProcScripts.correlation_alignment_ODNP import correl_align
+from pyspecProcScripts.correlation_alignment import correl_align
 import numpy as np
 fl = figlist_var()
 signal_pathway = {'ph1': 1, 'ph2':0}
 excluded_pathways = [(0,0),(0,3)]
 for thisfile,exp_type,nodename in [
-        ('201113_TEMPOL_capillary_probe_16Scans_ModCoil','ODNP_NMR_comp/test_equipment','signal')
+        ('201113_TEMPOL_capillary_probe_16Scans_noModCoil','ODNP_NMR_comp/test_equipment','signal')
         ]:
     s = find_file(thisfile,exp_type=exp_type,expno=nodename)
     nPoints = s.get_prop('acq_params')['nPoints']
@@ -24,18 +24,13 @@ for thisfile,exp_type,nodename in [
     s.setaxis('nScans',r_[0:nScans])
     s.set_units('t2','s')
     s.reorder('t2',first=False)
-    #print(ndshape(s))
-    #quit()
-    #s.chunk('nScans',['repeats','nScans'],[32,-1])
     s.ft('t2',shift=True)
     s.ft(['ph1','ph2'])
-    #s = s.mean('repeats')
     fl.next('raw data')
     fl.image(s)
     s.ift('t2')
     fl.next('raw data time domain')
     fl.image(s)
-    #fl.show();quit()
     t_range=(0,0.05)
     f_range = (-0.11e3,0.12e3)
     s.ift(['ph1','ph2'])
@@ -50,8 +45,6 @@ for thisfile,exp_type,nodename in [
     fl.image(s)
     s.ift('t2')
     best_shift,window_size = hermitian_function_test(select_pathway(s,signal_pathway))
-    print(best_shift)
-    #best_shift = 0.003
     s.setaxis('t2',lambda x: x-best_shift)
     s.register_axis({'t2':0})
     fl.next('After hermitian phase correction')
@@ -77,33 +70,34 @@ for thisfile,exp_type,nodename in [
     fl.image(s)
     s.ift('t2')
     #{{{alignment
-    #s.ift(['ph1','ph2'])
-    #s.ft('t2')
-    #opt_shift,sigma = correl_align(s,indirect_dim='nScans',
-    #        ph1_selection = signal_pathway['ph1'],
-    #        ph2_selection = signal_pathway['ph2'],sigma=50)
-    #s.ift('t2')
-    #s *= np.exp(-1j*2*pi*opt_shift*s.fromaxis('t2'))
-    #s.ft('t2')
-    #fl.basename=None
-    #fl.next(r'after correlation, $\varphi$ domain')
-    #fl.image(s)
-    #s.ift('t2')
-    #s.ft(['ph1','ph2'])
-    #fl.next('after correl - time domain')
-    #fl.image(s)
-    #s.ft('t2')
-    #fl.next('after correl - freq domain')
-    #fl.image(s)
+    s.ift(['ph1','ph2'])
+    s.ft('t2')
+    opt_shift,sigma = correl_align(s,indirect_dim='nScans',
+            ph1_selection = signal_pathway['ph1'],
+            ph2_selection = signal_pathway['ph2'],sigma=50)
+    s.ift('t2')
+    s *= np.exp(-1j*2*pi*opt_shift*s.fromaxis('t2'))
+    s.ft('t2')
+    fl.basename=None
+    fl.next(r'after correlation, $\varphi$ domain')
+    fl.image(s)
+    s.ift('t2')
+    s.ft(['ph1','ph2'])
+    fl.next('after correl - time domain')
+    fl.image(s)
+    s.ft('t2')
+    fl.next('after correl - freq domain')
+    fl.image(s)
     ##}}}
-    #s.ift('t2')
+    s.ift('t2')
     s = s['t2':(0,t_range[-1])]
     s['t2':0] *= 0.5
     s.ft('t2')
     fl.next('FID sliced')
     fl.image(s)
     s.reorder(['ph1','ph2','nScans','t2'])
-    data = s.C    
+    s /= s.data.mean()
+    data = s.C  
     error_pathway = (set(((j,k) for j in range(ndshape(s)['ph1']) for k in range(ndshape(s)['ph2'])))
             - set(excluded_pathways)
             - set([(signal_pathway['ph1'],signal_pathway['ph2'])]))
@@ -135,7 +129,11 @@ for thisfile,exp_type,nodename in [
     print('error from integral w errors',avg_s_int)
     print('error associated with CT pathway of signal', float(after_slice_error.data))
     print('error associated with inactive CT pathway',float(off_CT_error.data))
-    #plt.ylim(0,160) 
+    plt.axis('tight')
+    ax = plt.gca()
+    lims = list(ax.get_ylim())
+    lims[0] = 0
+    ax.set_ylim(lims)
     plt.legend()
     fl.show();quit()
     data1.integrate('t2')
