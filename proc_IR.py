6f67a51170d97bc737004f32f31ecef071f82835
Samantha Betts <smbetts@syr.edu>
2021-06-22 11:22:47 -0400

    Added enhancement curve to proc_echo_mw_SMB.py

--- proc_IR.py
+++ proc_IR.py
@@ -8,10 +8,9 @@ save_npz = False
 #}}}
 coherence_pathway = {'ph1':0,'ph2':1}
 for thisfile,exp_type,nodename,postproc,f_range,t_range,clock_correction,IR,ILT in [
-<<<<<<< HEAD
-        ('210617_Y191R1a_pR_DDM_ODNP','odnp',
-            'FIR_36dBm','spincore_IR_v1',
-            (-0.5e3,0.5e3),(None,50e-3),True,False,False),
+        ('210617_T177R1a_pR_DDM_ODNP','odnp',
+            'FIR_0dBm','spincore_IR_v1',
+            (-0.4e3,0.4e3),(None,50e-3),True,False,False),
 #        ('210617_T177R1a_pR_DDM_ODNP','odnp',
 #            'FIR_36dBm','spincore_IR_v1',
 #            (-0.5e3,0.5e3),(None,50e-3),True,False,False),
--- proc_echo_mw_SMB.py
+++ proc_echo_mw_SMB.py
@@ -1,16 +1,16 @@
 from pyspecdata import *
 from pyspecProcScripts import *
 from pyspecProcScripts import postproc_dict
-from pyspecProcScripts.third_level.process_enhancement import process_enhancement
+#from pyspecProcScripts.third_level.process_enhancement import process_enhancement
 from sympy import symbols, Symbol, latex,limit,init_printing
 #from matplotlib import *
-#import numpy as np
-#import matplotlib.pyplot as plt
+from numpy import *
+import matplotlib.pyplot as plt
 #from pylab import *
 #from sympy import exp as s_exp
 #from scipy.optimize import leastsq,minimize,basinhopping,nnls
-import os
-os.chdir('C:/Users/saman/proc_scripts/')
+#import os
+#os.chdir('C:/Users/saman/proc_scripts/')
 #{{{input parameters
 plt.rcParams.update({
     "figure.facecolor":  (1.0, 1.0, 1.0, 0.0),  # clear
@@ -19,12 +19,10 @@ plt.rcParams.update({
 })
 logger = init_logging("info")
 t2 = symbols('t2')
-signal_pathway = {'ph1':1}
 fl = fl_mod()
 #}}}
 s = find_file('210617_T177R1a_pR_DDM_ODNP',exp_type='odnp',expno='enhancement',
         postproc='spincore_ODNP_v1',lookup=postproc_dict,fl=fl)
-print(s.get_prop('acq_params'));quit()
 fl.next('raw data')
 s = s['t2':(-1e3,1e3)]
 fl.next('sliced to integration bounds')
@@ -32,10 +30,29 @@ fl.image(s)
 ph0 = s['power',-4].sum('t2')
 ph0 /= abs(ph0)
 s /= ph0
+fl.next('phased')
 fl.image(s)
-fl.next('simple integral')
 s = s['t2':(-100,100)]
-s.integrate('t2')
-fl.plot(s,'o')
+d = s['ph1',1].C
+d.integrate('t2')
+d /= max(d.data)
+#{getting power axis
+power_axis_dBm = array(d.get_prop('meter_powers'))
+power_axis_W = zeros_like(power_axis_dBm)
+power_axis_W[:] = (1e-2*10**((power_axis_dBm[:]+10.)*1e-1))
+power_axis_W = r_[0,power_axis_W]
+d.setaxis('power',power_axis_W)
+#}
+fl.next('simple integral')
+fl.plot(d['power',:-3],'ko')
+fl.plot(d['power',-3:],'ro')
+plt.xlabel('power (W)')
+plt.ylabel('integrated 1HNMR signal')
+#{Enhancement
+E = 1 - d.C
+fl.next('enhancement curve')
+fl.plot(E['power',:-3],'ko')
+fl.plot(E['power',-3:],'ro')
+plt.xlabel('power (W)')
 fl.show()
-print(s.getaxis('power'))
+#}
