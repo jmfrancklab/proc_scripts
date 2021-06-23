06a2c806b00e1222ac85985a7d5a8427469a689c
Alex <guinneak198@github.com>
2021-06-23 15:29:34 -0400

    proc_Echo

--- proc_echo_mw.py
+++ proc_echo_mw.py
@@ -3,12 +3,6 @@ from pyspecProcScripts import *
 from pyspecProcScripts import postproc_dict
 from pyspecProcScripts.third_level.process_enhancement import process_enhancement
 from sympy import symbols, Symbol, latex,limit,init_printing
-#from matplotlib import *
-#import numpy as np
-#import matplotlib.pyplot as plt
-#from pylab import *
-#from sympy import exp as s_exp
-#from scipy.optimize import leastsq,minimize,basinhopping,nnls
 
 #{{{input parameters
 plt.rcParams.update({
@@ -19,20 +13,19 @@ plt.rcParams.update({
 logger = init_logging("info")
 t2 = symbols('t2')
 signal_pathway = {'ph1':1}
-freq_range= (-0.5e3,0.5e3)
+freq_range= (-1.5e3,1.5e3)
 t_range = (0,0.083)
 fl = fl_mod()
 #}}}
 for filename,nodename,file_location,postproc in [
-        ('210622_100mM_TEMPO_hexane_capillary_probe','enhancement_curve',
+        ('210610_3uM_TEMPOL_cap_probe_DNP','enhancement',
             'ODNP_NMR_comp/test_equipment','spincore_ODNP_v1'),
         ]:
     s = find_file(filename,exp_type=file_location,expno=nodename,
             postproc=postproc,lookup=postproc_dict,fl=fl)
-    #fl.show();quit()
     myslice = s['t2':freq_range]
     mysign = determine_sign(select_pathway(myslice,signal_pathway,mult_ph_dims=False))
-    enhancement,idx_maxpower = process_enhancement(s,searchstr = filename,
+    enhancement = process_enhancement(s,searchstr = filename,
             freq_range=freq_range, t_range=t_range,sign=mysign,fl=fl)
     fl.show()
 
