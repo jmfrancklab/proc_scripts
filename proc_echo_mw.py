5c817a02af596b82c1b7eef4f5c3e1c7fbdb8c52
Alex <guinneak198@github.com>
2021-06-23 16:10:07 -0400

    ignore

--- proc_echo_mw.py
+++ proc_echo_mw.py
@@ -22,16 +22,15 @@ signal_pathway = {'ph1':1}
 fl = fl_mod()
 #}}}
 for filename,nodename,file_location,postproc,freq_range,t_range in [
-        ('210617_Y191R1a_pR_DDM_ODNP','enhancement',
+        ('210622_100mM_TEMPO_hexane_capillary_probe','enhancement_curve',
             'odnp','spincore_ODNP_v1',
-            (-0.5e3,0.5e3),(0,50e-3)),
+            (-3e3,3e3),(0,83e-3)),
 #        ('210617_T177R1a_pR_DDM_ODNP','enhancement',
 #            'odnp','spincore_ODNP_v1',
 #            (-0.4e3,0.5e3),(0,45e-3)),
         ]:
     s = find_file(filename,exp_type=file_location,expno=nodename,
             postproc=postproc,lookup=postproc_dict,fl=fl)
-    print(ndshape(s));quit()
     #fl.show();quit()
     myslice = s['t2':freq_range]
     mysign = determine_sign(select_pathway(myslice,signal_pathway,mult_ph_dims=False))
