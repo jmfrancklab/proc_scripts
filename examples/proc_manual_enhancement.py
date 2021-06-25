859877e2bfb656646262ffeb12c55e826d48817a
John Franck (laptop) <jmfranck@syr.edu>
2021-06-25 17:15:47 -0400

    further cleanup

--- examples/proc_manual_enhancement.py
+++ examples/proc_manual_enhancement.py
@@ -1,16 +1,9 @@
 from pyspecdata import *
 from pyspecProcScripts import *
 from pyspecProcScripts import postproc_dict
-#from pyspecProcScripts.third_level.process_enhancement import process_enhancement
 from sympy import symbols, Symbol, latex,limit,init_printing
-#from matplotlib import *
 from numpy import *
 import matplotlib.pyplot as plt
-#from pylab import *
-#from sympy import exp as s_exp
-#from scipy.optimize import leastsq,minimize,basinhopping,nnls
-#import os
-#os.chdir('C:/Users/saman/proc_scripts/')
 #{{{input parameters
 plt.rcParams.update({
     "figure.facecolor":  (1.0, 1.0, 1.0, 0.0),  # clear
@@ -20,6 +13,8 @@ plt.rcParams.update({
 logger = init_logging("info")
 t2 = symbols('t2')
 fl = fl_mod()
+measured_vs_actual = 22. # how many dB down the split + measured power is from
+#                          the forward power
 #}}}
 
 s = find_file('210624_Y71R1a_Ras_ODNP',exp_type='ODNP_NMR_comp/ODNP', expno='enhancement',
@@ -44,18 +39,18 @@ s /= max(s.data.real)
 s.setaxis('power',r_[-9999,array(s.get_prop('meter_powers'))])
 print("here are the dBm",s.getaxis('power'))
 s.setaxis('power', lambda x:
-        1e-3*10**(x/10.))
+        1e-3*10**((x+measured_vs_actual)/10.))
 print("here are the powers",s.getaxis('power'))
 s.set_units('power','W')
 #}
 fl.next('simple integral')
-fl.plot(s['power',:-3],'ko')
-fl.plot(s['power',-3:],'ro')
+fl.plot(s['power',:-3],'ko', human_units=False) # human_units = False because the range for the red points tries to force mW, which is incompatible with W
+fl.plot(s['power',-3:],'ro', human_units=False)
 plt.ylabel('integrated $^1$H NMR signal')
 #{Enhancement
 s /= s['power',0]
 fl.next('enhancement curve')
-fl.plot(s['power',:-3],'ko')
-fl.plot(s['power',-3:],'ro')
+fl.plot(s['power',:-3],'ko', human_units=False)
+fl.plot(s['power',-3:],'ro', human_units=False)
 fl.show()
 #}
