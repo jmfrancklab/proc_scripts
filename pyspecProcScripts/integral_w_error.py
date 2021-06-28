d20143f7da4a288e668e930ed0deed7574b13700
Alex <guinneak198@github.com>
2021-06-28 14:56:26 -0400

    trying PR again

--- pyspecProcScripts/integral_w_error.py
+++ pyspecProcScripts/integral_w_error.py
@@ -29,6 +29,8 @@ def integral_w_errors(s,sig_path,error_path, indirect='vd', direct='t2',fl=None,
                 data with error associated with coherence pathways
                 not included in the signal pathway
     """
+    assert s.get_ft_prop(direct), "need to be in frequency domain!"
+    print("you made it this far once")
     frq_slice = integrate_limits(select_pathway(s,sig_path),fl=fl)
     logging.debug(strm('frq_slice is',frq_slice))
     s = s[direct:frq_slice]
@@ -41,25 +43,57 @@ def integral_w_errors(s,sig_path,error_path, indirect='vd', direct='t2',fl=None,
     if len(extra_dims) > 0:
      raise ValueError("You have extra (non-phase cycling, non-indirect) dimensions: "
              +str(extra_dims))
+
+
+
     collected_variance = ndshape(
          [ndshape(s)[indirect],len(error_path)],[indirect,'pathways']).alloc()
+
+    # this is not averaging over all the pathways!!!! -- addressed in issue #44 
+    avg_error = []
     for j in range(len(error_path)):
-     # calculate N₂ Δf² σ², which is the variance of the integral (by error propagation)
-     # where N₂ is the number of points in the indirect dimension
-     s_forerror = select_pathway(s,error_path[j])
-     if j==0: N2 = ndshape(s_forerror)[direct]
-     # mean divides by N₁ (indirect), integrate multiplies by Δf, and the
-     # mean sums all elements (there are N₁N₂ elements)
-     s_forerror -= s_forerror.C.mean_all_but([indirect, direct]).mean(direct)
-     s_forerror.run(lambda x: abs(x)**2/2).mean_all_but([direct,indirect]).mean(direct)
-     s_forerror *= df**2 # Δf
-     s_forerror *= N2
+        # calculate N₂ Δf² σ², which is the variance of the integral (by error propagation)
+        # where N₂ is the number of points in the indirect dimension
+        s_forerror = select_pathway(s,error_path[j])
+        # previous line wipes everything out and starts over -- why not use
+        # collected_variance above, as I had originally set up --> part of
+        # issue #44 
+        if j==0: N2 = ndshape(s_forerror)[direct]
+        # mean divides by N₁ (indirect), integrate multiplies by Δf, and the
+        # mean sums all elements (there are N₁N₂ elements)
+        s_forerror -= s_forerror.C.mean_all_but([indirect, direct]).mean(direct)
+        s_forerror.run(lambda x: abs(x)**2/2).mean_all_but([direct,indirect]).mean(direct)
+        s_forerror *= df**2 # Δf
+        s_forerror *= N2
+        avg_error.append(s_forerror)
     # {{{ variance calculation for debug
-    #print("(inside automatic routine) the stdev seems to be",sqrt(collected_variance/(df*N2)))
-    #print("automatically calculated integral error:",sqrt(collected_variance.data))
+    avg_error = sum(avg_error)/len(avg_error)
     # }}}
     s = select_pathway(s,sig_path)
+    retval = s.integrate(direct).set_error(sqrt(s_forerror.data))
     if not return_frq_slice:
-        return s.integrate(direct).set_error(sqrt(collected_variance.data))
+        return retval
     elif return_frq_slice:
-        return s.integrate(direct).set_error(sqrt(s_forerror.data)), frq_slice, sqrt(s_forerror.data)
+        return retval, frq_slice
+
+def active_propagation(s, signal_path, indirect='vd', direct='t2',fl=None):
+    assert s.get_ft_prop(direct), "need to be in frequency domain!"
+    frq_slice = integrate_limits(select_pathway(s,signal_path),fl=fl)
+    logging.debug(strm('frq_slice is',frq_slice))
+    s = s[direct:((frq_slice[-1]+50),None)]
+    f = s.getaxis(direct)
+    df = f[1]-f[0]
+    all_labels = set(s.dimlabels)
+    all_labels -= set([indirect,direct])
+    extra_dims = [j for j in all_labels if not j.startswith('ph')]
+    if len(extra_dims) > 0:
+     raise ValueError("You have extra (non-phase cycling, non-indirect) dimensions: "
+             +str(extra_dims))
+    s_forerror = select_pathway(s, signal_path)
+    N = ndshape(s_forerror)[direct]
+    s_forerror.run(lambda x: abs(x)**2/2).mean_all_but([direct,indirect]).mean(direct)
+    s_forerror *= df**2
+    s_forerror *= N
+    retval = sqrt(s_forerror.data)
+    return retval,N,df
+     
