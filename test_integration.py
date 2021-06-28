660ceef1683b56d03d530498fdee80434dc44a5d
Alex <guinneak198@github.com>
2021-06-28 14:14:15 -0400

    Testing integration errors

--- test_integration.py
+++ test_integration.py
@@ -2,19 +2,19 @@ from pyspecdata import *
 from pylab import *
 from matplotlib import *
 from pyspecProcScripts import *
-from pyspecProcScripts.correlation_alignment_ODNP import correl_align
+from pyspecProcScripts.correlation_alignment import correl_align
 import numpy as np
 fl = figlist_var()
 signal_pathway = {'ph1': 1, 'ph2':0}
+t_range=(0,0.05)
+f_range = (-0.11e3,0.12e3)
 excluded_pathways = [(0,0),(0,3)]
+colors = ['r','darkorange','gold','g','c','b','m','lightcoral']
 for thisfile,exp_type,nodename in [
-        ('210409_Ni_cap_probe_echo_1024','ODNP_NMR_comp/test_equipment','signal')
+        ('201113_TEMPOL_capillary_probe_16Scans_noModCoil','ODNP_NMR_comp/test_equipment','signal')
         ]:
+#{{{processing data    
     s = find_file(thisfile,exp_type=exp_type,expno=nodename)
-    nPoints = s.get_prop('acq_params')['nPoints']
-    nEchoes = s.get_prop('acq_params')['nEchoes']
-    nPhaseSteps = 8
-    SW_kHz =s.get_prop('acq_params')['SW_kHz']
     nScans = s.get_prop('acq_params')['nScans']
     s.reorder('t',first=True)
     s.chunk('t',['ph2','ph1','t2'],[2,4,-1])
@@ -24,20 +24,16 @@ for thisfile,exp_type,nodename in [
     s.setaxis('nScans',r_[0:nScans])
     s.set_units('t2','s')
     s.reorder('t2',first=False)
-    s.chunk('nScans',['repeats','nScans'],[32,-1])
     s.ft('t2',shift=True)
     s.ft(['ph1','ph2'])
-    s = s.mean('repeats')
     fl.next('raw data')
     fl.image(s)
     s.ift('t2')
     fl.next('raw data time domain')
-    t_range=(0,0.06)
-    f_range = (-1e3,1e3)
+    fl.image(s)
     s.ift(['ph1','ph2'])
-    rx_offset_corr = s['t2':(0.045,None)]
-    rx_offset_corr = rx_offset_corr.data.mean()
-    s -= rx_offset_corr
+    t_rx = (t_range[-1]/4)*3
+    s -= s['t2':(t_rx,None)].data.mean()  # DC offset correction
     s.ft('t2')
     s.ft(['ph1','ph2'])
     s = s['t2':f_range]
@@ -45,28 +41,16 @@ for thisfile,exp_type,nodename in [
     fl.image(s)
     s.ift('t2')
     best_shift,window_size = hermitian_function_test(select_pathway(s,signal_pathway))
-    print(best_shift)
-    #best_shift = 0.003
     s.setaxis('t2',lambda x: x-best_shift)
     s.register_axis({'t2':0})
     fl.next('After hermitian phase correction')
     s.ft('t2')
     fl.image(s)
     s.ift('t2')
-    s.ift(['ph1','ph2'])
-    phasing = s['t2',0].C
-    phasing.data *= 0
-    phasing.ft(['ph1','ph2'])
-    phasing['ph1',1]['ph2',0] = 1
-    phasing.ift(['ph1','ph2'])
-    s /= phasing
-    fl.next('ph, freq -- apply reciever phase')
-    fl.image(s)
-    ph0 = s['t2':0]/phasing
+    ph0 = select_pathway(s,signal_pathway)['t2':0]
     ph0 /= abs(ph0)
     logger.info(strm("there is only one dimension left -- standard 1D zeroth order phasing"))
     s /= ph0
-    s.ft(['ph1','ph2'])
     fl.next('after zeroth order phasing applied')
     s.ft('t2')
     fl.image(s)
@@ -92,86 +76,91 @@ for thisfile,exp_type,nodename in [
     #fl.image(s)
     ##}}}
     #s.ift('t2')
-    fl.next('time domain for slicing')
-    fl.image(s)
     s = s['t2':(0,t_range[-1])]
     s['t2':0] *= 0.5
     s.ft('t2')
     fl.next('FID sliced')
     fl.image(s)
     s.reorder(['ph1','ph2','nScans','t2'])
-    data = s.C    
+    #}}}
+    
+    #{{{Normalization
+    frq_slice = integrate_limits(select_pathway(s,signal_pathway),fl=fl)
+    d = s.C['t2':frq_slice]
+    d = select_pathway(d,signal_pathway)
+    d.integrate('t2')
+    avg_d = d.C.mean().item()
+    d /= avg_d
+    s /= avg_d
+    #}}}
+    
+    #{{{integral w errors
     error_pathway = (set(((j,k) for j in range(ndshape(s)['ph1']) for k in range(ndshape(s)['ph2'])))
             - set(excluded_pathways)
             - set([(signal_pathway['ph1'],signal_pathway['ph2'])]))
     error_pathway = [{'ph1':j,'ph2':k} for j,k in error_pathway]
-    s_int,frq_slice,std = integral_w_errors(s,signal_pathway,error_pathway,
-            indirect='nScans',fl=fl,return_frq_slice=True)
-    x = s_int.get_error()
-    x[:] /= 2
-    fl.next('error bars')
-    int_error = s_int.get_error()
-    fl.plot(int_error,'o',
-            label='returned error from integral_w_errors')
-    avg_s_int = int_error.mean().item()
-    s_int1 = avg_s_int
-    axhline(y=avg_s_int,c='red',linestyle=":",
-            label='averaged returned error from integral w errors')
-    data1 =data.C
-    data = select_pathway(data,signal_pathway)
-    data.integrate('t2')
-    data1 = data1['t2':frq_slice]
-    data2 = data1.C
-    data1=select_pathway(data1,signal_pathway)
-    data2=select_pathway(data2,{'ph1':0,'ph2':0})
-    data2.integrate('t2')
-    data1.integrate('t2')
-    after_slice_error = data1.real.run(np.std,'nScans')
-    off_CT_error = data2.real.run(np.std,'nScans')
-    axhline(y=float(after_slice_error.data),
-            c='k',linestyle=":",
-            label='error associated with CT pathway of signal')
-    axhline(y=float(off_CT_error.data),
-            c='blue',linestyle=":",
-            label='error associated with inactive CT pathway')
-    plt.ylim(0,None) 
-    fl.show();quit()
-    data1.integrate('t2')
-    data_on = select_pathway(data1,signal_pathway)
-    d1_err = data_on.real.run(np.std,'nScans')
-    axhline(y=d1_err,linestyle=":",
-            label='CT pathway[0]')
-    axhline(y=d1_err,linestyle=":",
-            label='CT pathway[-1]')    
-    data_off = select_pathway(data1,{'ph1':0,'ph2':0})
-    d2_err = data_off.real.run(np.std,'nScans')
-    axhline(y=d2_err,linestyle=":",
-            label='off-CT pathway[0]')
-    axhline(y=d1_err,linestyle=":",
-            label='CT pathway[-1]')
-    fl.show();quit()
-    fl.next('signal_pathway error vs off_pathway error')
-    data1 = select_pathway(data1,signal_pathway)
-    data1.set_error(d1_err.data)
-    fl.plot(data1,'o',capsize=6, label='CT pathway')
-    data1.set_error(d2_err.data)
-    fl.plot(data1,'o',capsize=6,label='off CT pathway')
-    #fl.show();quit()
-    fl.next('diagnostic 1D plot')
-    fl.plot(s['nScans',:]['ph1',signal_pathway['ph1']]['ph2',signal_pathway['ph2']].real,alpha=0.4)
-    axvline(x=frq_slice[0],c='k',linestyle=":",alpha=0.8)
-    axvline(x=frq_slice[-1],c='k',linestyle=":",alpha=0.8)
-    fl.next('integrated for error')
-    fl.plot(s_int,'.',capsize=6,label='integral with error')
-    #data = data['t2':frq_slice]
-    data = select_pathway(data,signal_pathway)
-    data.integrate('t2')
+    #{{{Making lists for all individual inactive pathways to get error
+    # associated with each one
+    s_int_lst = []
+    ph_lst = [[{'ph1':0,'ph2':0}],
+        [{'ph1':0,'ph2':1}],
+        [{'ph1':1,'ph2':1}],
+        [{'ph1':2,'ph2':0}],
+        [{'ph1':2,'ph2':1}],
+        [{'ph1':3,'ph2':0}],
+        [{'ph1':3,'ph2':1}]]
+    error_lst = []
+    avg_error_lst = []
+    for i in range(len(ph_lst)):
+        test = s.get_ft_prop('t2')
+        if s.get_ft_prop('t2') is False:
+            s.ft('t2')
+        s_int,frq_slice = integral_w_errors(s, signal_pathway, ph_lst[i],
+                    indirect='nScans', fl=fl, return_frq_slice=True)
+        error = s_int.get_error()
+        error[:] /= 2
+        avg_error = error.mean().item()
+        s_int_lst.append(s_int)
+        error_lst.append(error)
+        avg_error_lst.append(avg_error)
+    #}}}
 
-    new_error = data.real.run(np.std,'nScans')
-    #new_error = data.real.C.mean('nScans', std=True).get_error()
-    fl.plot(data,'o',label='data')
-    #print("new_error, data",ndshape(new_error), ndshape(data))
-    data.set_error(new_error.data)
-    fl.plot(data,'.',capsize=6,label='numpy std')
+    #{{{Calculating propagated error averaged over all inactive CTs
+    averaged_inactive, frq_slice = integral_w_errors(s, signal_pathway,
+            error_pathway, indirect='nScans', fl=fl, return_frq_slice=True)
+    averaged_inactive_error = averaged_inactive.get_error()
+    averaged_inactive_error[:] /= 2
+    avg_avg_error = averaged_inactive_error.mean().item()
+    #}}}
+
+    #{{{Calculating propagated error along active CT on noise slice
+    active_error,N,df = active_propagation(s, signal_pathway, indirect='nScans',fl=fl)
+    active_error[:] /= 2
+    avg_active_error = active_error.mean().item()
+    #}}}
+    #{{{Calculating the std dev -- error associated with the integrals
+    numpy_s_int = d.run(np.std,'nScans')
+   #}}} 
+    #{{{Plotting Errors
+    fl.next('comparison of std')
+    #for i in range(len(s_int_lst)):
+    #    fl.plot(error_lst[i],'o',color=colors[i],label = 'on excluded path of %s'%ph_lst[i])
+    #fl.plot(active_error,'x',
+    #        label='propagated error from active CT in noise slice')
+    #fl.plot(averaged_inactive_error,'o',color='brown',label='averaged propagated error from all inactive CTs')
+    #for i in range(len(s_int_lst)):
+    #    axhline(y=avg_error_lst[i], linestyle=":", color=colors[i],
+    #            label = "averaged %s"%ph_lst[i])
+    #axhline(y=avg_active_error,linestyle=":", label='averaged propagated error from active CT in noise slice')
+    #axhline(y=avg_avg_error,linestyle=":",color='brown',label='averaged average propagated error from inactive CTs')
+    axhline(y=numpy_s_int.data,c='k',
+            linestyle=":",label='std dev - error associated with the integrals')
+    #}}}
+    plt.axis('tight')
+    ax = plt.gca()
+    lims = list(ax.get_ylim())
+    lims[0] = 0
+    ax.set_ylim(lims)
+    plt.legend()
     fl.show();quit()
 
