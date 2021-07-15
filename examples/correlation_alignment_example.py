e3d1d67f749d1d7be2c70360631d1a7fa0482e62
John Franck (laptop) <jmfranck@syr.edu>
2021-07-15 11:21:20 -0400

    ran black

--- examples/correlation_alignment_example.py
+++ examples/correlation_alignment_example.py
@@ -55,50 +55,55 @@ with figlist_var() as fl:
         ),
     ]:
         fl.basename = "(%s)" % label
-        fig, ax_list = subplots(1, 4, figsize = (7,7))
+        fig, ax_list = subplots(1, 4, figsize=(7, 7))
         fig.suptitle(fl.basename)
         fl.next("Data Processing", fig=fig)
         data = fake_data(expression, OrderedDict(orderedDict), signal_pathway)
         data.reorder([indirect, "t2"], first=False)
         data.ft("t2")
         data /= sqrt(ndshape(data)["t2"]) * data.get_ft_prop("t2", "dt")
-        fl.image(data, ax = ax_list[0])
+        fl.image(data, ax=ax_list[0])
         ax_list[0].set_title("Raw Data")
         data = data["t2":f_range]
         data.ift("t2")
         rough_center = (
-                abs(select_pathway(data, signal_pathway))
-                .C.convolve("t2", 0.01)
-                .mean_all_but("t2")
-                .argmax("t2")
-                .item()
+            abs(select_pathway(data, signal_pathway))
+            .C.convolve("t2", 0.01)
+            .mean_all_but("t2")
+            .argmax("t2")
+            .item()
         )
         logger.info(strm("Rough center is:", rough_center))
-        data.setaxis("t2",lambda x: x - rough_center).register_axis({"t2": 0})
+        data.setaxis("t2", lambda x: x - rough_center).register_axis({"t2": 0})
         data.ft("t2")
         data.ift("t2")
         ph0 = select_pathway(data, signal_pathway)["t2":0]
         ph0 /= abs(ph0)
         data /= ph0
-        #{{{ Applying the phase corrections
+        # {{{ Applying the phase corrections
         best_shift, max_shift = hermitian_function_test(
-            select_pathway(data.C.mean(indirect), signal_pathway))
+            select_pathway(data.C.mean(indirect), signal_pathway)
+        )
         data.setaxis("t2", lambda x: x - best_shift).register_axis({"t2": 0})
-        fl.image(data,ax=ax_list[3])
+        fl.image(data, ax=ax_list[3])
         ax_list[3].set_title("Hermitian Test (t)")
         data.ft("t2")
         fl.image(data, ax=ax_list[2])
         ax_list[2].set_title("Hermitian Test (v)")
-        fig.tight_layout(rect=[0,0.03,1,0.95])
-        #}}}
-        #{{{ Applying Correlation Routine to Align Data
-        mysgn = select_pathway(data, signal_pathway).C.real.sum("t2").run(np.sign) # this is the sign of the signal -- note how on the next line, I pass sign-flipped data, so that we don't need to worry about messing with the original signal
+        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+        # }}}
+        # {{{ Applying Correlation Routine to Align Data
+        mysgn = (
+            select_pathway(data, signal_pathway).C.real.sum("t2").run(np.sign)
+        )  # this is the sign of the signal -- note how on the next line,
+        #    I pass sign-flipped data, so that we don't need to worry about
+        #    messing with the original signal
         opt_shift, sigma = correl_align(
-            data*mysgn, indirect_dim=indirect, signal_pathway=signal_pathway, sigma=50
+            data * mysgn, indirect_dim=indirect, signal_pathway=signal_pathway, sigma=50
         )
-        s.ift('t2')
-        s *= np.exp(-1j*2*pi*opt_shift*s.fromaxis('t2'))
-        s.ft('t2')
+        s.ift("t2")
+        s *= np.exp(-1j * 2 * pi * opt_shift * s.fromaxis("t2"))
+        s.ft("t2")
         data.ift("t2")
         for k, v in signal_pathway.items():
             data.ft([k])
@@ -110,4 +115,4 @@ with figlist_var() as fl:
         ax_list[3].set_title("Aligned Data (t)")
         fig.tight_layout(rect=[0, 0.03, 1, 0.95])
 
-        #}}} 
+        # }}}
