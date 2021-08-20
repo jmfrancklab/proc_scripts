cd0a1bd161d27003effe4fea69066482a8053b34
Alec Beaton (laptop) <aabeaton@syr.edu>
2021-08-20 14:56:25 -0400

    Make changes from PR, function for Lorentzian or Gaussian convolve only

--- pyspecProcScripts/integrate_limits.py
+++ pyspecProcScripts/integrate_limits.py
@@ -9,9 +9,7 @@ logger = init_logging("debug")
 
 def integrate_limits(s, axis="t2",
         filter_width=100,
-        Gaussian=False,
-        Lorentzian=True,
-        Lorentzian_to_Gaussian=False,
+        convolve_method='Gaussian',
         fl=None):
     r"""
     Integrate Limits
@@ -29,68 +27,52 @@ def integrate_limits(s, axis="t2",
         width of the matched filter is `fwhm` - this is calculated more precisely
         within the program
 
-    Gaussian: boolean
-        set `Gaussian` to True in order to calculate the matched-filter via
-        Gaussian convolution of the frequency domain signal (i.e., apply a matched
-        Gaussian filter to peak in frequency domain).
-        If `Gaussian` is set to True, set `Lorentzian` to False.
+    convolve_method: str
+        specify as one of the following 3 options:
+        * Option 1 - 'Gaussian' 
+        to calculate the matched-filter via
+        Gaussian convolution of the frequency
+        domain signal.
+        * Option 2 - 'Lorentzian' 
+        to calculate the matched-filter via
+        Lorentzian convolution of the frequency
+        domain signal.
+        * Option 3 - 'Lorentzian_to_Gaussian'
+        apply Lorentzian to Gaussian
+        transformation to your data, in order to
+        determine integral limits.
 
-    Lorentzian: boolean
-        set `Lorentzian` to True in order to calculate the matched-filter via
-        Lorentzian convolution of the frequency domain signal (i.e., apply a
-        matched Lorentzian filter to peak in frequency domain).
-        If `Lorentzian` is set to True, set `Gaussian` to False.
-
-
-    Lorentzian_to_Gaussian: boolean
-        set `Lorentzian_to_Gaussian` to True in order to apply this
-        transformation to your data, in order to determine integral limits.
-        Must set `Lorentzian` to True and `Gaussian` to False.
-        
     fl: None or figlist_var()
         to show diagnostic plots, set `fl` to the figure list; set `fl` to None in
         order not to see any diagnostic plots
 
     """
+    Lorentz_to_Gauss = False
     temp = s.C.mean_all_but(axis)
     if fl is not None:
         fl.next('integration diagnostic')
         fl.push_marker()
         fl.plot(abs(temp.C.ft('t2'))/abs(temp.C.ft('t2')).max(), alpha=0.6, label='before convolve')
-    Gaussian_Conv = Gaussian
-    Lorentzian_Conv = Lorentzian
-    Lorentz_to_Gauss = Lorentzian_to_Gaussian
-    #{{{ make sure to calculate Lorentzian filter for L-to-G transform
-    if Lorentz_to_Gauss:
-        print("Must set Lorentzian convolve to True for Lorentzian to Gaussian transformation... doing so now")
-        Lorentzian_Conv = True
-        Gaussian_Conv = False
-    #}}}
-    # Calculating matched filter
     sigma = nddata(np.linspace(1e-10,1e-1,1000),'sigma').set_units('sigma','s')
-    if Gaussian_Conv:
-        gaussians = np.exp(-temp.C.fromaxis(axis)**2/2/sigma**2)
-        signal_E = (abs(temp * gaussians)**2).sum(axis)
-    elif Lorentzian_Conv:
-        lorentzians = np.exp(-abs(temp.C.fromaxis(axis))/sigma)
-        signal_E = (abs(temp * lorentzians)**2).sum(axis)
+    if convolve_method == 'Gaussian':
+        convolution_set = np.exp(-temp.C.fromaxis(axis)**2/2/sigma**2)
+    elif convolve_method == 'Lorentzian':
+        convolution_set = np.exp(-abs(temp.C.fromaxis(axis))/sigma)
+    signal_E = (abs(temp * convolution_set)**2).sum(axis)
     signal_E /= signal_E.data.max()
-    if Gaussian_Conv:
+    if convolve_method == 'Gaussian':
         filter_width = abs(signal_E-1/sqrt(2)).argmin('sigma').item()
-    elif Lorentzian_Conv:
+    elif convolve_method == 'Lorentzian':
         filter_width = abs(signal_E-signal_E.max()/2).argmin('sigma').item()
+    logger.info(strm("FILTER WIDTH IS",filter_width))
     if fl is not None:
         fl.next('integration diagnostic -- signal Energy')
         fl.plot(signal_E, human_units=False)
         fl.plot(signal_E['sigma':(filter_width,filter_width+1e-6)],'o', human_units=False)
-    logger.info(strm("FILTER WIDTH IS",filter_width))
-    # Generate the appropriate convolution functions
-    Gaussian_func = lambda x,y: exp(-(x**2)/(2.0*(y**2)))
-    Lorentzian_func = lambda x,y: exp(-(abs(x))/y)
-    if Gaussian_Conv:
-        convfunc = Gaussian_func
-    elif Lorentzian_Conv:
-        convfunc = Lorentzian_func
+    if convolve_method == 'Gaussian':
+        temp *= np.exp(-temp.C.fromaxis(axis)**2/2/filter_width**2)
+    temp.ft('t2')
+
     if Lorentz_to_Gauss:
         # not immediately sure why, but I need to
         # do this in order to get freq limits
@@ -99,7 +81,6 @@ def integrate_limits(s, axis="t2",
         rough_center = abs(temp).C.mean_all_but('t2').argmax('t2').item()
         temp.setaxis('t2', lambda t: t- rough_center).register_axis({'t2':0})
         temp = temp['t2':(7e-3,None)]
-    logger.info(strm("I want this filter_width",filter_width))
     if Lorentz_to_Gauss:
         # filter_width is λ/π which is FWHM for Lorentzian in Hz
         # filter for L-to-G lies between λ/2 and λ*2 (Cav p.116)
@@ -110,9 +91,6 @@ def integrate_limits(s, axis="t2",
         temp.convolve('t2', 1/this_filter, convfunc=Gaussian_func)
         temp.ift('t2')
         temp /= Lorentzian_func(temp.C.fromaxis('t2'),filter_width)
-        temp.ft('t2')
-    else:
-        temp *= np.exp(-temp.C.fromaxis(axis)**2/2/filter_width**2)
         temp.ft('t2')
     if fl is not None:
         fl.next('integration diagnostic')
