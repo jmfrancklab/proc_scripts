7093f2966860999c0f14dd0f8ab841531140de68
John Franck (laptop) <jmfranck@syr.edu>
2021-09-05 18:13:55 -0400

    run black on correct file and remove ipynb directory

--- pyspecProcScripts/phasing.py
+++ pyspecProcScripts/phasing.py
@@ -2,12 +2,14 @@
 from pyspecdata import *
 from matplotlib.patches import Ellipse
 from scipy.optimize import minimize
-from pylab import xlim,subplots,axvline,ylim
+from pylab import xlim, subplots, axvline, ylim
 import numpy as np
 from scipy import linalg
 import logging
+
+
 def zeroth_order_ph(d, fl=None):
-    r'''determine the covariance of the datapoints
+    r"""determine the covariance of the datapoints
     in complex plane, and use to phase the
     zeroth-order even if the data is both negative
     and positive.
@@ -31,24 +33,23 @@ def zeroth_order_ph(d, fl=None):
         complex number with magnitude 1.
         To correct the zeroth order phase of the data,
         divide by ``retval``.
-    '''
-    cov_mat = np.cov(c_[
-        d.data.real.ravel(),
-        d.data.imag.ravel()].T,
-        aweights=abs(d.data).ravel()**2 # when running proc_square_refl, having
+    """
+    cov_mat = np.cov(
+        c_[d.data.real.ravel(), d.data.imag.ravel()].T,
+        aweights=abs(d.data).ravel() ** 2  # when running proc_square_refl, having
         # this line dramatically reduces the size of the imaginary component
         # during the "blips," and the magnitude squared seems to perform
         # slightly better than the magnitude -- this should be both a robust
         # test and a resolution for issue #23
-        )
+    )
     eigenValues, eigenVectors = linalg.eigh(cov_mat)
     mean_point = d.data.ravel().mean()
-    mean_vec = r_[mean_point.real,mean_point.imag]
+    mean_vec = r_[mean_point.real, mean_point.imag]
     # next 3 lines from stackexchange -- sort by
     # eigenvalue
-    idx = eigenValues.argsort()[::-1]   
+    idx = eigenValues.argsort()[::-1]
     eigenValues = eigenValues[idx]
-    eigenVectors = eigenVectors[:,idx] # first dimension x,y second evec #
+    eigenVectors = eigenVectors[:, idx]  # first dimension x,y second evec #
     # determine the phase angle from direction of the
     # largest principle axis plus the mean
     # the vector in the direction of the largest
@@ -57,57 +58,76 @@ def zeroth_order_ph(d, fl=None):
     # that we only want to rotate when the distribution
     # is assymetric, so include only the excess of the
     # larger eval over the smaller
-    assymetry_mag = float(sqrt(eigenValues[0])-sqrt(eigenValues[1]))
+    assymetry_mag = float(sqrt(eigenValues[0]) - sqrt(eigenValues[1]))
     try:
-        assym_ineq = (assymetry_mag*eigenVectors[:,0]*mean_vec).sum()
+        assym_ineq = (assymetry_mag * eigenVectors[:, 0] * mean_vec).sum()
     except:
-        raise ValueError(strm("check the sizes of the following:",size(assymetry_mag),size(eigenVectors),size(mean_vec)))
+        raise ValueError(
+            strm(
+                "check the sizes of the following:",
+                size(assymetry_mag),
+                size(eigenVectors),
+                size(mean_vec),
+            )
+        )
     if assym_ineq > 0:
         # we want the eigenvector on the far side of the ellipse
-        rotation_vector = mean_vec + assymetry_mag*eigenVectors[:,0]
+        rotation_vector = mean_vec + assymetry_mag * eigenVectors[:, 0]
     else:
-        rotation_vector = mean_vec - assymetry_mag*eigenVectors[:,0]
-    ph0 = np.arctan2(rotation_vector[1],rotation_vector[0])
+        rotation_vector = mean_vec - assymetry_mag * eigenVectors[:, 0]
+    ph0 = np.arctan2(rotation_vector[1], rotation_vector[0])
     if fl is not None:
         d_forplot = d.C
-        fl.next('check covariance test')
+        fl.next("check covariance test")
         fl.plot(
-                d_forplot.data.ravel().real,
-                d_forplot.data.ravel().imag,
-                '.',
-                alpha=0.25,
-                label='before'
-                )
-        d_forplot /= np.exp(1j*ph0)
+            d_forplot.data.ravel().real,
+            d_forplot.data.ravel().imag,
+            ".",
+            alpha=0.25,
+            label="before",
+        )
+        d_forplot /= np.exp(1j * ph0)
         fl.plot(
-                d_forplot.data.ravel().real,
-                d_forplot.data.ravel().imag,
-                '.',
-                alpha=0.25,
-                label='after'
-                )
-        fl.plot(0,0,'ko', alpha=0.5)
-        fl.plot(mean_vec[0],mean_vec[1],'kx', label='mean', alpha=0.5)
-        evec_forplot = sqrt(eigenValues.reshape(1,2))*np.ones((2,1))*eigenVectors # scale by the std, not the variance!
-        evec_forplot += mean_vec.reshape((-1,1))*np.ones((1,2))
-        fl.plot(evec_forplot[0,0],evec_forplot[1,0],'o', alpha=0.5,
-                label='first evec')
-        fl.plot(evec_forplot[0,1],evec_forplot[1,1],'o', alpha=0.5)
-        fl.plot(rotation_vector[0],rotation_vector[1],'o', alpha=0.5,
-                label='rotation vector')
-        norms = sqrt((evec_forplot**2).sum(axis=0))
-        ell = Ellipse(xy=mean_vec,
-                width=2*sqrt(eigenValues[0]),
-                height=2*sqrt(eigenValues[1]),
-                angle=180/pi*np.arctan2(eigenVectors[1,0],
-                    eigenVectors[0,0]),
-                color='k', fill=False)
+            d_forplot.data.ravel().real,
+            d_forplot.data.ravel().imag,
+            ".",
+            alpha=0.25,
+            label="after",
+        )
+        fl.plot(0, 0, "ko", alpha=0.5)
+        fl.plot(mean_vec[0], mean_vec[1], "kx", label="mean", alpha=0.5)
+        evec_forplot = (
+            sqrt(eigenValues.reshape(1, 2)) * np.ones((2, 1)) * eigenVectors
+        )  # scale by the std, not the variance!
+        evec_forplot += mean_vec.reshape((-1, 1)) * np.ones((1, 2))
+        fl.plot(
+            evec_forplot[0, 0], evec_forplot[1, 0], "o", alpha=0.5, label="first evec"
+        )
+        fl.plot(evec_forplot[0, 1], evec_forplot[1, 1], "o", alpha=0.5)
+        fl.plot(
+            rotation_vector[0],
+            rotation_vector[1],
+            "o",
+            alpha=0.5,
+            label="rotation vector",
+        )
+        norms = sqrt((evec_forplot ** 2).sum(axis=0))
+        ell = Ellipse(
+            xy=mean_vec,
+            width=2 * sqrt(eigenValues[0]),
+            height=2 * sqrt(eigenValues[1]),
+            angle=180 / pi * np.arctan2(eigenVectors[1, 0], eigenVectors[0, 0]),
+            color="k",
+            fill=False,
+        )
         ax = plt.gca()
-        ax.set_aspect('equal', adjustable='box')
+        ax.set_aspect("equal", adjustable="box")
         ax.add_patch(ell)
-    return np.exp(1j*ph0)
-def ph1_real_Abs(s,dw,ph1_sel=0,ph2_sel=1,fl = None):
-    r''' Performs first order phase correction with cost function
+    return np.exp(1j * ph0)
+
+
+def ph1_real_Abs(s, dw, ph1_sel=0, ph2_sel=1, fl=None):
+    r"""Performs first order phase correction with cost function
     by taking the sum of the absolute value of the real [DeBrouwer2009].
 
     .. todo::
@@ -119,7 +139,7 @@ def ph1_real_Abs(s,dw,ph1_sel=0,ph2_sel=1,fl = None):
         Complex data whose first order phase you want
         to find.
 
-    Returns    
+    Returns
     =========
     retval
 
@@ -129,49 +149,52 @@ def ph1_real_Abs(s,dw,ph1_sel=0,ph2_sel=1,fl = None):
         automated phase correction of NMR spectra. Journal of Magnetic
         Resonance (San Diego, Calif. : 1997), 201(2), 230–238.
         https://doi.org/10.1016/j.jmr.2009.09.017
-    '''
-    if fl: fl.push_marker() 
-    ph1 = nddata(r_[-5:5:70j]*dw,'phcorr')
-    dx = np.diff(ph1.getaxis('phcorr')[r_[0,1]]).item()
-    ph1 = np.exp(-1j*2*pi*ph1*s.fromaxis('t2'))
+    """
+    if fl:
+        fl.push_marker()
+    ph1 = nddata(r_[-5:5:70j] * dw, "phcorr")
+    dx = np.diff(ph1.getaxis("phcorr")[r_[0, 1]]).item()
+    ph1 = np.exp(-1j * 2 * pi * ph1 * s.fromaxis("t2"))
     s_cost = s * ph1
-    ph0 = s_cost.C.sum('t2')
+    ph0 = s_cost.C.sum("t2")
     ph0 /= abs(ph0)
     s_cost /= ph0
     if fl:
-        fl.next('phasing cost function')
-    s_cost.run(np.real).run(abs).sum('t2')
-    s_cost = s_cost['ph2',1]['ph1',0]
+        fl.next("phasing cost function")
+    s_cost.run(np.real).run(abs).sum("t2")
+    s_cost = s_cost["ph2", 1]["ph1", 0]
     if fl:
-        fl.plot(s_cost,'.')
-    s_cost.smoosh(['vd','phcorr'],'transients')
-    ph1_opt = np.asarray(s_cost.argmin('transients').item())
-    logging.info(strm("THIS IS PH1_OPT",ph1_opt))
-    logging.info(strm('optimal phase correction',repr(ph1_opt)))
+        fl.plot(s_cost, ".")
+    s_cost.smoosh(["vd", "phcorr"], "transients")
+    ph1_opt = np.asarray(s_cost.argmin("transients").item())
+    logging.info(strm("THIS IS PH1_OPT", ph1_opt))
+    logging.info(strm("optimal phase correction", repr(ph1_opt)))
     # }}}
     # {{{ apply the phase corrections
-    def applyphase(arg,ph1):
-        arg *= np.exp(-1j*2*pi*ph1*arg.fromaxis('t2'))
-        ph0 = arg.C.sum('t2')
+    def applyphase(arg, ph1):
+        arg *= np.exp(-1j * 2 * pi * ph1 * arg.fromaxis("t2"))
+        ph0 = arg.C.sum("t2")
         ph0 /= abs(ph0)
         arg /= ph0
         return arg
+
     def costfun(ph1):
-        logging.info(strm("PH1 IS THIS:",ph1))
+        logging.info(strm("PH1 IS THIS:", ph1))
         if type(ph1) is np.ndarray:
             ph1 = ph1.item()
         temp = s.C
-        retval = applyphase(temp,ph1).run(np.real).run(abs).sum('t2').item()
+        retval = applyphase(temp, ph1).run(np.real).run(abs).sum("t2").item()
         return retval
-    r = minimize(costfun,
-            ph1_opt,
-            bounds = [(ph1_opt-dx,ph1_opt+dx)])
+
+    r = minimize(costfun, ph1_opt, bounds=[(ph1_opt - dx, ph1_opt + dx)])
     assert r.success
-    s = applyphase(s,r.x.item())
-    fl.plot(r.x,r.fun,'x')
+    s = applyphase(s, r.x.item())
+    fl.plot(r.x, r.fun, "x")
     fl.pop_marker()
     return s
-    #}}}
+    # }}}
+
+
 def hermitian_function_test(
     s,
     direct="t2",
@@ -221,13 +244,13 @@ def hermitian_function_test(
     s = s[direct:frq_range]
     s.ift(direct, pad=2048 * 8)
     if fl is not None:
-        plt.rcParams["figure.figsize"] = [7.5,3.5]
-        plt.rcParams["figure.autolayout"]=True
+        plt.rcParams["figure.figsize"] = [7.5, 3.5]
+        plt.rcParams["figure.autolayout"] = True
         fig, ax_list = subplots(2, 3, figsize=(15, 15))
         fl.next("Hermitian Function Test Diagnostics", fig=fig)
         s = s[direct:(ini_delay, None)]
-        fl.plot(abs(s), ".", ax=ax_list[0,0])
-        ax_list[0,0].set_title("Data with Padding")
+        fl.plot(abs(s), ".", ax=ax_list[0, 0])
+        ax_list[0, 0].set_title("Data with Padding")
     selection = s[direct:selection_range]
     N = ndshape(selection)[direct]
     mid_idx = N // 2 + N % 2 - 1
@@ -255,16 +278,16 @@ def hermitian_function_test(
     # }}}
     if fl is not None:
         if "power" in s.dimlabels:
-            fl.image(residual["power", -4], ax=ax_list[0,1])
+            fl.image(residual["power", -4], ax=ax_list[0, 1])
         else:
-            fl.image(residual, ax=ax_list[0,1])
-        ax_list[0,1].set_title("shifted and phased")
+            fl.image(residual, ax=ax_list[0, 1])
+        ax_list[0, 1].set_title("shifted and phased")
     residual = abs(residual - residual[direct, ::-1].runcopy(np.conj)).mean_all_but(
         ["shift", direct]
     )
     if fl is not None:
-        fl.image(residual, ax=ax_list[0,2])
-        ax_list[0,2].set_title("Residual 2D")
+        fl.image(residual, ax=ax_list[0, 2])
+        ax_list[0, 2].set_title("Residual 2D")
     if band_mask:
         n_mask_pts = int(band_mask / dt)
         if n_mask_pts % 2:
@@ -291,20 +314,20 @@ def hermitian_function_test(
         mask.setaxis(direct, residual.getaxis(direct))
         mask.setaxis("shift", residual.getaxis("shift"))
         if fl is not None:
-            fl.image(mask, ax=ax_list[1,0], human_units=False)
-            ax_list[1,0].set_title("Mask")
+            fl.image(mask, ax=ax_list[1, 0], human_units=False)
+            ax_list[1, 0].set_title("Mask")
         residual *= mask
         title_str = "triangular mask"
     if fl is not None:
-        fl.image(residual, ax=ax_list[1,1])
-        ax_list[1,1].set_title("Masked Residual")
+        fl.image(residual, ax=ax_list[1, 1])
+        ax_list[1, 1].set_title("Masked Residual")
     residual.rename("shift", "center").setaxis(
         "center", dt * (mid_idx - r_[0:mid_idx]) + ini_delay
     )
     residual = residual["center", ::-1]
     if fl is not None:
-        fl.image(residual, ax=ax_list[1,2])
-        ax_list[1,2].set_title("Masked Residual -- Relabeled")
+        fl.image(residual, ax=ax_list[1, 2])
+        ax_list[1, 2].set_title("Masked Residual -- Relabeled")
         xlim(0, 15)
         fig.tight_layout()
     residual.mean(direct)
