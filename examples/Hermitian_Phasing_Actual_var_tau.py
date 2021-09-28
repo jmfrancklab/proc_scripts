026efabc99cad1c1addde7f19de8a3981327a78a
Alex <guinneak198@github.com>
2021-09-28 16:41:23 -0400

    var_tau guy takes forever

--- examples/Hermitian_Phasing_Actual_var_tau.py
+++ examples/Hermitian_Phasing_Actual_var_tau.py
@@ -28,48 +28,32 @@ t2, td, vd, power, ph1, ph2 = s.symbols("t2 td vd power ph1 ph2")
 f_range = (-200, 200)
 filename = '201113_TEMPOL_capillary_probe_var_tau_1'
 signal_pathway = {'ph1':1,'ph2':0}
+est_tau = []
+diff_tau = []
 with figlist_var() as fl:
     for nodename,file_location,postproc,label in [
         ('var_tau','ODNP_NMR_comp/var_tau','spincore_var_tau_v1',
             'tau is 1 ms'),
-        #('tau_3500','ODNP_NMR_comp/Echoes','spincore_echo_v1',
-        #    'tau is 3.5 ms'),
-        #('tau_11135','ODNP_NMR_comp/Echoes','spincore_echo_v1',
-        #    'tau is 11.135 ms')
             ]:
         data = find_file(filename,exp_type=file_location,expno=nodename,
-                postproc=postproc,lookup=lookup_table,fl=fl)
+                postproc=postproc,lookup=lookup_table)
         tau_list = list(data.getaxis('tau'))
-        logger.info(strm("programmed tau:",tau_list[7]))
-        programmed_tau = tau_list[7]
-        data = data['tau',7]
-        fl.basename = "(%s)" %programmed_tau
-        fig, ax_list = subplots(1, 3, figsize = (7,7))
-        fig.suptitle(fl.basename)
-        data.reorder(['ph1','ph2','t2'])
-        fl.next("Data processing", fig=fig)
-        fl.image(data['t2':(-1e3,1e3)], ax=ax_list[0])
-        ax_list[0].set_title("Raw Data")
+        data.reorder(['ph1','ph2','tau','t2'])
         data = data['t2':f_range]
-        data.ift("t2")
-        logger.debug(strm("shape before extend",ndshape(data)))
-        data.extend("t2", tau_list[-1]*2)
-        logger.debug(strm("extending to", tau_list[-1]*2))
-        logger.debug(strm("shape after extend",ndshape(data)))
-        best_shift = hermitian_function_test(
-            select_pathway(data.C, signal_pathway),
-            aliasing_slop=3,
-            fl=fl
-        )
-        print("best shift is:",best_shift)
-        data.setaxis("t2", lambda x: x - best_shift).register_axis({"t2": 0})
-        data /= zeroth_order_ph(select_pathway(data,signal_pathway), fl=fl)
-        data.ft('t2')
-        fl.image(data['t2':(-1e3,1e3)], ax=ax_list[1])
-        ax_list[1].set_title("Phased and centered (ν)")
-        data.ift("t2")
-        fl.image(data, ax=ax_list[2], human_units=False)
-        ax_list[2].set_title("Phased and Centered (t)")
-        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-        print("difference between programmed and estimated",(best_shift-programmed_tau)*1e6)
+        for j in range(len(tau_list)):
+            programmed_tau = tau_list[j]
+            logger.info(strm("programmed tau:",programmed_tau))
+            tau_data = data['tau',j]
+            this_data = tau_data.C
+            this_data.ift("t2")
+            if programmed_tau > 0.04:
+                print("extending data")
+                this_data.extend("t2", tau_list[-1]*2)
+            best_shift = hermitian_function_test(
+                select_pathway(this_data.C, signal_pathway),
+                aliasing_slop=3)
+            print("best shift is:",best_shift)
+            est_tau.append(best_shift)
+            diff = abs(best_shift - programmed_tau)
+            diff_tau.append(diff)
 
