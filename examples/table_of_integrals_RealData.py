f73500dd283556396b886b7bb19f9468ce2d94c6
John Franck (laptop) <jmfranck@syr.edu>
2022-01-10 10:45:20 -0500

    JF review

--- examples/table_of_integrals_RealData.py
+++ examples/table_of_integrals_RealData.py
@@ -14,29 +14,59 @@ from pyspecProcScripts import *
 from numpy.random import normal, seed
 import sympy as s
 from collections import OrderedDict
-rcParams['image.aspect'] = 'auto' # needed for sphinx gallery
+
+rcParams["image.aspect"] = "auto"  # needed for sphinx gallery
 
 # sphinx_gallery_thumbnail_number = 6
 
 init_logging(level="debug")
 fl = fl_mod()
-t2, td, vd, power, ph1, ph2 = s.symbols('t2 td vd power ph1 ph2')
-signal_pathway = {'ph1':1,'ph2':0}
-t_range= (0,85e-3)
+t2, td, vd, power, ph1, ph2 = s.symbols("t2 td vd power ph1 ph2")
+signal_pathway = {"ph1": 1, "ph2": 0}
+t_range = (0, 85e-3)
 
 with figlist_var() as fl:
-    for filename, exp_type, nodename, postproc, indirect, clock_correction, label, f_range in [
-            ('201113_TEMPOL_capillary_probe_DNP_1','ODNP_NMR_comp/old', 'signal',
-                'spincore_ODNP_v2','power',False,'no power', 
-                (-0.5e3,0.5e3))
-            ]:
-        fl.basename = "(%s)"%label
-        s = find_file(filename, exp_type=exp_type, expno=nodename,
-                postproc=postproc,lookup=lookup_table)
-        myslice = s['t2':f_range]
-        mysgn = determine_sign(select_pathway(myslice,signal_pathway))
-        s_int, s = peak_intensities(s,signal_pathway=signal_pathway,
-                searchstr = label, f_range=f_range, t_range=t_range,
-                sgn = mysgn,indirect='power', Real =True, alias_slop=3,
-                clock_correction=clock_correction,
-                fl=fl)
+    for (
+        filename,
+        exp_type,
+        nodename,
+        postproc,
+        indirect,
+        clock_correction,
+        label,
+        f_range,
+    ) in [
+        (
+            "201113_TEMPOL_capillary_probe_DNP_1",
+            "ODNP_NMR_comp/old",
+            "signal",
+            "spincore_ODNP_v2",
+            "power",
+            False,
+            "no power",
+            (-0.5e3, 0.5e3),
+        )
+    ]:
+        fl.basename = "(%s)" % label
+        s = find_file(
+            filename,
+            exp_type=exp_type,
+            expno=nodename,
+            postproc=postproc,
+            lookup=lookup_table,
+        )
+        myslice = s["t2":f_range]
+        mysgn = determine_sign(select_pathway(myslice, signal_pathway))
+        s_int, s = peak_intensities(
+            s,
+            signal_pathway=signal_pathway,
+            searchstr=label,
+            f_range=f_range,
+            t_range=t_range,
+            sgn=mysgn,
+            indirect="power",
+            Real=True,
+            alias_slop=3,
+            clock_correction=clock_correction,
+            fl=fl,
+        )
