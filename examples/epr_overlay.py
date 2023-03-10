c968c58bab6b9559b84a60c8e1a2f7d1e9c58d4a
John M. Franck (SU laptop) <jmfranck@syr.edu>
2023-03-09 19:44:52 -0500

    done at CTB

--- examples/epr_overlay.py
+++ examples/epr_overlay.py
@@ -1,4 +1,4 @@
-"""
+r"""
 EPR correlation alignment
 =========================
 
@@ -9,20 +9,33 @@ spectra, and small imperfections -- such as free MTSL
 -- can play an outsized role.
 
 Therefore, here, we use correlation to align the
-spectra and then use dot-product scaling to normalize
-them.  Note that dot-product scaling gives the optimal
-solution that minimizes the following expression by varying the
+spectra and then use "dot-product scaling" to normalize
+them.  By "dot-product scaling" we mean scaling the
+amplitude of one vector (here a spectrum,
+:math:`\mathbf{b}`) relative to a reference (here
+:math:`\mathbf{c}` to minimize the residual between the
+two -- *i.e.* minimizing the following expression by varying the
 scaling constant :math:`c`:
 :math:`|\mathbf{a}-c\mathbf{b}|^2`
+The solution to this is
+:math:`c = \frac{\Re[\mathbf{a}\cdot \mathbf{b}]}{||\mathbf{b}||^2}`
 
 In order to do all this, we need a common *x*-axis that
 we can use for correlation, etc.
 Here, we use the first spectrum in order to determine
 the *x*-axis.
+Also, for the purposes of dot-product scaling, 
+it is better to scale the less noisy spectrum
+(:math:`\mathbf{b}` above) 
+relative to the noisier spectrum
+(:math:`\mathbf{a}` above),
+so we assume that the first spectrum is the least
+noisy.
 """
 from pylab import *
 from pyspecdata import *
 from pyspecProcScripts import QESR_scalefactor
+from collections import OrderedDict
 import matplotlib as mpl
 import pickle
 init_logging(level='debug')
@@ -33,6 +46,24 @@ filenames_w_labels =  [
         ('220307_prS175_KH2PO4.DSC','220307_S175_KH2PO4'), 
         ]
 Bname = "$B_0$"
+all_files = OrderedDict()
+# {{{ load all files first, so that we can determine
+# the axis we will want to interpolate onto
+for j, (filename, label_str) in enumerate(filenames_w_labels):
+    # {{{ load, rescale
+    d = find_file(filename, exp_type="francklab_esr/Farhana")
+    print(d.name()); quit()
+    d /= QESR_scalefactor(d)
+    if "harmonic" in d.dimlabels:
+        d = d["harmonic", 0]
+    d -= d[Bname, :50].C.mean(Bname).data
+    # }}}
+B_start, B_stop = (
+    3400,
+    3650,
+)  # assume this is a good range to cover any amount of Hall sensor drift
+ref_axis = r_[B_start:B_stop:4096j]
+# }}}
 
 with figlist_var(width=0.7, filename="ESR_align_example.pdf") as fl:
     # {{{ arrange the figures in the PDF
@@ -43,14 +74,7 @@ with figlist_var(width=0.7, filename="ESR_align_example.pdf") as fl:
     fl.par_break()
     fl.next("aligned, autoscaled", legend=True)
     # }}}
-    for j, (filename, label_str) in enumerate(filenames_w_labels):
-        # {{{ load, rescale
-        d = find_file(filename, exp_type="francklab_esr/Farhana")
-        d /= QESR_scalefactor(d)
-        if "harmonic" in d.dimlabels:
-            d = d["harmonic", 0]
-        d -= d[Bname, :50].C.mean(Bname).data
-        # }}}
+    for label_str, d in all_files:
         # {{{ just show the raw data
         fl.next("Raw")
         fl.plot(d, label=label_str, alpha=0.5)
@@ -58,11 +82,6 @@ with figlist_var(width=0.7, filename="ESR_align_example.pdf") as fl:
         if j == 0:
             # if the reference spectrum, store its range
             B_slice = d.getaxis(Bname)[r_[0, -1]]
-        B_start, B_stop = (
-            3400,
-            3650,
-        )  # assume this is a good range to cover any amount of Hall sensor drift
-        ref_axis = r_[B_start:B_stop:4096j]
         d = d.interp(Bname, ref_axis.copy(), kind="linear")
         d[Bname] -= B_start
         if j == 0:
