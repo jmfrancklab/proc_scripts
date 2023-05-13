2f469048577519b9924b761e4d94a3fbd2e32cb6
John M. Franck (SU laptop) <jmfranck@syr.edu>
2023-05-13 17:37:09 -0400

    tweak documentation and make scaling code more general

--- examples/epr_overlay.py
+++ examples/epr_overlay.py
@@ -2,7 +2,7 @@ r"""
 EPR correlation alignment
 =========================
 
-while we can align by microwave frequency and normalize
+While we can align by microwave frequency and normalize
 according to peak-to-peak amplitude, it scan still be
 hard to identify subtle differences between ESR
 spectra, and small imperfections -- such as free MTSL
@@ -13,24 +13,34 @@ spectra and then use "dot-product scaling" to normalize
 them.  By "dot-product scaling" we mean scaling the
 amplitude of one vector (here a spectrum,
 :math:`\mathbf{b}`) relative to a reference (here
-:math:`\mathbf{c}` to minimize the residual between the
-two -- *i.e.* minimizing the following expression by varying the
-scaling constant :math:`c`:
-:math:`|\mathbf{a}-c\mathbf{b}|^2`
+:math:`\mathbf{a}`) to minimize the residual between the
+two -- *i.e.* we minimize the expression
+
+.. math::
+
+    |\mathbf{a}-c\mathbf{b}|^2
+
+by varying the
+scaling constant :math:`c`.
 The solution to this is
-:math:`c = \frac{\Re[\mathbf{a}\cdot \mathbf{b}]}{||\mathbf{b}||^2}`
+
+.. math::
+
+    c = \frac{\Re[\mathbf{a}\cdot \mathbf{b}]}{||\mathbf{b}||^2}
 
 In order to do all this, we need a common *x*-axis that
 we can use for correlation, etc.
-Here, we use the first spectrum in order to determine
-the *x*-axis.
+Here, we look for the fields that are furthest left and furthest right,
+and for the smallest spacing between field samples
+-- we use these values to construct a (therefore all-inclusive) x axis.
+
 Also, for the purposes of dot-product scaling, 
 it is better to scale the less noisy spectrum
 (:math:`\mathbf{b}` above) 
 relative to the noisier spectrum
-(:math:`\mathbf{a}` above),
-so we assume that the first spectrum is the least
-noisy.
+(:math:`\mathbf{a}` above)
+-- *i.e.* above, we want :math:`\mathbf{b}` to be less noisy.
+Here, we assume that smaller spectra are noisier.
 """
 from pylab import *
 from pyspecdata import *
@@ -47,6 +57,7 @@ filenames_w_labels =  [
         ]
 Bname = "$B_0$"
 all_files = OrderedDict()
+all_scalings = dict()
 # {{{ load all files first, so that we can determine
 # the axis we will want to interpolate onto
 for j, (filename, label_str) in enumerate(filenames_w_labels):
@@ -67,6 +78,7 @@ for j, (filename, label_str) in enumerate(filenames_w_labels):
         temp = diff(d.getaxis(Bname)[r_[0,1]]).item()
         dB = temp if temp<dB else dB
     all_files[label_str] = d
+    all_scalings[label_str] = d.data.max()-d.data.min()
     # }}}
 ref_axis = r_[minB:maxB+dB:dB]
 # }}}
@@ -111,8 +123,10 @@ with figlist_var(width=0.7, filename="ESR_align_example.pdf") as fl:
             d *= exp(1j * 2 * pi * d.fromaxis(Bname) * thisshift)
             d.ift(Bname)
             d.set_units(Bname, "G")
-            scaling = (d * ref_spec_Bdom).sum(Bname) / (
-                ref_spec_Bdom * ref_spec_Bdom
+            a = d
+            b = ref_spec_Bdom
+            scaling = (a * b).sum(Bname) / (
+                b * b
             ).sum(Bname)
             scaling = scaling.real.item()
         d[Bname] += minB
