ce9b93cd1db4056e917e6064b30594de4251f004
John M. Franck (SU laptop) <jmfranck@syr.edu>
2023-05-13 17:11:15 -0400

    pull needed rescale function from FS runthrough branch

--- pyspecProcScripts/__init__.py
+++ pyspecProcScripts/__init__.py
@@ -1,8 +1,13 @@
-from .phasing import zeroth_order_ph,hermitian_function_test,ph1_real_Abs,determine_sign
+from .phasing import (
+    zeroth_order_ph,
+    hermitian_function_test,
+    ph1_real_Abs,
+    determine_sign,
+)
 from .first_level.fake_data import fake_data
 from .generate_integrals import generate_integrals
 from .load_data import lookup_table
-from .plotting import expand_limits,draw_limits,fl_mod
+from .plotting import expand_limits, draw_limits, fl_mod
 from .slice_FID_from_echo import slice_FID_from_echo
 from .baseline import calc_baseline
 from .Utility import *
@@ -16,32 +21,35 @@ from .DCCT_func import DCCT
 from .integral_w_error import integral_w_errors, active_propagation
 from .apod_matched_filter import apod_matched_filter
 from .lorentzian_to_gaussian import L2G
-__all__ = ['calc_baseline',
-        'center_echo',
-        'correl_align',
-        'determine_sign',
-        'dBm2power',
-        'DCCT',
-        'decay',
-        'draw_limits',
-        'expand_limits',
-        'fake_data',
-        'generate_integrals',
-        'fl_mod',
-        'hermitian_function_test',
-        'integral_w_errors',
-        'active_propagation',
-        'fwhm_calculator',
-        'integrate_limits',
-        'ph1_real_Abs',
-        'lookup_table',
-        'power2dBm',
-        'recovery',
-        'select_pathway',
-        'slice_FID_from_echo',
-        'Vpp2power',
-        'zeroth_order_ph',
-        'apod_matched_filter',
-        'L2G',
-        ]
+from .first_level.QESR_rescale import QESR_scalefactor
 
+__all__ = [
+    "DCCT",
+    "L2G",
+    "QESR_scalefactor",
+    "Vpp2power",
+    "active_propagation",
+    "apod_matched_filter",
+    "calc_baseline",
+    "center_echo",
+    "correl_align",
+    "dBm2power",
+    "decay",
+    "determine_sign",
+    "draw_limits",
+    "expand_limits",
+    "fake_data",
+    "fl_mod",
+    "fwhm_calculator",
+    "generate_integrals",
+    "hermitian_function_test",
+    "integral_w_errors",
+    "integrate_limits",
+    "lookup_table",
+    "ph1_real_Abs",
+    "power2dBm",
+    "recovery",
+    "select_pathway",
+    "slice_FID_from_echo",
+    "zeroth_order_ph",
+]
