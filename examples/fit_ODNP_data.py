c1dd2b3f3165e087c57444e396b5739e8b4793bb
Alex <guinneak198@github.com>
2023-10-25 09:10:19 -0400

    moved mat file and used where instead of diff in for loop

--- examples/fit_ODNP_data.py
+++ examples/fit_ODNP_data.py
@@ -4,7 +4,8 @@ The T1(p) and E(p) integrals are stored in previous post processing pulled from 
 """
 from pyspecdata import *
 from sympy import symbols, Symbol, latex
-from scipy.io import loadmat
+import scipy.io as sio
+from os.path import dirname, join as pjoin
 
 target_directory = os.path.normpath(getDATADIR("AG_processed_data"))
 h5_file = "ras.h5"
@@ -12,8 +13,7 @@ nodename = "230706_M67_a"
 # {{{ plotting fn - for labels on final plot
 def list_symbs(f):
     list_symbs = [f"${latex(Symbol(j))} = {k:0.5g}$" for j, k in f.output().items()]
-    list_symbs = "\n".join(list_symbs)
-    return list_symbs
+    return "\n".join(list_symbs)
 
 
 # }}}
@@ -21,18 +21,9 @@ def list_symbs(f):
 Ep = nddata_hdf5(f"{h5_file}/{nodename}/Ep", directory=target_directory)
 R1p = nddata_hdf5(f"{h5_file}/{nodename}/R1p", directory=target_directory)
 # }}}
-# {{{Dataset Parameters
-SL_conc_M = Ep.get_prop("acq_params")["concentration"]
-ppt = Ep.get_prop("acq_params")["guessed_MHz_to_GHz"] * 1e-3
-Ep_pts = Ep.getaxis("power")
 # {{{Find the index where the return powers begin
-thisdiff = Ep.fromaxis("power").diff("power")
-for idx in range(len(Ep.getaxis("power"))):
-    if thisdiff["power", idx].data < 0:
-        flip_idx = idx
-        break
+flip_idx = np.where(Ep.fromaxis("power").diff("power").data < 0)[0][0]
 flip_idx += 1
-# }}}
 # }}}
 with figlist_var() as fl:
     # {{{Plot Ep
@@ -40,7 +31,7 @@ with figlist_var() as fl:
     fl.plot(
         Ep["power", :flip_idx],
         "o",
-        label="Data for increasing power",
+        label="Progressive Saturation Data",
         capsize=6,
         alpha=0.5,
     )
@@ -57,22 +48,19 @@ with figlist_var() as fl:
     fl.plot(R1p, "o", label="Experimental Data")
     # }}}
     # {{{load in T100 dataset
-    T10_p = loadmat("T10_DI_water_230412")["a"][0, :]
-    R10_p = nddata((T10_p[0] + T10_p[1] * R1p.getaxis("power")) ** -1, "power")
-    R10_p.setaxis("power", R1p.getaxis("power"))
+    data_dir = pjoin(
+        dirname(os.path.normpath(getDATADIR("AG_processed_data"))),
+        "AG_processed_data",
+        "T10_DI_water_230412",
+    )
+    T10_p = sio.loadmat(data_dir)["a"][0, :]
+    R10_p = R1p.fromaxis("power").eval_poly(T10_p, "power") ** -1
     # }}}
     # {{{ fit krho inverse with two degrees of freedom and then apply to fit R1p
-    krho_inv = SL_conc_M / (R1p - R10_p)
+    krho_inv = Ep.get_prop("acq_params")["concentration"] / (R1p - R10_p)
     krho_inv_fit = krho_inv.polyfit("power", order=2)
     krho_inv_fine = R1p.fromaxis("power").eval_poly(krho_inv_fit, "power")
-    R1p_fit = (T10_p[0] + T10_p[1] * R1p.fromaxis("power")) ** -1 + (
-        SL_conc_M
-        / (
-            krho_inv_fit[0]
-            + krho_inv_fit[1] * R1p.fromaxis("power")
-            + krho_inv_fit[2] * R1p.fromaxis("power") ** 2
-        )
-    )
+    R1p_fit = R10_p + Ep.get_prop("acq_params")["concentration"] / krho_inv_fine
     fl.plot(R1p_fit, ls=":", color="k", label="Fit", alpha=0.5)
     plt.ylabel(r"$R_{1} / s^{-1}$")
     plt.xlabel("Power / W")
@@ -82,12 +70,13 @@ with figlist_var() as fl:
     M0, A, phalf, p = symbols("M0 A phalf power", real=True)
     sp = p / (p + phalf)
     R1p = (T10_p[0] + T10_p[1] * p) ** -1 + (
-        SL_conc_M / (krho_inv_fit[0] + krho_inv_fit[1] * p + krho_inv_fit[2] * p**2)
+        Ep.get_prop("acq_params")["concentration"]
+        / (krho_inv_fit[0] + krho_inv_fit[1] * p + krho_inv_fit[2] * p**2)
     )  # Symbolic expression for R1p that is used in the symbolic function for the fitting of E(p)
     Ep_fit = lmfitdata(Ep["power", :flip_idx])
     Ep_fit.functional_form = M0 - ((M0 * A * sp) / R1p)
     Ep_fit.set_guess(
-        M0=dict(value=Ep["power", 0].real.data, min=1e4, max=11e4),
+        M0=dict(value=Ep["power", 0].real.item(), min=1e4, max=11e4),
         A=dict(value=13, min=0.5, max=17),
         phalf=dict(value=0.2, min=0.1, max=0.4),
     )
@@ -95,7 +84,9 @@ with figlist_var() as fl:
     Ep_fit.fit()
     thisfit = Ep_fit.eval(100)
     fl.plot(thisfit, ls=":", color="k", label="Fit", alpha=0.5)
-    ksig = (Ep_fit.output("A") * ppt) / SL_conc_M
+    ksig = (
+        Ep_fit.output("A") * Ep.get_prop("acq_params")["guessed_MHz_to_GHz"] * 1e-3
+    ) / Ep.get_prop("acq_params")["concentration"]
     ax = plt.gca()
     text(
         0.5,
