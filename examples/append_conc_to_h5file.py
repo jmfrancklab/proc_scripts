251be169649d9ddf3563eb6b89fcb947de08cbcf
Alex Guinness <aguinnes@syr.edu>
2023-11-07 13:02:42 -0500

    add docstring for HDF5 edit file

--- examples/append_conc_to_h5file.py
+++ examples/append_conc_to_h5file.py
@@ -1,9 +1,21 @@
+"""Edit the concentration parameter of a saved HDF5 file
+========================================================
+
+Loads in a previously saved HDF5 dataset. If the attribute
+'concentration' does not exist in the acq_params, this will 
+add the concentration defined by 'actual_conc' into acq_params
+under 'concentration'. If the attribute already exists it will
+simply replace the current concentration with the value of 
+'actual_conc'.
+"""
 from pyspecdata import *
 import h5py
 filename = '220126_Ras_M67R1a_capProbe.h5'
 actual_conc = 72e-6 #M
-h5 = search_filename(f"{filename}" , exp_type = 'ODNP_NMR_comp/ODNP')
+h5 = search_filename(f"{filename}" , exp_type = 'ODNP_NMR_comp/ODNP',unique=True)
+print(h5)
 h5 = ['/'.join(path.split('\\')) for path in h5]
+print(h5)
 with h5py.File(h5[0],'r+') as thisfile:
     acq_params = thisfile['enhancement']['other_info']['acq_params']
     acq_params.attrs['concentration'] = actual_conc
