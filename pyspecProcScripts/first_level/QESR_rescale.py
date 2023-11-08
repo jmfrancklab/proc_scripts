da1d6fb03ebdb8d8311698a9bd313af55a4d9402
Alex Guinness <aguinnes@syr.edu>
2023-11-08 08:25:52 -0500

    change "conversion" to "propFactor" since conversion factor is already a real thing

--- pyspecProcScripts/first_level/QESR_rescale.py
+++ pyspecProcScripts/first_level/QESR_rescale.py
@@ -9,15 +9,15 @@ class calib_info (object):
     def use_calibration(self, calibration_name):
         if calibration_name != self.current_calib_name:
             assert type(calibration_name) is str, "the calibration name must be specified as a string!"
-            assert len(calibration_name)>0, "you MUST use a calibration name and set the values `[calibration_name] q` and `[calibration_name] conversion` in your _pyspecdata file!"
+            assert len(calibration_name)>0, "you MUST use a calibration name and set the values `[calibration_name] q` and `[calibration_name] propFactor` in your _pyspecdata file!"
             self.current_calib_name = calibration_name
-            # note that this will fail if you don't have "[calibration_name] conversion" and "[calibration_name] q" set in your pyspecdata file
+            # note that this will fail if you don't have "[calibration_name] propFactor" and "[calibration_name] q" set in your pyspecdata file
             self.default_Q = float(pyspec_config.get_setting(f"{calibration_name} Q"))
-            self.dint_conversion = float(pyspec_config.get_setting(f"{calibration_name} Conversion"))
+            self.dint_propFactor = float(pyspec_config.get_setting(f"{calibration_name} Proportionality factor"))
     def use_diameter(self, diameter_name):
         if diameter_name != self.current_diam_name:
             assert type(diameter_name) is str, "the diameter name must be specified as a string!"
-            assert len(diameter_name)>0, "you MUST use a diameter name and set the values `[diameter_name] q` and `[diameter_name] conversion` in your _pyspecdata file!"
+            assert len(diameter_name)>0, "you MUST use a diameter name and set the values `[diameter_name] q` and `[diameter_name] propFactor` in your _pyspecdata file!"
             self.current_diam_name = diameter_name
             self.d = float(pyspec_config.get_setting(f"{diameter_name} diameter"))
 calibcache = calib_info()
@@ -40,7 +40,7 @@ def QESR_scalefactor(d, calibration_name=None, diameter_name=None):
     S = Q_(0.5, "dimensionless")
     c = Q_(
         1, "dimensionless"
-    )  # the first fraction on pg 2-17 -- essentially the conversion factor
+    )  # the first fraction on pg 2-17 -- essentially the proportionality factor
     signal_denom = G_R * C_t * sqrt(power) * B_m * n_B * S * (S + 1) * Q * d**2
     signal_denom = signal_denom.to(Q_("G") * sqrt(Q_("W")) * Q_("s") * Q_("m")**2)
     # }}}
@@ -55,7 +55,7 @@ def QESR_scalefactor(d, calibration_name=None, diameter_name=None):
             f"$c={c:~L} $\n",
             f"$d={d:~L} $\n",
             f"signal denom$={signal_denom:~L}$",
-            f"doubleint conversion$={calibcache.dint_conversion}$",
+            f"doubleint propFactor$={calibcache.dint_propFactor}$",
             )
-    # normally, we divide by signal_denom.magnitude and multiply by calibcache.dint_conversion and divide by 1e-6
-    return signal_denom.magnitude/calibcache.dint_conversion*1e-6
+    # normally, we divide by signal_denom.magnitude and multiply by calibcache.dint_propFactor and divide by 1e-6
+    return signal_denom.magnitude/calibcache.dint_propFactor*1e-6
