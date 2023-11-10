de7e845bfbdf631a1afcd33f18b4ceeb1a02bc79
Alex Guinness <aguinnes@syr.edu>
2023-11-09 21:55:46 -0500

    docstring to pickle table fn

--- examples/QESR.py
+++ examples/QESR.py
@@ -11,36 +11,57 @@ the following values under General:
     220720 propfactor = 8.637e-5
     220720 q = 4600
     qesr caps diameter = 0.704
-"""    
+"""
 
 from pyspecdata import *
 from pyspecProcScripts import *
 from pyspecProcScripts import QESR_scalefactor
-fieldaxis = '$B_0$'
+
+fieldaxis = "$B_0$"
 with figlist_var() as fl:
-    for filenum, (thisfile, that_exp_type, thislabel, pushout, calibration, diameter,
-            background) in enumerate(
-            [
-              ('220804_rasI36_MTSL.DSC','francklab_esr/Farhana','kRas I36',0.4, '220720','QESR caps',
-                  find_file('220804_water.DSC',exp_type='francklab_esr/Farhana')['harmonic',0]),
-             ]):
-        d = find_file(thisfile,exp_type = that_exp_type)
+    for filenum, (
+        thisfile,
+        that_exp_type,
+        thislabel,
+        pushout,
+        calibration,
+        diameter,
+        background,
+    ) in enumerate(
+        [
+            (
+                "220804_rasI36_MTSL.DSC",
+                "francklab_esr/Farhana",
+                "kRas I36",
+                0.4,
+                "220720",
+                "QESR caps",
+                find_file("220804_water.DSC", exp_type="francklab_esr/Farhana")[
+                    "harmonic", 0
+                ],
+            ),
+        ]
+    ):
+        d = find_file(thisfile, exp_type=that_exp_type)
         d -= d[fieldaxis, -100:].data.mean()
         if "harmonic" in d.dimlabels:
-            d = d['harmonic',0]
+            d = d["harmonic", 0]
         color = d.get_plot_color()
         fl.next("Raw QESR")
-        fl.plot(d,color=color, label=thislabel,alpha = 0.5)
+        fl.plot(d, color=color, label=thislabel, alpha=0.5)
         fl.next("Rescaled using the proportionality constant")
-        d /= QESR_scalefactor(d, calibration_name = calibration,
-                diameter_name = diameter)
-        fl.plot(d,color=color,label=thislabel)
-        c = protein_QESR(thisfile, label=thislabel,pushout=pushout,
-                exp_type = that_exp_type,
-                color=color,
-                calibration_name = calibration,
-                diameter_name = diameter,
-                background =  background,
-                which_plot = 'compare',
-                pickle_file = "TEMPOL_rerun_conc.pickle",
-                fl = fl)
+        d /= QESR_scalefactor(d, calibration_name=calibration, diameter_name=diameter)
+        fl.plot(d, color=color, label=thislabel)
+        c = protein_QESR(
+            thisfile,
+            label=thislabel,
+            pushout=pushout,
+            exp_type=that_exp_type,
+            color=color,
+            calibration_name=calibration,
+            diameter_name=diameter,
+            background=background,
+            which_plot="compare",
+            pickle_file="TEMPOL_rerun_conc.pickle",
+            fl=fl,
+        )
--- pyspecProcScripts/first_level/show_pickle_table.py
+++ pyspecProcScripts/first_level/show_pickle_table.py
@@ -1,11 +1,18 @@
+"""Useful function when comparing multiple data sets and
+takes advantage of the capability of protein_QESR to output
+pickle files. A latex table is generated pulling from the
+pickle file called.
+"""
 import pickle
+
+
 def show_pickle_table(pickle_file):
     """show a pickle file with scalar variables in a
     latex table"""
     with open(pickle_file, "rb") as fp:
         variables = pickle.load(fp)
-    print(r'\par\begin{tabular}{cc}')
-    print(r'\textbf{label} & \textbf{concentration}\\\hline\hline')
+    print(r"\par\begin{tabular}{cc}")
+    print(r"\textbf{label} & \textbf{concentration}\\\hline\hline")
     for k, v in variables.items():
         print(f"{k} & {v} \\\\")
-    print(r'\end{tabular}\par')
+    print(r"\end{tabular}\par")
