12376884f03d9cbe47ce37e1771dfefd520645e6
John Franck <jmfranck@jmfDebian.syr.edu>
2023-11-14 10:19:47 -0500

    ran black

--- pyspecProcScripts/__init__.py
+++ pyspecProcScripts/__init__.py
@@ -13,39 +13,45 @@ from .integral_w_error import integral_w_errors, active_propagation
 from .integrate_limits import integrate_limits
 from .load_data import lookup_table
 from .lorentzian_to_gaussian import L2G
-from .phasing import zeroth_order_ph,hermitian_function_test,ph1_real_Abs,determine_sign
-from .plotting import expand_limits,draw_limits,fl_mod
+from .phasing import (
+    zeroth_order_ph,
+    hermitian_function_test,
+    ph1_real_Abs,
+    determine_sign,
+)
+from .plotting import expand_limits, draw_limits, fl_mod
 from .simple_functions import select_pathway
 from .slice_FID_from_echo import slice_FID_from_echo
 from .third_level.QESR import QESR
-__all__ = ['DCCT',
-        'L2G',
-        'QESR_scalefactor',
-        'Vpp2power',
-        'active_propagation',
-        'apod_matched_filter',
-        'calc_baseline',
-        'center_echo',
-        'correl_align',
-        'dBm2power',
-        'decay',
-        'determine_sign',
-        'draw_limits',
-        'expand_limits',
-        'fl_mod',
-        'fwhm_calculator',
-        'generate_integrals',
-        'hermitian_function_test',
-        'integral_w_errors',
-        'integrate_limits',
-        'lookup_table',
-        'ph1_real_Abs',
-        'power2dBm',
-        'QESR',
-        'recovery',
-        'select_pathway',
-        'show_pickle_table',
-        'slice_FID_from_echo',
-        'zeroth_order_ph',
-        ]
 
+__all__ = [
+    "DCCT",
+    "L2G",
+    "QESR_scalefactor",
+    "Vpp2power",
+    "active_propagation",
+    "apod_matched_filter",
+    "calc_baseline",
+    "center_echo",
+    "correl_align",
+    "dBm2power",
+    "decay",
+    "determine_sign",
+    "draw_limits",
+    "expand_limits",
+    "fl_mod",
+    "fwhm_calculator",
+    "generate_integrals",
+    "hermitian_function_test",
+    "integral_w_errors",
+    "integrate_limits",
+    "lookup_table",
+    "ph1_real_Abs",
+    "power2dBm",
+    "QESR",
+    "recovery",
+    "select_pathway",
+    "show_pickle_table",
+    "slice_FID_from_echo",
+    "zeroth_order_ph",
+]
