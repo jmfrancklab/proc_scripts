ca4e5c2e16934d7b8f7457e16e91078109248936
Alex Guinness <aguinnes@syr.edu>
2024-01-15 12:27:37 -0500

    cleanup

--- pyspecProcScripts/__init__.py
+++ pyspecProcScripts/__init__.py
@@ -1,41 +1,37 @@
-from .CPMG_phasing import center_echo
 from .phasing import (
     zeroth_order_ph,
     hermitian_function_test,
     ph1_real_Abs,
     determine_sign,
 )
-from .Utility import *
+from .generate_integrals import generate_integrals
+from .load_data import lookup_table
+from .plotting import expand_limits,draw_limits,fl_mod
+from .slice_FID_from_echo import slice_FID_from_echo
 from .baseline import calc_baseline
+from .Utility import *
+from .CPMG_phasing import center_echo
 from .first_level.QESR_rescale import QESR_scalefactor
 from .first_level.show_pickle_table import show_pickle_table
 from .fitting import recovery, decay
 from .fwhm_calculate import fwhm_calculator
 from .integrate_limits import integrate_limits
-from .generate_integrals import generate_integrals
 from .correlation_alignment import correl_align
 from .simple_functions import select_pathway
 from .DCCT_func import DCCT
 from .integral_w_error import integral_w_errors, active_propagation
 from .apod_matched_filter import apod_matched_filter
-from .load_data import lookup_table
 from .lorentzian_to_gaussian import L2G
-from .plotting import expand_limits, draw_limits, fl_mod
-from .slice_FID_from_echo import slice_FID_from_echo
 from .third_level.QESR import QESR
 
 __all__ = [
-    "DCCT",
-    "L2G",
-    "QESR",
-    "QESR_scalefactor",
-    "Vpp2power",
     "active_propagation",
     "apod_matched_filter",
     "calc_baseline",
     "center_echo",
     "correl_align",
     "dBm2power",
+    "DCCT",
     "decay",
     "determine_sign",
     "draw_limits",
@@ -46,12 +42,16 @@ __all__ = [
     "hermitian_function_test",
     "integral_w_errors",
     "integrate_limits",
+    "L2G",
     "lookup_table",
     "ph1_real_Abs",
     "power2dBm",
+    "QESR",
+    "QESR_scalefactor",
     "recovery",
     "select_pathway",
     "show_pickle_table",
     "slice_FID_from_echo",
+    "Vpp2power",
     "zeroth_order_ph",
 ]
