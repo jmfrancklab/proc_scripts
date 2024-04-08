74a0b3afe17782ce7d76049ea38b1b663dc68032
Alex Guinness <aguinnes@syr.edu>
2024-04-08 13:07:06 -0700

    cleaned up and deleted unnecessary lines

--- examples/read_log.py
+++ examples/read_log.py
@@ -13,14 +13,12 @@ myfilename = search_filename(
 with figlist_var() as fl:
     # {{{ open h5 file to real log
     with h5py.File(myfilename, "r") as f:
-        thislog = logobj.from_group(f['log'])
+        thislog = logobj.from_group(f["log"])
     # }}}
     # In order to properly set the time axis to start at 0
     # both the log's start time will be subtracted from the
     # the relative time recorded
-    log_start_time = thislog.total_log["time"][0]
-    relative_time = thislog.total_log["time"]
-    time_axis = thislog.total_log['time'] - thislog.total_log['time'][0]
+    time_axis = thislog.total_log["time"] - thislog.total_log["time"][0]
     # }}}
     # {{{ plot the output power and reflection
     fig, (ax_Rx, ax_power) = subplots(2, 1, figsize=(10, 8))
@@ -37,9 +35,8 @@ with figlist_var() as fl:
     # {{{ Add a vertical line at the time the data acquisition for the
     #     set power began
     mask = thislog.total_log["cmd"] != 0
-    n_events = len(relative_time[mask])
     for j, thisevent in enumerate(thislog.total_log[mask]):
-        ax_Rx.axvline(x=thisevent["time"] - thislog.total_log['time'][0])
-        ax_power.axvline(x=thisevent["time"] - thislog.total_log['time'][0])
+        ax_Rx.axvline(x=thisevent["time"] - thislog.total_log["time"][0])
+        ax_power.axvline(x=thisevent["time"] - thislog.total_log["time"][0])
     # }}}
     plt.tight_layout()
