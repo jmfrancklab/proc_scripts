b8acb4f590bb2b34a99b8155b9f06bf057e0eadb
Alex Guinness <aguinnes@syr.edu>
2024-07-02 17:39:33 -0400

    handle nScans

--- examples/compare_CPMG_echo.py
+++ examples/compare_CPMG_echo.py
@@ -14,14 +14,14 @@ with figlist_var() as fl:
         (
             "240702_13p5mM_TEMPOL_CPMG.h5",
             "ODNP_NMR_comp/CPMG",
-            "CPMG_16",
+            "CPMG_17",
             True,
             "CPMG large cyc",
         ),
         (
             "240702_13p5mM_TEMPOL_echo.h5",
             "ODNP_NMR_comp/Echoes",
-            "echo_7",
+            "echo_10",
             False,
             "echo large cyc",
         ),
@@ -50,4 +50,6 @@ with figlist_var() as fl:
             acq = thisd.get_prop("acq_params")
             echo_time = 1e-6 * 2 * (acq["tau_us"] + acq["p90_us"])
             thisd["t2"] = (thisd["t2"]["nEcho"]) * echo_time + thisd["t2"]["t2"]
+        if 'nScans' in thisd.dimlabels:
+            thisd.mean('nScans')
         fl.plot(abs(thisd), label=thislabel)
