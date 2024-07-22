68f4c6a9eef18c51e0412700089e14f4a477174e
John Franck <jmfranck@jmfDebian.syr.edu>
2024-07-22 19:38:20 -0400

    flake and black stuff

--- examples/proc_nutation.py
+++ examples/proc_nutation.py
@@ -11,10 +11,8 @@ Tested with:
 ``py proc_nutation.py nutation_2 240710_27mM_TEMPOL_chokes_T_atprobe_nutation.h5 ODNP_NMR_comp/nutation``
 
 """
-from pyspecProcScripts.load_data import lookup_table
-from pyspecProcScripts import select_pathway, determine_sign, fid_from_echo, zeroth_order_ph
-from pyspecdata import *
-import numpy as np
+import pyspecProcScripts as prscr
+import pyspecdata as psd
 import sys
 import os
 from itertools import cycle
@@ -24,58 +22,84 @@ colorcyc = cycle(colorcyc_list)
 
 assert len(sys.argv) == 4
 d = find_file(
-    sys.argv[2], exp_type=sys.argv[3], expno=sys.argv[1], lookup=lookup_table
+    sys.argv[2],
+    exp_type=sys.argv[3],
+    expno=sys.argv[1],
+    lookup=prscr.load_data.lookup_table,
 )
 if d.get_prop("postproc_type") == "spincore_SE_v1":
     d.rename("indirect", "p_90")
     d["p_90"] *= 1e-9
 elif "indirect" in d.dimlabels:
-    d.rename("indirect","p_90")
-with figlist_var() as fl:
+    d.rename("indirect", "p_90")
+with psd.figlist_var() as fl:
     # {{{ Manual processing
     d.squeeze()
-    d.ift('t2')
-    d['t2'] -= 2e-3 # eyeball correction
-    d.ft('t2')
+    d.ift("t2")
+    d["t2"] -= 2e-3  # eyeball correction
+    d.ft("t2")
     fl.next("Raw Data")
     fl.image(d, interpolation="auto")
     fl.next("raw data, phase")
-    ph0 = select_pathway(d)['t2':(-100,0)]['p_90',:5].sum('t2').sum('p_90')
+    ph0 = (
+        prscr.select_pathway(d)["t2":(-100, 0)]["p_90", :5]
+        .sum("t2")
+        .sum("p_90")
+    )
     ph0 /= abs(ph0)
-    fl.image(select_pathway(d/ph0).angle*180/pi, interpolation="auto")
+    fl.image(
+        prscr.select_pathway(d / ph0).angle * 180 / pi, interpolation="auto"
+    )
     fl.next("raw data, abs")
-    select_pathway(d).C.mean('nScans').run(abs).pcolor()
+    prscr.select_pathway(d).C.mean("nScans").run(abs).pcolor()
     fl.next("time domain")
     d.ift("t2")
     fl.image(d["t2":(None, 20e-3)], interpolation="auto")
-    d_manualslice = d.C['t2':(0,None)]
-    d.ft('t2')
-    d_manualslice['t2',0] *= 0.5
+    d_manualslice = d.C["t2":(0, None)]
+    d.ft("t2")
+    d_manualslice["t2", 0] *= 0.5
     d_manualslice.ft("t2")
-    d_manualslice /= zeroth_order_ph(select_pathway(d_manualslice)["t2":(-250, 250)].mean('nScans'))
-    my_signs = determine_sign(select_pathway(d_manualslice)["t2":(-250, 250)].mean('nScans'))
+    d_manualslice /= prscr.zeroth_order_ph(
+        prscr.select_pathway(d_manualslice)["t2":(-250, 250)].mean("nScans")
+    )
+    my_signs = prscr.determine_sign(
+        prscr.select_pathway(d_manualslice)["t2":(-250, 250)].mean("nScans")
+    )
     fl.next("manual fid slice")
-    fl.image(select_pathway(d_manualslice)["t2":(-250, 250)], interpolation='auto')
+    fl.image(
+        prscr.select_pathway(d_manualslice)["t2":(-250, 250)],
+        interpolation="auto",
+    )
     fl.next("manual fid slice, with sign flip")
-    fl.image(select_pathway(d_manualslice)["t2":(-250, 250)]*my_signs, interpolation='auto')
+    fl.image(
+        prscr.select_pathway(d_manualslice)["t2":(-250, 250)] * my_signs,
+        interpolation="auto",
+    )
     # }}}
     ## {{{ correct for evolution during pulse
     ##     I do this, but if I plot it, I do find that it's not significant
-    #d *= np.exp(
+    # d *= np.exp(
     #    -1j * 2 * pi * (2 * d.fromaxis("p_90") / pi) * d.fromaxis("t2")
-    #)
+    # )
     ## }}}
     # {{{ do it the "correct" way
-    fl.next("fid_from_echo result")
-    d = fid_from_echo(d)
-    d /= zeroth_order_ph(select_pathway(d)["t2":(-250, 250)].mean('nScans'))
-    fl.image(select_pathway(d)["t2":(-250, 250)], interpolation="auto")
-    fl.next("fid_from_echo result, with sign flip")
-    my_signs = determine_sign(select_pathway(d)["t2":(-250, 250)].mean('nScans'))
-    fl.image(select_pathway(d)["t2":(-250, 250)]*my_signs, interpolation="auto")
+    fl.next("prscr.fid_from_echo result")
+    d = prscr.fid_from_echo(d)
+    d /= prscr.zeroth_order_ph(
+        prscr.select_pathway(d)["t2":(-250, 250)].mean("nScans")
+    )
+    fl.image(prscr.select_pathway(d)["t2":(-250, 250)], interpolation="auto")
+    fl.next("prscr.fid_from_echo result, with sign flip")
+    my_signs = prscr.determine_sign(
+        prscr.select_pathway(d)["t2":(-250, 250)].mean("nScans")
+    )
+    fl.image(
+        prscr.select_pathway(d)["t2":(-250, 250)] * my_signs,
+        interpolation="auto",
+    )
     # }}}
     if d.get_prop("coherence_pathway") is not None:
-        d.mean('nScans')
+        d.mean("nScans")
         fl.next("sum of abs of all coherence pathways (for comparison)")
         forplot = abs(d)
         guess_direct = (
@@ -89,6 +113,6 @@ with figlist_var() as fl:
             list(d.get_prop("coherence_pathway").keys()) + [guess_direct]
         )
         fl.plot(forplot)
-        d = select_pathway(d)
+        d = prscr.select_pathway(d)
         fl.next("with coherence pathway selected")
-        d['t2':(-300,300)].pcolor()
+        d["t2":(-300, 300)].pcolor()
