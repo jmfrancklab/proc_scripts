ccc253e5032fc18dd551282643c749f2fe3b4a1e
Alex Guinness <aguinnes@syr.edu>
2024-07-30 16:49:50 -0400

    repley to review

--- examples/proc_GDS_pulse_capture.py
+++ examples/proc_GDS_pulse_capture.py
@@ -8,33 +8,32 @@ sqrt(W)).
 """
 import pyspecdata as psd
 import matplotlib.pyplot as plt
-from numpy import sqrt, array
+from numpy import sqrt
 
-show_all = True
+atten_ratio = 101.35  # attenutation ratio
+show_all = False
 with psd.figlist_var() as fl:
     for filename, nodename, label in [
-        (
-            "240730_test_amp1_fin_pulse_calib.h5",
-            "pulse_calib_1",
-            "amplitude = 1"
-        ),
-        #(
-        #    "240730_test_amp0p1_fin_pulse_calib.h5",
-        #    "pulse_calib_1",
-        #    "amplitude = 0.1"
-        #),
+        ("240730_test_amp1_fin_pulse_calib.h5", "pulse_calib_1", "amplitude = 1"),
+        ("240730_test_amp0p1_fin_pulse_calib.h5", "pulse_calib_1", "amplitude = 0.1"),
     ]:
         d = psd.find_file(
-            filename,
-            expno = nodename,
-            exp_type="ODNP_NMR_comp/test_equipment"
+            filename, expno=nodename, exp_type="ODNP_NMR_comp/test_equipment"
         )
-        beta = psd.ndshape(d).pop('t').alloc().rename('p_90','prog_beta')
-        beta.setaxis('prog_beta',d.get_prop('desired_betas'))
+        beta = psd.ndshape(d).pop("t").alloc().rename("p_90", "prog_beta")
+        beta.setaxis("prog_beta", d.get_prop("desired_betas"))
+        t_v_beta = psd.ndshape(d).pop("t").alloc().rename("p_90", "At_p")
+        t_v_beta.setaxis(
+            "At_p",
+            list(
+                d.get_prop("acq_params")["amplitude"] * d.get_prop("set_p90s")[j]
+                for j in range(len(d["p_90"]))
+            ),
+        )
         for j in range(len(d["p_90"])):
             s = d["p_90", j].C
             s.set_units("t", "s")
-            s *= 101.35  # attenutation ratio
+            s *= atten_ratio
             s /= sqrt(2)  # Vrms
             s /= sqrt(50)  # V/sqrt(R) = sqrt(P)
             if show_all:
@@ -66,11 +65,10 @@ with psd.figlist_var() as fl:
                 # {{{ plot and integrate 90 pulse
                 fl.plot(abs(s), color="red")
                 # }}}
-            beta90_int = abs(s).contiguous(
-                lambda x: x > 0.01 * s.max()
-            )[0]
+            beta90_int = abs(s).contiguous(lambda x: x > 0.01 * s.max())[0]
             beta1 = abs(s["t":beta90_int]).integrate("t").data.item() * 1e6
-            beta['prog_beta',j] = beta1
+            beta["prog_beta", j] = beta1
+            t_v_beta["At_p", j] = beta1
             if show_all:
                 plt.ylabel(r"$\sqrt{P_{pulse}}$")
                 plt.text(
@@ -82,6 +80,12 @@ with psd.figlist_var() as fl:
                 plt.axvline(beta90_int[1] * 1e6, ls=":", alpha=0.2)
         # {{{ make nddata for beta of 90 pulse and 180 pulse
         fl.next(r"Measured $\beta$ vs programmed $\beta$")
-        fl.plot(beta, "o")
+        fl.plot(beta, "o", label=label)
+        psd.gridandtick(plt.gca())
         plt.ylabel(r"measured $\beta$ / $\mathrm{\mu s \sqrt{W}}$")
         plt.xlabel(r"programmed $\beta$ / $\mathrm{\mu s \sqrt{W}}$")
+        fl.next(r"Measured $\beta$ vs Amplitude*$t_{plse}$")
+        fl.plot(t_v_beta, "o", label=label)
+        plt.ylabel(r"measured $\beta$ / $\mathrm{\mu s \sqrt{W}}$")
+        plt.xlabel(r"programmed amplitude*$t_{pulse}$ / $\mu$s")
+        psd.gridandtick(plt.gca())
