38a23f90bad3f4787653a1dd3c5575c546709166
Alex Guinness <aguinnes@syr.edu>
2024-07-31 11:40:37 -0400

    add linear and curve threshold conditions

--- examples/proc_GDS_pulse_capture.py
+++ examples/proc_GDS_pulse_capture.py
@@ -30,17 +30,18 @@ color_cycle = cycle(
 atten_ratio = 101.35  # attenutation ratio
 show_all = False  # diagnostic
 with psd.figlist_var() as fl:
-    for filename, nodename, label in [
-        ("240730_test_amp1_fin_pulse_calib.h5", "pulse_calib_1", "amplitude = 1"),
-        ("240730_test_amp0p1_fin_pulse_calib.h5", "pulse_calib_1", "amplitude = 0.1"),
+    for filename, nodename, amplitude in [
+        ("240730_test_amp1_fin_pulse_calib.h5", "pulse_calib_1", 1.0),
+        ("240730_test_amp0p1_fin_pulse_calib.h5", "pulse_calib_1", 0.1),
     ]:
         d = psd.find_file(
             filename, expno=nodename, exp_type="ODNP_NMR_comp/test_equipment"
         )
+        label = "amplitude = %f" % amplitude
         thiscolor = next(color_cycle)
         # {{{ allocate shapes for the final t_v_beta and beta_v_t plots
-        t_v_beta = d.shape.pop("t").alloc().rename("p_90", "desired_beta")
-        t_v_beta.setaxis("desired_beta", d.get_prop("desired_betas"))
+        t_v_beta = d.shape.pop("t").alloc().rename("p_90", "beta")
+        t_v_beta.setaxis("beta", d.get_prop("desired_betas"))
         beta_v_t = d.shape.pop("t").alloc().rename("p_90", "At_p")
         beta_v_t.setaxis(
             "At_p",
@@ -95,21 +96,25 @@ with psd.figlist_var() as fl:
                     -1,
                     r"$t_{90} \sqrt{P_{tx}} = %f s \sqrt{W}$" % beta1,
                 )
-                plt.axvline(int_range[0] * 1e6, ls=":", alpha=0.2)
-                plt.axvline(int_range[1] * 1e6, ls=":", alpha=0.2)
         # {{{ make nddata for beta of 90 pulse and 180 pulse
         # {{{ beta vs t
         fl.next(r"Measured $\beta$ vs A * $t_{pulse}$")
         fl.plot(beta_v_t, "o", color=thiscolor, label=label)
-        if beta_v_t < 6:
+        if amplitude > 1:
+            linear_threshold = 7
+            curve_threshold = 7.5
+        else:
+            linear_threshold = 2
+            curve_threshol = 2.5
+        if beta_v_t < linear_threshold:
             mask = np.ones_like(beta_v_t.data, dtype=bool)
         else:
-            mask = beta_v_t > 6.5
+            mask = beta_v_t > curve_threshold
         calibration_data = psd.nddata(beta_v_t.data[mask], [-1], ["At_p"]).setaxis(
             "At_p", beta_v_t.getaxis("At_p")[mask]
         )
         calibration_data.sort("At_p")
-        if beta_v_t < 6:
+        if beta_v_t < linear_threshold:
             c = calibration_data.polyfit("At_p", order=10)
         else:
             c = calibration_data.polyfit("At_p", order=1)
@@ -121,20 +126,26 @@ with psd.figlist_var() as fl:
         plt.xlabel(r"programmed amplitude*$t_{pulse}$ / $\mu$s")
         # }}}
         # {{{ t vs beta
+        if amplitude > 1:
+            linear_threshold = 30
+            curve_threshold = 35
+        else:
+            linear_threshold = 10
+            curve_threshol = 11
         t_v_beta = beta_v_t.shape.alloc().rename("At_p", "beta")
         t_v_beta.setaxis("beta", beta_v_t.data)
         t_v_beta.data[:] = beta_v_t.getaxis("At_p")
         fl.next(r"Amplitude*$t_{pulse}$ vs Measured $\beta$")
         fl.plot(t_v_beta, "o", color=thiscolor, label=label)
-        if t_v_beta < 40:
+        if t_v_beta < linear_threshold:
             mask = np.ones_like(t_v_beta.data, dtype=bool)
         else:
-            mask = t_v_beta > 45
+            mask = t_v_beta > curve_threshold
         calibration_data = psd.nddata(t_v_beta.data[mask], [-1], ["beta"]).setaxis(
             "beta", t_v_beta.getaxis("beta")[mask]
         )
         calibration_data.sort("beta")
-        if t_v_beta < 40:
+        if t_v_beta < linear_threshold:
             c = calibration_data.polyfit("beta", order=10)
         else:
             c = calibration_data.polyfit("beta", order=1)
