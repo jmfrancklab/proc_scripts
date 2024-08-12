1a75f7502540eac5bf8f8c7e2156f843d1ba624e
Alex Guinness <aguinnes@syr.edu>
2024-08-12 10:23:57 -0400

    process and fit FID nutation

--- examples/proc_FID_nutation.py
+++ examples/proc_FID_nutation.py
@@ -1,31 +1,15 @@
 import pyspecdata as psd
 import pyspecProcScripts as prscr
 import numpy as np
+import sympy as sp
 import matplotlib.pyplot as plt
 
-
-def clock_correct(s, axis_along, direct="t2", max_cyc=0.5):
-    for_correct = s.C
-    Delta = np.diff(s[axis_along][np.r_[0, -1]]).item()
-    correction_axis = psd.nddata(np.r_[-0.5:0.5:300j] * max_cyc / Delta, "correction")
-    for_correct = for_correct * np.exp(
-        -1j * 2 * np.pi * correction_axis * for_correct.fromaxis(axis_along)
-    )
-    # {{{ determine the best sign flip for each correction
-    for j in range(for_correct.shape["correction"]):
-        thesign = prscr.determine_sign(for_correct["correction", j])
-        for_correct["correction", j] *= thesign
-    # }}}
-    for_correct.sum(direct)
-    return for_correct.sum(axis_along).run(abs).argmax("correction").item()
-
-
-disprange = (-500, 500)
+signal_range = (-25, 250)
 signal_range = (-250, -50)
 
 with psd.figlist_var() as fl:
     s = psd.find_file(
-        "240723_27mM_TEMPOL_test_FID_nutation.h5",
+        "240805_amp0p1_27mM_TEMPOL_FID_nutation.h5",
         exp_type="ODNP_NMR_comp/nutation",
         expno="FID_nutation_1",
         lookup=prscr.lookup_table,
@@ -33,42 +17,59 @@ with psd.figlist_var() as fl:
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
     fl.next("Raw data", fig=fig)
     fig.suptitle("Single Pulse nutation")
-    fl.image(s.C["t2":disprange], ax=ax1)
+    signal_pathway = s.get_prop("coherence_pathway")
+    if "nScans" in s.dimlabels:
+        s.mean("nScans")
+    mysgn = prscr.determine_sign(
+        prscr.select_pathway(s["t2":signal_range], signal_pathway)
+    )
+    # {{{ apply overall zeroth order correction
+    s /= prscr.zeroth_order_ph(prscr.select_pathway(s["t2":0], signal_pathway))
+    d_raw = s.C
+    fl.image(prscr.select_pathway(s["t2":signal_range].C, signal_pathway), ax=ax1)
     ax1.set_title("Raw Data")
-    total_corr = 0
-    og_signs = prscr.determine_sign(
-        prscr.select_pathway(s["t2":signal_range], s.get_prop("coherence_pathway"))
+    d_unc = prscr.select_pathway(s["t2":signal_range].C.real, signal_pathway).integrate(
+        "t2"
     )
-    for j in range(5):
-        corr = clock_correct(
-            prscr.select_pathway(s, s.get_prop("coherence_pathway"))
-            * og_signs
-            * np.exp(-1j * 2 * np.pi * total_corr * s.fromaxis("p_90")),
-            "p_90",
-        )
-        total_corr += corr
-    s *= np.exp(-1j * 2 * np.pi * total_corr * s.fromaxis("p_90"))
-    for j in range(len(s.getaxis("p_90"))):
+    # }}}
+    # {{{ zeroth_order on individual betas
+    for j in range(len(s.getaxis("beta"))):
         ph0 = prscr.zeroth_order_ph(
-            prscr.select_pathway(
-                s["p_90", j]["t2":signal_range], s.get_prop("coherence_pathway")
-            )
+            prscr.select_pathway(s["beta", j]["t2":signal_range], signal_pathway)
         )
-        s["p_90", j] /= ph0
-    my_signs = prscr.determine_sign(
-        prscr.select_pathway(s["t2":signal_range], s.get_prop("coherence_pathway"))
+        s["beta", j] /= ph0
+    d0 = prscr.select_pathway(s["t2":signal_range].C.real, signal_pathway).integrate(
+        "t2"
     )
-    s *= my_signs
-    s = prscr.fid_from_echo(s, s.get_prop("coherence_pathway"))
-    fl.image(s, ax=ax2)
-    ax2.set_title("Phased and FID sliced")
-    if og_signs.data[3] < 1:
-        s *= -og_signs
-    else:
-        s *= og_signs
-    fl.image(s, ax=ax3)
-    ax3.set_title("Sign Flipped Back")
-    s = prscr.select_pathway(s, s.get_prop("coherence_pathway"))
-    s = s.real.integrate("t2")
+    # }}}
+    # {{{ define mysgn based on phase difference between d_unc and d0
+    mysign = (d0 / d_unc).angle / np.pi
+    mysign = np.exp(1j * np.pi * mysign.run(np.round))
+    d_raw *= mysign
+    fl.image(prscr.select_pathway(d_raw, signal_pathway), ax=ax2)
+    ax2.set_title("Same Sign")
+    s = d_raw
+    s.set_units("t2", "s")
+    s.set_error(None)
+    s = prscr.fid_from_echo(s, signal_pathway)
+    s *= mysign
+    fl.image(s, human_units=False, ax=ax3)
+    ax3.set_title("Phased and FID sliced")
+    s = prscr.select_pathway(s["t2":signal_range].real, signal_pathway)
+    s.integrate("t2")
+    s.set_error(None)
+    A, beta_ninety, beta = sp.symbols("A beta_ninety beta", real=True)
     fl.next("Integrated")
     fl.plot(s, "o")
+    f = psd.lmfitdata(s)
+    f.functional_form = A * sp.sin(beta / beta_ninety * sp.pi / 2)
+    f.set_guess(
+        A=dict(
+            value=s.data.max() * 1.2, min=s.data.max() * 0.8, max=s.data.max() * 1.5
+        ),
+        beta_ninety=dict(value=2e-5, min=0, max=1),
+    )
+    f.fit()
+    fit = f.eval(100)
+    fl.plot(fit)
+    psd.gridandtick(plt.gca())
