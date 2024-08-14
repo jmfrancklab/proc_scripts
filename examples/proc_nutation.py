7e56334765c18fe10038cea674ec92f4eb55f5ae
Alex Guinness <aguinnes@syr.edu>
2024-08-14 14:35:45 -0400

    simplify nutation processing

--- examples/proc_nutation.py
+++ examples/proc_nutation.py
@@ -13,7 +13,6 @@ Tested with:
 """
 import pyspecdata as psd
 import pyspecProcScripts as prscr
-import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
 import sys
@@ -26,7 +25,7 @@ s = psd.find_file(
     expno=sys.argv[1],
     lookup=prscr.lookup_table,
 )
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+fig, (ax1, ax2) = plt.subplots(1, 2)
 with psd.figlist_var() as fl:
     # {{{ set up subplots
     fl.next("Raw Data with averaged scans", fig=fig)
@@ -35,50 +34,17 @@ with psd.figlist_var() as fl:
     signal_pathway = s.get_prop("coherence_pathway")
     if "nScans" in s.dimlabels:
         s.mean("nScans")
-    mysgn = prscr.determine_sign(
-        prscr.select_pathway(s["t2":signal_range], signal_pathway)
-    )
-    # {{{ apply overall zeroth order correction
-    s /= prscr.zeroth_order_ph(prscr.select_pathway(s["t2":0], signal_pathway))
-    d_raw = s.C
     fl.image(
         prscr.select_pathway(s["t2":signal_range], signal_pathway), ax=ax1
     )
     ax1.set_title("Raw Data")
-    d_unc = prscr.select_pathway(
-        s["t2":signal_range].real, signal_pathway
-    ).integrate("t2")
-    # }}}
-    # {{{ zeroth_order on individual betas
-    for j in range(len(s.getaxis("beta"))):
-        ph0 = prscr.zeroth_order_ph(
-            prscr.select_pathway(
-                s["beta", j]["t2":signal_range], signal_pathway
-            )
-        )
-        s["beta", j] /= ph0
-    d0 = prscr.select_pathway(
-        s["t2":signal_range].real, signal_pathway
-    ).integrate("t2")
-    # }}}
-    # {{{ define mysgn based on phase difference between d_unc and d0
-    mysign = (d0 / d_unc).angle / np.pi
-    mysign = np.exp(1j * np.pi * mysign.run(np.round))
-    # }}}
-    d_raw *= mysign
-    fl.image(prscr.select_pathway(d_raw, signal_pathway), ax=ax2)
-    ax2.set_title("Same Sign")
-    d_raw.set_units("t2", "s")
-    d_raw.set_error(None)
-    s = prscr.fid_from_echo(d_raw, signal_pathway)
-    if mysign["beta", 5] < 0:
-        s *= -mysign
-    else:
-        s *= mysign
-    fl.image(s, human_units=False, ax=ax3)
-    ax3.set_title("Phased and FID sliced")
-    s = prscr.select_pathway(s["t2":signal_range].real, signal_pathway)
-    s.integrate("t2")
+    # {{{ apply overall zeroth order correction
+    s /= prscr.zeroth_order_ph(prscr.select_pathway(s["t2":0], signal_pathway))
+    s = prscr.fid_from_echo(s, signal_pathway)
+    s = prscr.select_pathway(s, signal_pathway)
+    fl.image(s["t2":signal_range], human_units=False, ax=ax2)
+    ax2.set_title("Phased and FID sliced")
+    s = s["t2":signal_range].real.integrate("t2")
     s.set_error(None)
     A, R, beta_ninety, beta = sp.symbols("A R beta_ninety beta", real=True)
     fl.next("Integrated")
@@ -99,4 +65,12 @@ with psd.figlist_var() as fl:
     f.fit()
     fit = f.eval(100)
     fl.plot(fit)
+    plt.xlabel(r"$\beta$ / $\mathrm{\mu s \sqrt{W}}$")
+    beta_90 = fit.argmax("beta").item() * 1e6
+    plt.axvline(beta_90)
+    plt.text(
+        beta_90 + 5,
+        5e4,
+        r"$\beta_{90} = %f \mathrm{\mu s \sqrt{W}}$" % beta_90,
+    )
     psd.gridandtick(plt.gca())
