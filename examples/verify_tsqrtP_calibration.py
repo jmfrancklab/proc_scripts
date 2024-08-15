AG units PR was fixed

examples/verify_tsqrtP_calibration.py
@@ -53,9 +53,9 @@ with psd.figlist_var() as fl:
         fl.basename = f"amplitude = {amplitude}"
         if not d.get_units("t") == "s":
             print(
-                "************ AG still needs to finish pyspecdata PR to save units!!! ************"
+                "units weren't set for the t axis or else I can't read them from the hdf5 file!"
             )
-            d.set_units("t", "s")  # why isn't this done already??
+            d.set_units("t", "s")
         d *= V_atten_ratio
         d /= np.sqrt(50)  # V/sqrt(R) = sqrt(P)
         pulse_lengths = d.get_prop("programmed_t_pulse_us")