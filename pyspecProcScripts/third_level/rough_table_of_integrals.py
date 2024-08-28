0868b4eb92d08b6d5b16ff303eadebed1ea95249
John Franck <jmfranck@jmfDebian.syr.edu>
2024-08-28 14:09:59 -0400

    fix issue where rough alignment would have been aliasing stuff we don't want

--- pyspecProcScripts/third_level/rough_table_of_integrals.py
+++ pyspecProcScripts/third_level/rough_table_of_integrals.py
@@ -2,6 +2,7 @@ from ..phasing import zeroth_order_ph, determine_sign, fid_from_echo
 from ..simple_functions import select_pathway
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import r_
 from numpy import pi
 import pyspecdata as psd
 
@@ -24,18 +25,24 @@ def rough_table_of_integrals(
         of the integral over all the data.
     signal_range : tuple
         Narrow slice range where signal resides.
+        You probably want to get this from `find_peakrange` rather than trying
+        to specify manually!
+        (And expand the range that it gives you slightly)
     signal_pathway : dict (default None)
         If None, the function will go into the properties of the data looking
-        for the "coherence_pathway" property. If that doesn't exist the user
-        needs to feed a dictionary containing the coherence transfer pathway
-        where the signal resides.
+        for the "coherence_pathway" property.
+        If that doesn't exist the user needs to feed a dictionary containing
+        the coherence transfer pathway where the signal resides.
+        If the signal has already been sliced along the coherence dimensions,
+        you can feed an empty dictionary (note the *very* different behavior
+        *vs* `None`)
     echo_like : boolean
-        If the signal has an echo the fid_from_echo function will be applied
+        If the signal has an echo the `fid_from_echo` function will be applied
         prior to integration to apply the FID slice as well as proper phasing
         and peak selection.
     title : str
         Title for the returned figure.
-    direct : str
+    direct : str (default "t2")
         Name of direct dimension.
 
     Returns
@@ -44,9 +51,12 @@ def rough_table_of_integrals(
         The table of integrals (collapse the direct dimension into a single number).
         Processing is done in place.
     ax_last : Axes
-        Return the axis with the table of integrals, in case you want to add a fit!
+        Return the axis with the table of integrals (plotted as `"o"`), in case you want to add a fit!
     """
-
+    center_of_slice = np.mean(signal_range)
+    signal_range_expanded = center_of_slice + 2 * r_[-0.5, 0.5] * np.diff(
+        signal_range
+    )
     assert fl is not None, "for now, fl can't be None"
     # {{{ set up subplots
     if echo_like:
@@ -56,9 +66,9 @@ def rough_table_of_integrals(
         fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
     fig.set_figwidth(15)
     fig.set_figheight(6)
+    fig.suptitle(title)
     fl.next("Raw Data with averaged scans", fig=fig)
     fl.skip_units_check()
-    fig.suptitle(title)
     # }}}
     # To handle both older data where the coherence pathway was not set as a property
     # and handle newer data where it is set I have the following if/else
@@ -67,16 +77,15 @@ def rough_table_of_integrals(
     s /= zeroth_order_ph(
         select_pathway(s[direct:signal_range].sum(direct), signal_pathway)
     )
-    s = select_pathway(s[direct:signal_range], signal_pathway)
-    # {{{ determine shift for rough alignment, but don't use this yet, because
-    #     I want to see the signal resonance frequency
+    s = select_pathway(s[direct:signal_range_expanded], signal_pathway)
+    # {{{ determine shift for rough alignment
     s.ift(direct)
     shift = s * np.exp(-(pi**2) * s.fromaxis(direct) ** 2 * (2 * 50**2))
     shift.ft(direct)
     shift = shift.real.run(abs).argmax(direct)
     shift.set_error(None)
-    # interestingly, if we were correcting here, we would multiply by the phase
-    # here to perform the shift, which would be efficient!
+    # interestingly, if we were correcting here, we would multiply `s` by
+    # the phase here to perform the shift, which would be efficient!
     s.ft(direct)
     # }}}
     fl.image(
@@ -86,6 +95,8 @@ def rough_table_of_integrals(
     ax1.set_title("Signal pathway / ph0")
     # }}}
     # {{{ Check phase variation along indirect
+    #     I don't want the data aligned to do this,  in case there is
+    #     some type of second-order phase shift that becomes apparend
     mysign = determine_sign(
         s,
         signal_range,
@@ -107,11 +118,13 @@ def rough_table_of_integrals(
         s *= mysign  # flip the sign back, so we get sensible integrals
     # {{{ generate the table of integrals
     # {{{ actually apply the rough alignment
+    #     Note that we center about the center of the slice, so our
+    #     sliced integration works out OK, not about zero.
     s.ift(direct)
-    s *= np.exp(-1j * 2 * pi * shift * s.fromaxis(direct))
+    s *= np.exp(-1j * 2 * pi * (shift - center_of_slice) * s.fromaxis(direct))
     s.ft(direct)
     # }}}
-    s = s.real.integrate(direct).set_error(None)
+    s = s[direct:signal_range].real.integrate(direct).set_error(None)
     ax_last = ax4 if echo_like else ax3
     fl.plot(s, "o", ax=ax_last)
     psd.gridandtick(plt.gca())
