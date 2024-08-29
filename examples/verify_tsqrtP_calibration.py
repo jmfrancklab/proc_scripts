1d46738242bf3b2eb1f1827c400d68f50cfb2f44
Alex Guinness <aguinnes@syr.edu>
2024-08-29 10:34:49 -0400

    AG reviewed and commented

--- examples/verify_tsqrtP_calibration.py
+++ examples/verify_tsqrtP_calibration.py
@@ -1,13 +1,14 @@
 r"""
 Verify the pulse calibration
 ============================
-Once we have the pulse-length calibration in place, we want to simply verify that we get the
-β values that we program, where
-:math:`\beta = \frac{1}{\sqrt{2} \int \sqrt{P(t)} dt` 
+Once we have the pulse-length calibration in place, we want to simply verify
+that we get the β values that we program, where :math:`\beta =
+\frac{1}{\sqrt{2} \int \sqrt{P(t)_{amp}} dt` 
 
-This is performed with amplitude pulses driving into an attenuator and measured by a scope,
-as for the calibration.
+This data should be acquired using `example/run_pulse_calibration.py` in
+FLInst.
 """
+
 import pyspecdata as psd
 from pyspecProcScripts import find_apparent_anal_freq
 import matplotlib.pyplot as plt
@@ -46,16 +47,12 @@ with psd.figlist_var() as fl:
             d.get_prop("postproc_type") == "GDS_capture_v1"
         ), "The wrong postproc_type was set so you most likely used the wrong script for acquisition"
         amplitude = d.get_prop("acq_params")["amplitude"]
-        fl.basename = f"amplitude = {amplitude:.2f}"  # .2 to differentiate amplitude 0.1 from 0.05
-        if not d.get_units("t") == "s":
-            psd.logger.info(
-                "units weren't set for the t axis or else I can't read them from the hdf5 file!"
-            )
-            d.set_units("t", "s")
+        fl.basename = f"amplitude = {amplitude:.2f}"
         d *= V_atten_ratio  # V at output of amplifier
-        d /= np.sqrt(50)  # V/sqrt(R) = sqrt(P_p)
+        d /= np.sqrt(50)  # V/sqrt(R) = sqrt(P_amp)
 
-        # {{{ functions that streamline plotting the desired pulse length datasets
+        # {{{ functions that streamline plotting the desired pulse length 
+        #     datasets
         def switch_to_plot(d, j):
             thislen = d.get_prop("programmed_t_pulse")[j] / 1e-6
             fl.next(f"pulse length = {thislen:.2f} μs")
@@ -72,14 +69,13 @@ with psd.figlist_var() as fl:
                         color=thiscolor,
                         label=thislabel,
                     )
-
+                    plt.ylabel(r"$\sqrt{P}$ / $\mathrm{\sqrt{W}}$")
         # }}}
-
         # {{{ data is already analytic, and downsampled to below 24 MHz
-        indiv_plots(abs(d), "abs(analytic)", "orange")  # sqrt(P_p)
+        indiv_plots(abs(d), "abs(analytic)", "orange")
         d, nu_a, _ = find_apparent_anal_freq(d)  # find frequency of signal
         d.ft("t")
-        # {{{ Diagnostic to ensure the frequency of the signal was properly identified
+        # {{{ Diagnostic to ensure the frequency properly identified
         fl.next("Frequency Domain")
         fl.plot(d)
         plt.text(
@@ -92,26 +88,27 @@ with psd.figlist_var() as fl:
             0 < nu_a - 0.5 * HH_width
         ), "unfortunately the region I want to filter includes DC -- this is probably not good, and you should pick a different timescale for your scope so this doesn't happen"
         # }}}
-        # {{{ apply frequency filter
+        # {{{ apply HH frequency filter
         d["t" : (None, nu_a - 0.5 * HH_width)] *= 0
         d["t" : (nu_a + 0.5 * HH_width, None)] *= 0
         # }}}
         d.ift("t")
         indiv_plots(abs(d), "filtered analytic", "red")
         # }}}
-        thiscolor = next(color_cycle)
-        # {{{ set up shape of data to drop the correct values in
+        # {{{ set up shape of data to drop the calculated beta values in
         verify_beta = d.shape.pop("t").alloc(dtype=np.float64)
         verify_beta.copy_axes(d)
         verify_beta.set_units(r"s√W").set_units("beta", r"s√W")
         # }}}
+        thiscolor = next(color_cycle)
         for j in range(len(d["beta"])):
             s = d["beta", j]
             int_range = abs(s).contiguous(lambda x: x > 0.03 * s.max())[0]
             # slightly expand int range to include rising edges
-            int_range[0] -= 2e-6
-            int_range[-1] += 2e-6
-            # {{{ plot the data that we're integrating all together
+            int_range[0] -= 5e-6
+            int_range[-1] += 5e-6
+            # {{{ plot the integration range of all pulses prior to integrating.
+            #     Serves as diagnostic to ensure the beta is consistently increasing.
             fl.push_marker()
             fl.basename = None
             fl.next("collect filtered analytic")
@@ -120,8 +117,8 @@ with psd.figlist_var() as fl:
             # }}}
             verify_beta["beta", j] = (
                 abs(s["t":int_range]).integrate("t").data.item()
-            )  # tp * sqrt(P_p)
-            verify_beta["beta", j] /= np.sqrt(2)  # tp*sqrt(Prms)
+            )  # tp * sqrt(P_amp)
+            verify_beta["beta", j] /= np.sqrt(2)  # tp*sqrt(P_rms)
             # {{{ Can't use indiv_plots because we've already indexed the beta
             # out and we also want to plot the calculated beta on top
             if skip_plots is not None and j % skip_plots == 0:
@@ -131,7 +128,7 @@ with psd.figlist_var() as fl:
                     color="black",
                     label="integrated slice",
                 )
-                plt.ylabel(r"$\sqrt{P_{pulse}}$")
+                plt.ylabel(r"$\sqrt{P_{pulse}}$ / $\sqrt{W}}$")
                 plt.text(
                     int_range[0] * 1e6 - 1,
                     0.25,
@@ -139,7 +136,7 @@ with psd.figlist_var() as fl:
                     % (verify_beta["beta", j].item() / 1e-6),
                 )
             # }}}
-        # {{{ show what we observe -- how does β vary with the programmed pulse length
+        # {{{ show what we observe -- how does β vary with the desired β
         fl.basename = None  # we want to plot all amplitudes together now
         fl.next(r"Measured $\beta$ vs programmed $\beta$")
         fl.plot(
