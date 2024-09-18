7edf1d8480fae8da58926b67006f60c48e286c69
Alex Guinness <aguinnes@syr.edu>
2024-09-18 15:32:24 -0400

    line size

--- pyspecProcScripts/load_data.py
+++ pyspecProcScripts/load_data.py
@@ -1,6 +1,6 @@
-"""Used to preprocess data based on type of experiment performed. Returns all data FTed
-into the frequency domain with the phase cycles also FTed (coherence domain). Data is
-not sliced or altered in anyway.
+"""Used to preprocess data based on type of experiment performed. Returns all
+data FTed into the frequency domain with the phase cycles also FTed (coherence
+domain). Data is not sliced or altered in anyway.
 
 Parameters
 ==========
@@ -25,7 +25,11 @@ def proc_bruker_deut_IR_withecho_mancyc(s, fl=None):
         fl.image(s.C.setaxis("indirect", "#").set_units("indirect", "scan #"))
     s.chunk(
         "indirect", ["ph2", "ph1", "indirect"], [4, 2, -1]
-    )  # expands the indirect dimension into indirect, ph1, and ph2. inner most dimension is the inner most in the loop in pulse sequence, is the one on the farthest right. Brackets with numbers are the number of phase cycle steps in each one. the number of steps is unknown in 'indirect' and is therefore -1.
+    )  # expands the indirect dimension into indirect, ph1, and ph2. inner most
+    #    dimension is the inner most in the loop in pulse sequence, is the one
+    #    on the farthest right. Brackets with numbers are the number of phase
+    #    cycle steps in each one. the number of steps is unknown in 'indirect'
+    #    and is therefore -1.
     s.setaxis("ph1", r_[0:2.0] / 4)  # setting values of axis ph1 to line up
     s.setaxis("ph2", r_[0:4.0] / 4)  # setting values of axis ph1 to line up
     s.setaxis("indirect", s.get_prop("vd"))
@@ -565,7 +569,8 @@ def proc_spincore_ODNP_v1(s, fl=None):
     if fl is not None:
         fl.next("all data: frequency domain")
         fl.image(s.C.setaxis("power", "#").set_units("power", "scan #"))
-    # {{{ since the power axis was saved with settings and not meter powers, fix that here
+    # {{{ since the power axis was saved with settings and not meter powers,
+    # fix that here
     power_axis_dBm = np.array(s.get_prop("meter_powers"))
     power_axis_W = np.zeros_like(power_axis_dBm)
     power_axis_W[:] = 1e-2 * 10 ** ((power_axis_dBm[:] + 10.0) * 1e-1)
@@ -603,7 +608,8 @@ def proc_spincore_ODNP_v2(s, fl=None):
     if fl is not None:
         fl.next("all data: frequency domain")
         fl.image(s.C.setaxis("power", "#").set_units("power", "scan #"))
-    # {{{ since the power axis was saved with settings and not meter powers, fix that here
+    # {{{ since the power axis was saved with settings and not meter powers,
+    # fix that here
     power_axis_dBm = np.array(s.get_prop("meter_powers"))
     power_axis_W = np.zeros_like(power_axis_dBm)
     power_axis_W[:] = 1e-2 * 10 ** ((power_axis_dBm[:] + 10.0) * 1e-1)
@@ -657,7 +663,8 @@ def proc_spincore_generalproc_v1(s, include_tau_sub=True, fl=None):
         neworder += [nonphdims[j] for j in sizeidx]
     # }}}
     s.reorder(neworder)
-    # {{{ put ph_overall outside, if it exists, since there should be nothing outside that
+    # {{{ put ph_overall outside, if it exists, since there should be nothing
+    # outside that
     if "ph_overall" in s.dimlabels:
         s.reorder("ph_overall")
     # }}}
@@ -680,7 +687,8 @@ def proc_DOSY_CPMG(s, fl=None):
     if fl is None:
         raise ValueError("you must pass kwarg fl or edit the source")
     logging.debug("loading pre-processing for DOSY-CPMG")
-    # {{{ all of this would be your "preprocessing" and would be tied to the name of your pulse sequence
+    # {{{ all of this would be your "preprocessing" and would be tied to the
+    # name of your pulse sequence
     l22 = int(
         s.get_prop("acq")["L"][22]
     )  # b/c the l are integers by definition
@@ -742,7 +750,8 @@ def proc_DOSY_CPMG(s, fl=None):
     s.reorder(["m", "n", "ph4", "ph8", "indirect", "echo", "t2"])
     s.ft("t2", shift=True).ift(
         "t2"
-    )  # this is overkill -- need a pyspecdata function that does this w/out the fft
+    )  # this is overkill -- need a pyspecdata function that does this w/out
+    #    the fft
     # }}}
     return s
 
@@ -816,9 +825,12 @@ lookup_table = {
     "spincore_nutation_v3": proc_nutation_chunked,
     "spincore_nutation_v4": proc_nutation_v4,
     "spincore_nutation_v6": proc_spincore_generalproc_v1,
-    "spincore_ODNP_v1": proc_spincore_ODNP_v1,  # for 4 x 1 phase cycle take meter power
-    "spincore_ODNP_v2": proc_spincore_ODNP_v2,  # for 2 x 2 phase cycle take meter powers
-    "spincore_ODNP_v3": proc_spincore_ODNP_v3,  # for 4 x 1 phase cycle no meter powers
+    "spincore_ODNP_v1": proc_spincore_ODNP_v1,  # for 4 x 1 phase cycle take
+    #                                             meter power
+    "spincore_ODNP_v2": proc_spincore_ODNP_v2,  # for 2 x 2 phase cycle take 
+    #                                             meter powers
+    "spincore_ODNP_v3": proc_spincore_ODNP_v3,  # for 4 x 1 phase cycle no 
+    #                                             meter powers
     "spincore_ODNP_v4": proc_spincore_generalproc_v1,
     "spincore_ODNP_v5": proc_spincore_generalproc_v1,
     "spincore_echo_v1": proc_spincore_echo_v1,
