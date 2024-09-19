7b0799a02747ee345d264b2505b7b693f18e5a50
Warren Kincaid <wfkincai@syr.edu>
2024-09-19 18:45:51 -0400

    added field sweep example to doc string, passes flake

--- examples/proc_raw.py
+++ examples/proc_raw.py
@@ -12,9 +12,14 @@ Tested with:
 
 ``py proc_raw.py echo_6 240620_200uM_TEMPOL_pm_echo.h5 ODNP_NMR_comp/Echoes``
 
-``py proc_raw.py echo_8 240620_200uM_TEMPOL_pm_generic_echo.h5 ODNP_NMR_comp/Echoes``
+``py proc_raw.py echo_8 240620_200uM_TEMPOL_pm_generic_echo.h5
+ODNP_NMR_comp/Echoes``
 
-``py proc_raw.py CPMG_9 240620_200uM_TEMPOL_pm_generic_CPMG.h5 ODNP_NMR_comp/Echoes``
+``py proc_raw.py CPMG_9 240620_200uM_TEMPOL_pm_generic_CPMG.h5
+ODNP_NMR_comp/Echoes``
+
+``py proc_raw.py field_2 240918_L56_MTSL_A1_Rasbatch240911_field
+ODNP_NMR_comp/field_dependent``
 
 ``py proc_raw.py ODNP K42.*A1_kRasbatch240814 ODNP_NMR_comp/ODNP``
 
