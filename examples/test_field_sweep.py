130ea5dfbc09bbf7757a77390d1dec8aaef5c220
Warren Kincaid <wfkincai@syr.edu>
2024-09-26 19:54:51 -0400

    using rough_table func, posting error to slack

--- examples/test_field_sweep.py
+++ examples/test_field_sweep.py
@@ -11,7 +11,7 @@ with psd.figlist_var() as fl:
             "ODNP_NMR_comp/field_dependent",
             "field_1",
             "field_sweep_v4",
-            "9-24-24 13.5 mM TEMPOL sweep",
+            "240924 13.5 mM TEMPOL field sweep",
         )
     ]:
         s = psd.find_file(
@@ -23,17 +23,18 @@ with psd.figlist_var() as fl:
         )
         s_indirect = s["indirect"]
         ###{{{ Raw data
-        label = label_str
-        fl.basename = "(%s)" % label
+        fl.basename = "(%s)" % label_str
         fl.next("raw data")
         fl.image(s, interpolation="auto")
         ###}}}
         ###{{{ Time Domain
-        field_axis = []
-        for indir_val in range(np.shape(s.getaxis("indirect"))[0]):
-            field_axis.append(s.getaxis("indirect")[indir_val][0])
+        print(s)
+        print(psd.ndshape(s))
+        #fl.show();quit()
+        field_axis = s["indirect"]        
         s.setaxis("indirect", field_axis)
-        nu_B12 = s.get_prop("acq_params")["uw_dip_center_GHz"]
+        print("field_axis is", field_axis)
+        nu_B12, half_window = prscr.find_peakrange(s, fl=fl)
         if "nScans" in s.dimlabels:
             s.reorder(["ph1", "indirect", "nScans", "t2"])
         else:
@@ -46,25 +47,21 @@ with psd.figlist_var() as fl:
         s -= rx_offset_corr
         fl.next("time domain")
         s.ft(["ph1"])
-        fl.image(s["t2":(None, 0.05)])
+        fl.image(s["t2":(None, t2_max)])
+        print("t2 units", s.get_units("t2"))
+        #fl.show();quit()
         ###}}}
         ###{{{ Before Phasing
-        # print(np.shape(s['indirect','data{0}','carrierFreq']))
-        s = s["t2":(-800, 800)]
+        s.ft("t2")
+        s = s["t2":s.get_prop("peakrange")]
+        print("peakrange before rough func", s.get_prop("peakrange"))
+        s = prscr.rough_table_of_integrals(s, fl=fl, title=label_str)
+        print("peakrange after rough func", s.get_prop("peakrange"))
         fl.next("before phasing")
         print(psd.ndshape(s))
-        if "nScans" in s.dimlabels:
-            fl.plot(prscr.select_pathway(s.C.mean('nScans'),signal_pathway))
-        else:
-            fl.plot(prscr.select_pathway(s, signal_pathway))
+        fl.show();quit()
         ###}}}
-        # fl.show();quit()
-        # s.ift('t2')
-        # print(s.getaxis('t2')[0])
-        # fl.show();quit()
-        # s['t2'] -= s.getaxis('t2')[0]
         s.set_units("t2","s")
-        s.ft("t2")
         s = prscr.fid_from_echo(s, signal_pathway, fl=fl)
         print(s.get_prop("peakrange"))
         peakrange = s.get_prop("peakrange")
