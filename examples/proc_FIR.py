228896ee502955756fb3ef8bc835a5bb13521183
Warren Kincaid <wfkincai@syr.edu>
2024-09-30 19:28:05 -0400

    initial rough FIR processing script

---
+++ examples/proc_FIR.py
@@ -0,0 +1,30 @@
+"""
+Process FIR experiment 
+====================================================
+Opens .h5 results file, uses rough_table_of_integrals() to roughly process dataset including generating a table of integrals
+"""
+
+
+import pyspecProcScripts as prscr
+import pyspecdata as psd
+from Instruments.logobj import logobj
+
+with psd.figlist_var() as fl:
+    thisfile, exptype, nodename, post_proc, lookup = (
+        "240924_13p5mM_TEMPOL_ODNP_1.h5",
+        "ODNP_NMR_comp/ODNP",
+        "FIR_34dBm",
+        "spincore_IR_v3",
+        prscr.lookup_table,
+    )
+    s = psd.find_file(
+        thisfile,
+        exp_type=exptype,
+        expno=nodename,
+        postproc=post_proc,
+        lookup=prscr.lookup_table,
+    )
+    s.set_units("vd", "s")
+    prscr.rough_table_of_integrals(s, fl=fl)
+    fl.show()
+
