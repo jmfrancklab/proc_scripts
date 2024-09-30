1594c75388f2fac2eda08ffaf88477fc891e6eb4
Warren Kincaid <wfkincai@syr.edu>
2024-09-30 11:24:15 -0400

    added back in docstring, reviewed JF mods

--- examples/proc_fieldSweep.py
+++ examples/proc_fieldSweep.py
@@ -1,3 +1,9 @@
+"""
+Check NMR/ESR resonance ratio using a field sweep
+====================================================
+Analyzes field sweep data. Determines the optimal field across a gradient that is on-resonance with the Bridge 12 μw frequency stored in the file to determine the resonance ratio of MHz/GHz.
+"""
+
 import pyspecdata as psd
 import pyspecProcScripts as prscr
 
