25cea439aa882ab007f8c47e299a31fae4fc81a3
Warren Kincaid <wfkincai@syr.edu>
2024-10-01 12:05:24 -0400

    flake on proc_Ep

--- examples/proc_Ep.py
+++ examples/proc_Ep.py
@@ -1,13 +1,13 @@
 """
 Process Enhancement experiment 
 ====================================================
-Opens .h5 results file, uses rough_table_of_integrals() to roughly process dataset including generating a table of integrals
+Opens .h5 results file, uses rough_table_of_integrals() to roughly process
+dataset including generating a table of integrals
 """
 
 
 import pyspecProcScripts as prscr
 import pyspecdata as psd
-from Instruments.logobj import logobj
 
 with psd.figlist_var() as fl:
     thisfile, exptype, nodename, post_proc, lookup = (
@@ -27,3 +27,4 @@ with psd.figlist_var() as fl:
     s["indirect"] = s["indirect"]["start_times"]
     s.set_units("indirect", "s")
     prscr.rough_table_of_integrals(s, fl=fl)
+    fl.show()
