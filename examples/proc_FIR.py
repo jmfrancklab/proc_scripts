13237e262605301dd6b2c830e5322b201672e073
Warren Kincaid <wfkincai@syr.edu>
2024-10-07 19:55:59 -0400

    ran black and flake, still problem with nodename loop

--- examples/proc_FIR.py
+++ examples/proc_FIR.py
@@ -1,21 +1,40 @@
 """
 Process FIR experiment 
 ====================================================
-Opens .h5 results file, uses rough_table_of_integrals() to roughly process dataset including generating a table of integrals
+Opens .h5 results file, uses rough_table_of_integrals() to roughly process
+dataset including generating a table of integrals
 """
 
 
 import pyspecProcScripts as prscr
 import pyspecdata as psd
-from Instruments.logobj import logobj
 import numpy as np
 import sympy
 import matplotlib.pyplot as plt
 from itertools import cycle
 
-color_cycle = cycle(['red','orange','yellow','green','cyan','blue','purple','magenta',
-    '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2',
-    '#7f7f7f', '#bcbd22', '#17becf'])
+color_cycle = cycle(
+    [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "cyan",
+        "blue",
+        "purple",
+        "magenta",
+        "#1f77b4",
+        "#ff7f0e",
+        "#2ca02c",
+        "#d62728",
+        "#9467bd",
+        "#8c564b",
+        "#e377c2",
+        "#7f7f7f",
+        "#bcbd22",
+        "#17becf",
+    ]
+)
 T1_list = []
 
 with psd.figlist_var() as fl:
@@ -26,9 +45,9 @@ with psd.figlist_var() as fl:
         prscr.lookup_table,
     )
     for nodename in [
-        "FIR_noPower",
+        # "FIR_noPower",
         "FIR_34dBm",
-        ]:
+    ]:
         print(nodename)
         s = psd.find_file(
             thisfile,
@@ -48,8 +67,10 @@ with psd.figlist_var() as fl:
             if fl is not None:
                 fl.next("before clock correction")
                 fl.image(s)
-            s_clock = prscr.select_pathway(s, s.get_prop("coherence_pathway")).mean('nScans').sum(
-                direct
+            s_clock = (
+                prscr.select_pathway(s, s.get_prop("coherence_pathway"))
+                .mean("nScans")
+                .sum(direct)
             )
             phase_dims = [j for j in s.dimlabels if j.startswith("ph")]
             s.ift(phase_dims)
@@ -69,37 +90,52 @@ with psd.figlist_var() as fl:
                 fl.image(s)
             # }}}
         np.squeeze(s)
-        s,_ = prscr.rough_table_of_integrals(s, fl=fl)
-        s_int = s.C.mean("nScans")
+        s, _ = prscr.rough_table_of_integrals(s, fl=fl)
+        if "nScans" in s.dimlabels:
+            s_int = s.C.mean("nScans")
+        else:
+            s_int = s.C
         print(s_int)
         thiscolor = next(color_cycle)
-        Mi,R1,vd = sympy.symbols("M_inf R_1 vd",real=True)
-        psd.logger.debug(psd.strm("acq keys",s.get_prop('acq_params')))
-        W = (s.get_prop('acq_params')['FIR_rep']*1e-6
-                +
-                s.get_prop('acq_params')['acq_time_ms']*1e-3)
-        functional_form = Mi*(1-(2-sympy.exp(-W*R1))*sympy.exp(-vd*R1))
-        IR_data = psd.nddata(s_int.C.data,['vd'])
-        IR_data.setaxis('vd',s_int.getaxis('vd'))
+        Mi, R1, vd = sympy.symbols("M_inf R_1 vd", real=True)
+        psd.logger.debug(psd.strm("acq keys", s.get_prop("acq_params")))
+        W = (
+            s.get_prop("acq_params")["FIR_rep"] * 1e-6
+            + s.get_prop("acq_params")["acq_time_ms"] * 1e-3
+        )
+        functional_form = Mi * (
+            1 - (2 - sympy.exp(-W * R1)) * sympy.exp(-vd * R1)
+        )
+        IR_data = psd.nddata(s_int.C.data, ["vd"])
+        IR_data.setaxis("vd", s_int.getaxis("vd"))
         f = psd.lmfitdata(IR_data)
         f.functional_form = functional_form
         f.set_guess(
-                M_inf = dict(value=3.9e4, min=0, max=2e7),
-                R_1 = dict(value=0.8, min=0.01, max=100)
-                )
-        fl.next('IR fit - before norm')
-        fl.plot(s_int, 'o', color=thiscolor, label='%s'%nodename)
+            M_inf=dict(value=3.9e4, min=0, max=2e7),
+            R_1=dict(value=0.8, min=0.01, max=100),
+        )
+        fl.next("IR fit - before norm")
+        fl.plot(s_int, "o", color=thiscolor, label="%s" % nodename)
         f.fit()
-        T1 = 1./f.output('R_1')
-        Mi = f.output('M_inf')
+        T1 = 1.0 / f.output("R_1")
+        Mi = f.output("M_inf")
         T1_list.append(T1)
-        f.set_units('vd','s')
+        f.set_units("vd", "s")
         fit = f.eval(100)
-        fl.plot(fit, ls = '-', color=thiscolor,alpha=0.5,
-                label = 'fit for %s'%nodename)
-        fl.next('IR fit - before norm - %s'%nodename)
-        fl.plot(s_int, 'o', color=thiscolor, label='%s'%nodename)
-        fl.plot(fit, ls = '-', color=thiscolor,alpha=0.5,
-                label = 'fit for %s'%nodename)
-        ax=plt.gca()
-        
+        fl.plot(
+            fit,
+            ls="-",
+            color=thiscolor,
+            alpha=0.5,
+            label="fit for %s" % nodename,
+        )
+        fl.next("IR fit - before norm - %s" % nodename)
+        fl.plot(s_int, "o", color=thiscolor, label="%s" % nodename)
+        fl.plot(
+            fit,
+            ls="-",
+            color=thiscolor,
+            alpha=0.5,
+            label="fit for %s" % nodename,
+        )
+        ax = plt.gca()
