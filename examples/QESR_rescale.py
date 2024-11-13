87502c127434541dfeedc50d354cafa789d79ae9
Alex Guinness <aguinnes@syr.edu>
2024-11-13 17:33:26 -0500

    ran black

--- examples/QESR_rescale.py
+++ examples/QESR_rescale.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 
 file_loc = "francklab_esr/alex"
 fig, ax_list = plt.subplots(1, 2)
-calib_info = "220720" # QESR_scalefactor takes the propfactor and diameter from
-#                       my config file and I want to use the values that I've 
+calib_info = (  # QESR_scalefactor takes the propfactor and diameter from
+    "220720"
+)
+#                       my config file and I want to use the values that I've
 #                       designated with the label 220720
 with psd.figlist_var() as fl:
     d = psd.find_file("220922_70mM_270deg_teflon.DSC", exp_type=file_loc)
@@ -14,7 +16,8 @@ with psd.figlist_var() as fl:
     fl.next("QESR rescaling", fig=fig)
     fl.plot(d, ax=ax_list[0])
     ax_list[0].set_title("Raw ESR spectra")
-    d /= QESR_scalefactor(d, calibration_name=calib_info, 
-            diameter_name=calib_info)
+    d /= QESR_scalefactor(
+        d, calibration_name=calib_info, diameter_name=calib_info
+    )
     fl.plot(d, ax=ax_list[1])
     ax_list[1].set_title("Rescaled ESR spectra")
