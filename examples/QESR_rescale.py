1e44f44ed609300f12901224bba6aa32b5b03fc0
Alex Guinness <aguinnes@syr.edu>
2024-11-13 17:28:58 -0500

    example runs

--- examples/QESR_rescale.py
+++ examples/QESR_rescale.py
@@ -1,17 +1,16 @@
-from pyspecdata import *
-from pyspecProcScripts import *
+import pyspecdata as psd
 from pyspecProcScripts import QESR_scalefactor
-file_loc = 'francklab_esr/alex'
-fieldaxis = '$B_0$'
-background = find_file('220922_water.DSC', exp_type = file_loc)['harmonic',0]
-with figlist_var() as fl:
-    d = find_file('220922_70mM_270deg_teflon.DSC', exp_type = file_loc)
-    if 'harmonic' in d.dimlabels:
-        d = d['harmonic',0]
-    color = d.get_plot_color()
-    fl.next("Raw QESR")
-    fl.plot(d,'o',color = color, alpha = 0.5)
-    fl.next("Rescaled")
-    d /= QESR_scalefactor(d, calibration_name = "220720")
-    fl.plot(d)
-    fl.show()
+import matplotlib.pyplot as plt
+
+file_loc = "francklab_esr/alex"
+fig, ax_list = plt.subplots(1, 2)
+with psd.figlist_var() as fl:
+    d = psd.find_file("220922_70mM_270deg_teflon.DSC", exp_type=file_loc)
+    if "harmonic" in d.dimlabels:
+        d = d["harmonic", 0]
+    fl.next("QESR rescaling", fig=fig)
+    fl.plot(d, ax=ax_list[0])
+    ax_list[0].set_title("Raw ESR spectra")
+    d /= QESR_scalefactor(d, calibration_name="220720", diameter_name="220720")
+    fl.plot(d, ax=ax_list[1])
+    ax_list[1].set_title("Rescaled ESR spectra")
