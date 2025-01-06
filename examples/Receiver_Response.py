162e9d9f89491db802f255ade61a20be3183ad5a
Alex Guinness <aguinnes@syr.edu>
2025-01-06 14:20:30 -0500

    version 2

--- examples/Receiver_Response.py
+++ examples/Receiver_Response.py
@@ -26,22 +26,29 @@ import re
 
 file1 = "240123_10mV_AFG_GDS_5mV_100MSPS_analytic.h5"
 file2 = "240117_afg_sc_10mV_3p9kHz_zoom.h5"
+lambda_G = 0.4e3  # Width for Gaussian convolution
 # {{{ Calculate input power (acquired on Oscilloscope)
 #     To determine the input power, just take a capture of the signal in the
 #     time domain and fit to a complex exponential
-all_nodes = psd.find_file(
+# There are less nodes acquired for the control case so I need to separately
+# define them
+file1_nodes = psd.find_file(
     re.escape(file1),
+    exp_type="ODNP_NMR_comp/noise_tests",
+    return_list=True,
+)
+file2_nodes = psd.find_file(
+    re.escape(file2),
     exp_type="ODNP_NMR_comp/noise_tests",
     return_list=True,
 )
 # {{{ Sort node names based on frequency output
 #     by source
-frqs_str_kHz = sorted(all_nodes, key=lambda x: int(x.split("_")[1]))
+frqs_str_kHz = sorted(file1_nodes, key=lambda x: int(x.split("_")[1]))
 # }}}
-# {{{ Make empty nddata to drop the calculated
-#     $V^{2}$ into with corresponding frequency
-#     ($\nu$) output by AFG source, and set the
-#     frequencies based on the node names
+# {{{ Make empty nddata to drop the calculated $V^{2}$ into with corresponding
+#     frequency ($\nu$) output by AFG source, and set the frequencies based on
+#     the node names
 control = psd.ndshape([len(frqs_str_kHz)], ["nu"]).alloc()
 control.setaxis(
     "nu",
@@ -74,21 +81,19 @@ for j, nodename in enumerate(frqs_str_kHz):
     V_amp = f.output("A")
     control["nu", j] = V_amp**2
 # {{{ make spline for power going into RX box
-t_Pin_cs = CubicSpline(np.array(control.getaxis("nu")), control.data)
+Pin_spline = control.spline_lambda()
 # }}}
 # }}}
 # {{{ Calculate $dg^2$
-lambda_G = 0.4e3  # Width for Gaussian convolution
-all_nodes = psd.find_file(
-    re.escape(file2),
-    exp_type="ODNP_NMR_comp/noise_tests",
-    return_list=True,
-)
+frq_kHz = np.array([int(j.split("_")[1]) for j in file2_nodes])
+rec_data = (
+        psd.ndshape([len(file2_nodes)],
+        ["nu_test"]).alloc().set_units("nu_test","Hz")
+        )
+rec_data["nu_test"] = frq_kHz
 # Lists for calculated dg^2, frequency output by source, and
 # the offset of the output frequency and $\nu_{RX,LO}$
-rec_data = []
-nu_test_kHz = []  # $\nu_{test}$
-for j, nodename in enumerate(all_nodes):
+for j, nodename in enumerate(file2_nodes):
     d = psd.find_file(
         file2,
         exp_type="ODNP_NMR_comp/noise_tests",
@@ -97,53 +102,54 @@ for j, nodename in enumerate(all_nodes):
     carrier = (
         d.get_prop("acq_params")["carrierFreq_MHz"] * 1e6
     )  # $\nu_{RX,LO}$
-    rec_data.append(d)
     SW = str(d.get_prop("acq_params")["SW_kHz"])
-    nu_test_kHz.append(d.get_prop("afg_frq"))
-nu_test_kHz.sort()
-# Make additional dimension to store the frequencies of test
-# signal
-rec_data = psd.concat(rec_data, "nu_test")
-rec_data["nu_test"] = np.array(nu_test_kHz) * 1e3
-# }}}
-rec_data.sort("nu_test")
-rec_data.set_units("t", "s")
-rec_data.set_units("nu_test", "s")
-rec_data.rename(
-    "nScans", "capture"
-)  # To be more consistent with the oscilloscope data rename
-#    the nScans dimension
-acq_time = np.diff(rec_data.getaxis("t")[np.r_[0, -1]]).item()
-rec_data.ft("t", shift=True)  # $dg\sqrt{s/Hz}$
-rec_data = abs(rec_data) ** 2  # $dg^{2}*s/Hz$
-rec_data.mean("capture")
-rec_data /= acq_time  # $dg^{2}/Hz$
-# Convolve using $\lambda_{G}$ specified above
-rec_data.convolve("t", lambda_G, enforce_causality=False)
-rec_data.ift("t")
-rec_data.run(np.conj)  # Empirically needed to give offset
-#                        that increases with field
-rec_data.ft("t")
-rec_data["t"] += carrier
-# }}}
-# {{{ Calculate power of test signal (Eq. S3)
-rec_data.run(np.max, "t")
-rec_data *= (lambda_G / (2 * np.sqrt(np.log(2)))) * np.sqrt(np.pi)
-# }}}
+    d.set_units("t", "s")
+    d.rename(
+        "nScans", "capture"
+    )  # To be more consistent with the oscilloscope data rename
+    #    the nScans dimension
+    acq_time = np.diff(d.getaxis("t")[np.r_[0, -1]]).item()
+    d.ft("t", shift=True)  # $dg\sqrt{s/Hz}$
+    d = abs(d) ** 2  # $dg^{2}*s/Hz$
+    d.mean("capture")
+    d /= acq_time  # $dg^{2}/Hz$
+    # Convolve using $\lambda_{G}$ specified above
+    d.convolve("t", lambda_G, enforce_causality=False)
+    d.ift("t")
+    d.run(np.conj)  # Empirically needed to give offset
+    #                        that increases with field
+    d.ft("t")
+    d["t"] += carrier
+    # }}}
+    # {{{ Calculate power of test signal (Eq. S3)
+    d.run(np.max, "t")
+    d *= (lambda_G / (2 * np.sqrt(np.log(2)))) * np.sqrt(np.pi)
+    # }}}
+    rec_data["nu_test",j] = d
 # set x axis to $\Delta\nu$ and rename accordingly
 rec_data.rename("nu_test", "nu_offset")
-rec_data["nu_offset"] -= carrier
+print(carrier)
+print(rec_data["nu_offset"])
+rec_data["nu_offset"] -= int(carrier)
 Dnu = np.linspace(
-    (carrier / 1e3) - (rec_data.getaxis("nu_offset")[-1] / 2),
-    (carrier / 1e3) + (rec_data.getaxis("nu_offset")[-1] / 2),
+    (carrier) - (rec_data.getaxis("nu_offset")[-1] / 2),
+    (carrier) + (rec_data.getaxis("nu_offset")[-1] / 2),
     len(rec_data.getaxis("nu_offset")),
 )
+print(Dnu)
 # }}}
 # {{{ Calculate receiver response as function of frequencies
-P_in = t_Pin_cs(Dnu)  # Generate spline of input powers
+P_in = Pin_spline(Dnu)  # Generate spline of input powers
 dig_filter = rec_data / P_in  # $dg^2/V_{in}^2$
-print(dig_filter["nu_offset"])
 # }}}
+with psd.figlist_var() as fl:
+    fl.next("P in")
+    P_in.human_units(scale_data=True)
+    fl.plot(P_in)
+    fl.next("test")
+    dig_filter.human_units(scale_data=True)
+    fl.plot(dig_filter,'o')
+    fl.show();quit()
 # {{{ Fit receiver response
 A, omega, delta_nu, nu_offset = symbols(
     "A omega delta_nu nu_offset", real=True
