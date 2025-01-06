7dcd01da5274139217d5eabff6fb32209cb56ca3
Alex Guinness <aguinnes@syr.edu>
2025-01-06 16:02:49 -0500

    AG cleanup and tidy

--- examples/Receiver_Response.py
+++ examples/Receiver_Response.py
@@ -24,41 +24,47 @@ from scipy.interpolate import CubicSpline
 import sympy as sp
 import re
 
+def for_plot(thisdata):
+    """ Converts :math:`dg^{2}/V^{2}` to :math:`dg/\mu V`
+    and sets labels accordingly for pretty plotting"""
+    # Take sqrt for units of $dg/V$
+    thisdata.data = np.sqrt(thisdata.data)
+    # Units of $dg/\mu V$
+    thisdata /= 1e6
+    thisdata.rename("nu_test",r"$\Delta \nu$")
+    thisdata.name(r"$\mathrm{dg_{%s\ \mathrm{kHz}}}/ \mu V$" % SW)
+    return thisdata
 file1 = "240123_10mV_AFG_GDS_5mV_100MSPS_analytic.h5"
 file2 = "240117_afg_sc_10mV_3p9kHz_zoom.h5"
 lambda_G = 0.4e3  # Width for Gaussian convolution
-# {{{ Calculate input power (acquired on Oscilloscope)
-#     To determine the input power, just take a capture of the signal in the
-#     time domain and fit to a complex exponential
-# There are less nodes acquired for the control case so I need to separately
-# define them
+# There are less nodes acquired for the control case (since we assume it's
+# relatively flat) so I need to separately define them
 file1_nodes = psd.find_file(
     re.escape(file1),
     exp_type="ODNP_NMR_comp/noise_tests",
     return_list=True,
 )
+control_frqs_kHz = np.array([float(j.split("_")[1]) for j in file1_nodes])
 file2_nodes = psd.find_file(
     re.escape(file2),
     exp_type="ODNP_NMR_comp/noise_tests",
     return_list=True,
 )
-# {{{ Sort node names based on frequency output
-#     by source
-frqs_str_kHz = sorted(file1_nodes, key=lambda x: int(x.split("_")[1]))
-# }}}
+# $\nu_{test}$
+resp_frq_kHz = np.array([float(j.split("_")[1]) for j in file2_nodes])
+# {{{ Calculate input power (acquired on Oscilloscope)
+#     To determine the input power, just take a capture of the signal in the
+#     time domain and fit to a complex exponential
 # {{{ Make empty nddata to drop the calculated $V^{2}$ into with corresponding
 #     frequency ($\nu$) output by AFG source, and set the frequencies based on
 #     the node names
-control = psd.ndshape([len(frqs_str_kHz)], ["nu"]).alloc()
-control.setaxis(
-    "nu",
-    np.array(list(int(j.split("_")[1]) * 1e3 for j in frqs_str_kHz)),
-).set_units("nu", "Hz")
+control = psd.ndshape([len(control_frqs_kHz)], ["nu_test"]).alloc()
+control.setaxis("nu_test", control_frqs_kHz).set_units("nu_test", "Hz")
 # }}}
-for j, nodename in enumerate(frqs_str_kHz):
-    rf_frq = control["nu"][j]
+for j, nodename in enumerate(file1_nodes):
+    rf_frq = control["nu_test"][j]
     d = psd.find_file(
-        re.escape(file1),
+        file1,
         expno=nodename,
         exp_type="ODNP_NMR_comp/noise_tests",
     )
@@ -79,20 +85,13 @@ for j, nodename in enumerate(frqs_str_kHz):
     fit = f.eval()
     # }}}
     V_amp = f.output("A")
-    control["nu", j] = V_amp**2
+    control["nu_test", j] = V_amp**2
 # {{{ make spline for power going into RX box
+control.sort("nu_test")
 Pin_spline = control.spline_lambda()
 # }}}
 # }}}
 # {{{ Calculate $dg^2$
-frq_kHz = np.array([int(j.split("_")[1]) for j in file2_nodes])
-rec_data = (
-        psd.ndshape([len(file2_nodes)],
-        ["nu_test"]).alloc().set_units("nu_test","Hz")
-        )
-rec_data["nu_test"] = frq_kHz
-# Lists for calculated dg^2, frequency output by source, and
-# the offset of the output frequency and $\nu_{RX,LO}$
 for j, nodename in enumerate(file2_nodes):
     d = psd.find_file(
         file2,
@@ -102,59 +101,52 @@ for j, nodename in enumerate(file2_nodes):
     carrier = (
         d.get_prop("acq_params")["carrierFreq_MHz"] * 1e6
     )  # $\nu_{RX,LO}$
+    if j == 0:
+        # Allocate and array that's shaped like the data acquired
+        # for one frequency but add axis to store the frequency of 
+        # the test signal frequency
+        rec_data1 = (d.shape + ("nu_test", len(resp_frq_kHz))).alloc()
+        rec_data1.setaxis("t", d.getaxis("t")).set_units("t", "s")
+        rec_data1.setaxis("nu_test", resp_frq_kHz).set_units("nu_test", "Hz")
+    rec_data1["nu_test", j] = d
     SW = str(d.get_prop("acq_params")["SW_kHz"])
-    d.set_units("t", "s")
-    d.rename(
-        "nScans", "capture"
-    )  # To be more consistent with the oscilloscope data rename
-    #    the nScans dimension
-    acq_time = np.diff(d.getaxis("t")[np.r_[0, -1]]).item()
-    d.ft("t", shift=True)  # $dg\sqrt{s/Hz}$
-    d = abs(d) ** 2  # $dg^{2}*s/Hz$
-    d.mean("capture")
-    d /= acq_time  # $dg^{2}/Hz$
-    # Convolve using $\lambda_{G}$ specified above
-    d.convolve("t", lambda_G, enforce_causality=False)
-    d.ift("t")
-    d.run(np.conj)  # Empirically needed to give offset
-    #                        that increases with field
-    d.ft("t")
-    d["t"] += carrier
-    # }}}
-    # {{{ Calculate power of test signal (Eq. S3)
-    d.run(np.max, "t")
-    d *= (lambda_G / (2 * np.sqrt(np.log(2)))) * np.sqrt(np.pi)
-    # }}}
-    rec_data["nu_test",j] = d
-# set x axis to $\Delta\nu$ and rename accordingly
-rec_data.rename("nu_test", "nu_offset")
-print(carrier)
-print(rec_data["nu_offset"])
-rec_data["nu_offset"] -= int(carrier)
-Dnu = np.linspace(
-    (carrier) - (rec_data.getaxis("nu_offset")[-1] / 2),
-    (carrier) + (rec_data.getaxis("nu_offset")[-1] / 2),
-    len(rec_data.getaxis("nu_offset")),
-)
-print(Dnu)
+rec_data1.sort("nu_test")
+rec_data1.rename("nScans", "capture")  # To be more consistent with the
+#                                         oscilloscope data rename the nScans
+#                                         dimension
+acq_time = np.diff(rec_data1.getaxis("t")[np.r_[0, -1]]).item()
+# {{{ Calculate PSD for each frequency
+rec_data1.ft("t", shift=True)  # $dg\sqrt{s/Hz}$
+rec_data1 = abs(rec_data1) ** 2  # $dg^{2}*s/Hz$
+rec_data1.mean("capture")
+rec_data1 /= acq_time  # $dg^{2}/Hz$
+# Convolve using $\lambda_{G}$ specified above
+rec_data1.convolve("t", lambda_G, enforce_causality=False)
+rec_data1.ift("t")
+rec_data1.run(np.conj)  # Empirically needed to give offset
+#                        that increases with field
+rec_data1.ft("t")
+rec_data1["t"] += carrier
+# }}}
+# {{{ Calculate power of test signal (Eq. S3)
+rec_data1.run(np.max, "t")
+rec_data1 *= (lambda_G / (2 * np.sqrt(np.log(2)))) * np.sqrt(2 * np.pi)
+# }}}
 # }}}
 # {{{ Calculate receiver response as function of frequencies
-P_in = Pin_spline(Dnu)  # Generate spline of input powers
-dig_filter = rec_data / P_in  # $dg^2/V_{in}^2$
-# }}}
-with psd.figlist_var() as fl:
-    fl.next("P in")
-    P_in.human_units(scale_data=True)
-    fl.plot(P_in)
-    fl.next("test")
-    dig_filter.human_units(scale_data=True)
-    fl.plot(dig_filter,'o')
-    fl.show();quit()
-# {{{ Fit receiver response
-A, omega, delta_nu, nu_offset = symbols(
-    "A omega delta_nu nu_offset", real=True
+# Make axis of finely spaced frequencies to feed to spline
+rec_data1["nu_test"] -= carrier
+Dnu = np.linspace(
+    (carrier) - (rec_data1.getaxis("nu_test")[-1] / 2),
+    (carrier) + (rec_data1.getaxis("nu_test")[-1] / 2),
+    len(rec_data1.getaxis("nu_test")),
 )
-func_form = A * sp.sinc((2 * np.pi * (nu_offset - omega)) / (delta_nu)) ** 2
+P_in = Pin_spline(Dnu)  # Generate spline of input powers
+dig_filter = rec_data1 / P_in
+# }}}
+# {{{ Fit receiver response
+A, omega, delta_nu, nu_test = symbols("A omega delta_nu nu_test", real=True)
+func_form = A * sp.sinc((2 * np.pi * (nu_test - omega)) / (delta_nu)) ** 2
 f = psd.lmfitdata(dig_filter)
 f.functional_form = func_form
 f.set_guess(
@@ -165,14 +157,7 @@ f.set_guess(
 f.fit()
 fit = f.eval()
 # }}}
-# Take sqrt for units of $dg/V$
-dig_filter.data = np.sqrt(dig_filter.data)
-fit.data = np.sqrt(fit.data)
-dig_filter /= 1e6  # Units of $dg/\mu V$
-fit /= 1e6  # Units of $dg/\mu V$
 with psd.figlist_var() as fl:
     fl.next("Receiver Response")
-    fl.plot(dig_filter, "o")
-    fl.plot(fit, color="red", alpha=0.5)
-    plt.xlabel(r"$\Delta \nu$ / kHz")
-    plt.ylabel(r"$\mathrm{dg_{%s\ \mathrm{kHz}}}/ \mu V$" % SW)
+    fl.plot(for_plot(dig_filter), "o")
+    fl.plot(for_plot(fit), color="red", alpha=0.5)
