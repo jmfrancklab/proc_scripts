3b38a144ff3443dd25236c35b49bec55ac2b7ec1
Alex Guinness <aguinnes@syr.edu>
2025-02-13 11:27:36 -0500

    clean up for flake

--- examples/integration_with_error.py
+++ examples/integration_with_error.py
@@ -1,151 +1,117 @@
 """
-Check Integration
-=================
+Check Integration_w_error Function
+==================================
 
-Makes sure that automatically chosen integral bounds perform similar to or
+Generates a fake dataset of an inversion recovery w/ normally distributed
+random noise.
+Then checks that automatically chosen integral bounds perform similar to or
 better than what you would choose by hand.
 """
-from pylab import *
-from pyspecdata import *
-from pyspecProcScripts import integrate_limits, integral_w_errors
+import pyspecdata as psd
+from pyspecProcScripts import integral_w_errors, select_pathway
 from numpy.random import seed
+import numpy as np
+from numpy import r_
+from pylab import rcParams
 
 rcParams["image.aspect"] = "auto"  # needed for sphinx gallery
 # sphinx_gallery_thumbnail_number = 2
 
-seed(
-    2021
-)  # so the same random result is generated every time -- 2021 is meaningless
-init_logging(level="debug")
-fl = figlist_var()
-t2 = nddata(r_[0:1:1024j], "t2")
-vd = nddata(r_[0:1:40j], "vd")
-ph1 = nddata(r_[0, 2] / 4.0, "ph1")
-ph2 = nddata(r_[0:4] / 4.0, "ph2")
+seed(2021)  # so the same random result is generated every time -- 2021 is
+#             meaningless
+t2 = psd.nddata(r_[0:1:1024j], "t2")
+vd = psd.nddata(r_[0:1:40j], "vd")
+ph1 = psd.nddata(r_[0, 2] / 4.0, "ph1")
+ph2 = psd.nddata(r_[0:4] / 4.0, "ph2")
 signal_pathway = {"ph1": 0, "ph2": 1}
-excluded_pathways = [(0, 0), (0, 3)]
 manual_slice = (60, 140)  # manually chosen integration bounds
-# this generates fake data w/ a T₂ of 0.2s
-# amplitude of 21, just to pick a random amplitude
-# offset of 300 Hz, FWHM 10 Hz
+# {{{ Generate fake IR dataset
+# This generates fake data w/ a T₂ of 0.2s amplitude of 21, just to pick a
+# random amplitude offset of 300 Hz, FWHM 10 Hz
 data = (
-    21 * (1 - 2 * exp(-vd / 0.2)) * exp(+1j * 2 * pi * 100 * t2 - t2 * 10 * pi)
+    21 * (1 - 2 * np.exp(-vd / 0.2)) * np.exp(
+        +1j * 2 * np.pi * 100 * t2 - t2 * 10 * np.pi)
 )
-data *= exp(signal_pathway["ph1"] * 1j * 2 * pi * ph1)
-data *= exp(signal_pathway["ph2"] * 1j * 2 * pi * ph2)
+data *= np.exp(signal_pathway["ph1"] * 1j * 2 * np.pi * ph1)
+data *= np.exp(signal_pathway["ph2"] * 1j * 2 * np.pi * ph2)
 data["t2":0] *= 0.5
 fake_data_noise_std = 2.0
 data.add_noise(fake_data_noise_std)
 data.reorder(["ph1", "ph2", "vd"])
-# at this point, the fake data has been generated
+# }}}
+# {{{ unitary FT
 data.ft(["ph1", "ph2"])
-fl.next("what does a usual error bar look like?")
-just_noise = nddata(r_[0:1:50j], "t")
-just_noise.data *= 0
-just_noise.add_noise(fake_data_noise_std)
-just_noise.set_error(fake_data_noise_std)
-fl.plot(just_noise, ".", capsize=6)
-# {{{ usually, we don't use a unitary FT -- this makes it unitary
-data /= 0.5 * 0.25  # the dt in the integral for both dims
-data /= sqrt(ndshape(data)["ph1"] * ndshape(data)["ph2"])  # normalization
+data /= 0.5 * 0.25
+data /= np.sqrt(
+    psd.ndshape(data)["ph1"] * psd.ndshape(data)["ph2"]
+)  # Normalization
 # }}}
 data.ft("t2", shift=True)
 dt = data.get_ft_prop("t2", "dt")
-# {{{ vector-normalize the FT
-data /= sqrt(ndshape(data)["t2"]) * dt
+# {{{ Vector-normalize the FT
+data /= np.sqrt(psd.ndshape(data)["t2"]) * dt
 # }}}
-# {{{ First, run the code that automatically chooses integration bounds
-# and also assigns error
-error_pathway = (
-    set(
-        (
-            (j, k)
-            for j in range(ndshape(data)["ph1"])
-            for k in range(ndshape(data)["ph2"])
+with psd.figlist_var() as fl:
+    # {{{ First, run the code that automatically chooses integration bounds,
+    #     integrates and assigns error
+    s_int, returned_frq_slice = integral_w_errors(
+        data, signal_pathway, fl=fl, return_frq_slice=True
+    )
+    fl.next("compare manual vs. automatic", legend=True)
+    fl.plot(s_int, ".", label="fully auto: real", capsize=6)
+    fl.plot(s_int.imag, ".", label="fully auto: imaginary", capsize=6)
+    # }}}
+    print("check the std after FT", np.std(data["ph1", 0]["ph2", 0].data.real))
+    fl.next("compare manual vs. automatic")
+    # Run a controlled comparison between manually chosen integration bounds
+    # and compare against automatically generated 
+    # Leave this as a loop so user can experiment with different bounds
+    # As noted in issue #44 , manually chosen bounds underperform 
+    for bounds, thislabel in [
+        (manual_slice, "manual bounds"),
+        (tuple(returned_frq_slice), "auto bounds"),
+    ]:
+        manual_bounds = select_pathway(data["t2":bounds],signal_pathway)
+        assert manual_bounds.get_ft_prop("t2")
+        # Check that the noise in an off coherence pathway correctly matches
+        # the preset noise_std from above
+        std_from_off_pathway = (
+            data["ph1", 0]["ph2", 0]["t2":bounds]
+            .C.run(lambda x: abs(x) ** 2 / 2)
+            .mean_all_but(["t2", "vd"])
+            .mean("t2")
+            .run(np.sqrt)
         )
-    )
-    - set(excluded_pathways)
-    - set([(signal_pathway["ph1"], signal_pathway["ph2"])])
-)
-error_pathway = [{"ph1": j, "ph2": k} for j, k in error_pathway]
-s_int, returned_frq_slice = integral_w_errors(
-    data, signal_pathway, error_pathway, fl=fl, return_frq_slice=True
-)
-fl.next("compare manual vs. automatic", legend=True)
-fl.plot(s_int, ".", label="fully auto: real", capsize=6)
-fl.plot(s_int.imag, ".", label="fully auto: imaginary", capsize=6)
-# }}}
-logger.debug(
-    strm("check the std after FT", std(data["ph1", 0]["ph2", 0].data.real))
-)
-# the sqrt on the next line accounts for the var(real)+var(imag)
-fl.next("raw data")
-fl.image(data, alpha=0.5)
-fl.next("real part of raw data")
-fl.image(data.real, alpha=0.5)
-fl.next("compare manual vs. automatic")
-# run a controlled comparison between manually chosen integration bounds and
-# compare against automatically generated
-# as noted in issue #44 , manually chosen bounds underperform
-for bounds, thislabel in [
-    (
-        manual_slice,
-        "manual bounds",
-    ),  # leave this as a loop so user can experiment with different bounds
-    (
-        tuple(returned_frq_slice),
-        "auto bounds",
-    ),  # leave this as a loop so user can experiment with different bounds
-]:
-    manual_bounds = data["ph1", 0]["ph2", 1]["t2":bounds]
-    assert manual_bounds.get_ft_prop("t2")
-    std_from_00 = (
-        data["ph1", 0]["ph2", 0]["t2":bounds]
-        .C.run(lambda x: abs(x) ** 2 / 2)
-        .mean_all_but(["t2", "vd"])
-        .mean("t2")
-        .run(sqrt)
-    )
-    logger.debug(
-        strm(
-            "here is the std calculated from an off pathway",
-            std_from_00,
+        print(
+            "here is the average of the std calculated from an off pathway",
+            np.mean(std_from_off_pathway.data),
             "does it match",
             fake_data_noise_std,
             "?",
         )
-    )
-    N = ndshape(manual_bounds)["t2"]
-    df = manual_bounds.get_ft_prop("t2", "df")
-    logger.debug(
-        strm(ndshape(manual_bounds), "df is", df, "N is", N, "N*df is", N * df)
-    )
-    manual_bounds.integrate("t2")
-    # N terms that have variance given by fake_data_noise_std**2 each multiplied by df
-    # the 2 has to do w/ real/imag/abs -- see check_integration_error
-    propagated_variance = N * df**2 * fake_data_noise_std**2
-    propagated_variance_from_inactive = N * df**2 * std_from_00**2
-    logger.debug(
-        strm(
-            "manually calculated integral error is", sqrt(propagated_variance)
+        # Manually calculate variance of data with manually set bounds
+        N = psd.ndshape(manual_bounds)["t2"]
+        df = manual_bounds.get_ft_prop("t2", "df")
+        manual_bounds.integrate("t2")
+        propagated_variance = N * df**2 * fake_data_noise_std**2
+        propagated_variance_from_inactive = N * df**2 * std_from_off_pathway**2
+        manual_bounds.set_error(np.sqrt(propagated_variance))
+        fl.plot(
+            manual_bounds,
+            ".",
+            capsize=6,
+            label="%s (programmed σ)\n$%4g\\rightarrow%4g$"
+            % ((thislabel,) + bounds),
+            alpha=0.5,
         )
-    )
-    manual_bounds.set_error(sqrt(propagated_variance))
-    fl.plot(
-        manual_bounds,
-        ".",
-        capsize=6,
-        label="%s (programmed σ)\n$%4g\\rightarrow%4g$"
-        % ((thislabel,) + bounds),
-        alpha=0.5,
-    )
-    manual_bounds.set_error(sqrt(propagated_variance_from_inactive.data))
-    fl.plot(
-        manual_bounds,
-        ".",
-        capsize=6,
-        label="%s (inactive CT σ)\n$%4g\\rightarrow%4g$"
-        % ((thislabel,) + bounds),
-        alpha=0.5,
-    )
-fl.show()
+        manual_bounds.set_error(
+            np.sqrt(propagated_variance_from_inactive.data)
+        )
+        fl.plot(
+            manual_bounds,
+            ".",
+            capsize=6,
+            label="%s (inactive CT σ)\n$%4g\\rightarrow%4g$"
+            % ((thislabel,) + bounds),
+            alpha=0.5,
+        )
--- pyspecProcScripts/integral_w_error.py
+++ pyspecProcScripts/integral_w_error.py
@@ -4,61 +4,60 @@ from .simple_functions import select_pathway
 import pyspecProcScripts as psdpr
 import logging
 import numpy as np
-from numpy import r_
+
 
 def masked_mean_multi(x, axis=None):
     "Calculates the mean on a 1D axis"
     assert axis is not None
+
     def masked_mean(x):
-        "this only works for 1D data"
         return np.mean(x[np.isfinite(x)])
-    return np.apply_along_axis(masked_mean,axis,x)
+
+    return np.apply_along_axis(masked_mean, axis, x)
+
+
 def masked_var_multi(x, var_has_imag=False, axis=None):
-    "calculates the variance along a 1D axis"
     assert axis is not None
+
     def masked_var(x):
-        "this only works for 1D data"
         if var_has_imag:
             return np.var(x[np.isfinite(x)], ddof=1) / 2
         else:
-            return np.var(x[np.isfinite(x)],ddof=1)
+            return np.var(x[np.isfinite(x)], ddof=1)
+
     return np.apply_along_axis(masked_var, axis, x)
+
+
 def integral_w_errors(
     s,
     sig_path,
-    error_path,
     cutoff=0.1,
     convolve_method="Gaussian",
     indirect="vd",
     direct="t2",
     fl=None,
     return_frq_slice=False,
+    has_imag=True,
 ):
     """Calculates the propagation of error for the given signal and returns
     signal with the error associated.
 
-    Before declaring the error_path,
-    look at an examples such as integration_w_error.py to see how to decide which
-    excluded pathways to take the error over.
-
     Parameters
     ==========
     sig_path:   dict
-                Dictionary of the path of the desired signal.
-    error_path: dict
-                Dictionary of all coherence pathways that are
-                not the signal pathway.
-
-                Before declaring the error_path,
-                look at an examples such as integration_w_error.py to see how to decide which
-                excluded pathways to take the error over.
+        Dictionary of the path of the desired signal.
     convolve_method: str
-                method of convolution used in integrating limits
-                passed on to :func:`integrate_limits`
+        method of convolution used in integrating limits passed on to
+        :func:`integrate_limits`
     indirect:   str
-                Indirect axis.
+        Indirect axis.
     direct:     str
-                Direct axis.
+        Direct axis.
+    return_frq_slice: bool
+        If true a tuple of the calculated frequency slice will be returned
+    has_imag: bool
+        When calculating the variance, we must account for whether ther is an
+        imaginary component to the data. If true, the variance is divided by 2.
 
     Returns
     =======
@@ -67,17 +66,22 @@ def integral_w_errors(
              not included in the signal pathway.
     """
     assert s.get_ft_prop(direct), "need to be in frequency domain!"
-    if convolve_method is not None:
-        kwargs = {"convolve_method": convolve_method}
-    else:
-        kwargs = {}
     frq_slice = integrate_limits(
         select_pathway(s, sig_path),
         convolve_method=convolve_method,
         cutoff=cutoff,
-        fl=fl,
-    )
+        fl=fl)
     logging.debug(psp.strm("frq_slice is", frq_slice))
+    f = s.getaxis(direct)
+    df = f[1] - f[0]
+    all_labels = set(s.dimlabels)
+    all_labels -= set([indirect, direct])
+    extra_dims = [j for j in all_labels if not j.startswith("ph")]
+    if len(extra_dims) > 0:
+        raise ValueError(
+            "You have extra (non-phase cycling, non-indirect) dimensions"
+            + str(extra_dims)
+        )
     noise = s.C
     noise[direct:frq_slice] = np.nan
     temp = psdpr.select_pathway(noise, sig_path)
@@ -85,13 +89,16 @@ def integral_w_errors(
     if fl is not None:
         fl.next("frequency noise masked")
         fl.image(noise)
-    noise.run(masked_var_multi, direct)
+    noise.run(masked_var_multi, direct, has_imag)
     for j in [k for k in s.dimlabels if k.startswith("ph")]:
-        noise.run(masked_mean_multi,j)
-    s_int = psdpr.select_pathway(s,sig_path)[direct:frq_slice]    
-    df = s_int.get_ft_prop(direct,"df")
-    N = psp.ndshape(s)[direct]
-    retval = s_int.integrate(direct).set_error(psp.sqrt(noise.data * df**2 * N))
+        noise.run(masked_mean_multi, j)
+    s_int = psdpr.select_pathway(s, sig_path)[direct:frq_slice]
+    N = psp.ndshape(s_int)[direct]
+    s_forerror = noise.data * df**2
+    s_forerror *= N
+    retval = s_int.integrate(direct).set_error(
+        psp.sqrt(noise.data * df**2 * N)
+    )
     if not return_frq_slice:
         return retval
     elif return_frq_slice:
