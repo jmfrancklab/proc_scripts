59d137183bbfd1d95f56147135ff5f9564f06084
Alex Guinness <aguinnes@syr.edu>
2025-02-17 09:10:03 -0500

    review again

--- examples/proc_NMR_covariance.py
+++ examples/proc_NMR_covariance.py
@@ -11,20 +11,31 @@ import numpy as np
 import pyspecProcScripts as psdpr
 import matplotlib.pyplot as plt
 
-signal_pathway = {"ph1": 1}
 d = psd.find_file(
-    "241010_ssProbe_toroid_pmp90_16p7_echo",
+    "250214_27mM_TEMPOL_30scans_echo",
     exp_type="ODNP_NMR_comp/Echoes",
     expno="echo_1",
     lookup=psdpr.lookup_table,
 )
+d.ift("t2")
+# {{{ Phasing
+d["t2"] -= d.getaxis("t2")[0]
+best_shift = psdpr.hermitian_function_test(
+    psdpr.select_pathway(d.C.mean("nScans"), d.get_prop("coherence_pathway"))
+)
+d.setaxis("t2", lambda x: x - best_shift).register_axis({"t2": 0})
+d /= psdpr.zeroth_order_ph(
+    psdpr.select_pathway(d["t2":0], d.get_prop("coherence_pathway"))
+)
+d.ft("t2")
+# }}}
 # {{{ Make copy of data and align to compare with unaligned data
 aligned = d.C
 aligned.ift("ph1")  # go into phase cycling dimension for alignment
 opt_shift, sigma, mask_fn = psdpr.correl_align(
     aligned,
     indirect_dim="nScans",
-    signal_pathway=signal_pathway,
+    signal_pathway=d.get_prop("coherence_pathway"),
 )
 aligned.ift("t2")
 aligned *= np.exp(-1j * 2 * np.pi * opt_shift * aligned.fromaxis("t2"))
@@ -40,10 +51,10 @@ with psd.figlist_var() as fl:
         fl.next("%s" % label, fig=fig)
         ax_list[0].set_title("Covariance-variance matrix of full echo")
         ax_list[1].set_title(
-            "Covariance-variance matrix, zoomed into late times"
+            "Covariance-variance matrix, zoomed into signal"
         )
         ax_list[2].set_title(
-            "Covariance-variance matrix, zoomed into even later times"
+            "Covariance-variance matrix, zoomed into FID tail"
         )
         # }}}
         # Show cov-var matrix of full time domain
@@ -52,7 +63,7 @@ with psd.figlist_var() as fl:
         # FID that's just noise (no shifting frequencies, I get the diagonal
         # covariance that I expect)
         fl.image(
-            abs(thisdata["$t2_i$":(0.6, 0.7)]["$t2_j$":(0.6, 0.7)]),
+            abs(thisdata["$t2_i$":(None, 0.1)]["$t2_j$":(None, 0.1)]),
             ax=ax_list[1],
         )
         fl.image(
