2b3a63136b5899f91655a3938f637d844605f0df
John Franck <jmfranck@jmfDebian.syr.edu>
2025-02-27 12:58:19 -0500

    flake happy

--- pyspecProcScripts/third_level/QESR.py
+++ pyspecProcScripts/third_level/QESR.py
@@ -16,20 +16,22 @@ fieldaxis = "$B_0$"
 # }}}
 
 
-def QESR(file_name, 
-        label, 
-        pushout=0.5,
-        threshold=0.05, 
-        pickle_file=None, 
-        background=None,
-        exp_type=None,
-        which_plot=None, 
-        calibration_name=None,
-        diameter_name=None, 
-        color=None,
-        fl=None):
+def QESR(
+    file_name,
+    label,
+    pushout=0.5,
+    threshold=0.05,
+    pickle_file=None,
+    background=None,
+    exp_type=None,
+    which_plot=None,
+    calibration_name=None,
+    diameter_name=None,
+    color=None,
+    fl=None,
+):
     """Converts a raw ESR spectra to the double integral and calculates the
-    appropriate concentration of spins present. 
+    appropriate concentration of spins present.
 
     If a background spectra is provided, background subtraction is applied to
     the loaded ESR spectra. The double integral is finally rescaled and
@@ -58,7 +60,7 @@ def QESR(file_name,
         Name of the value in your pyspecdata config file that points to
         the proportionality constant
     diameter_name:  str
-        Name of the value in your pyspecdata config file that points to 
+        Name of the value in your pyspecdata config file that points to
         the diameter of the capillary being used
     color:  str
             String of the desired color for the plot. This is especially
@@ -68,7 +70,9 @@ def QESR(file_name,
         required!
     """
     if exp_type is None:
-        raise ValueError("You must specify the location of the file with exp_type!")
+        raise ValueError(
+            "You must specify the location of the file with exp_type!"
+        )
     if which_plot is None:
         which_plot = file_name
     if fl is None:
@@ -90,33 +94,39 @@ def QESR(file_name,
     if "harmonic" in d.dimlabels:
         d = d["harmonic", 0]
     d -= d[fieldaxis, -100:].data.mean()
-    d.setaxis(fieldaxis,
-            lambda x: x-d.get_prop('MWFQ')/gammabar_e*1e4)
+    d.setaxis(fieldaxis, lambda x: x - d.get_prop("MWFQ") / gammabar_e * 1e4)
     # }}}
     if background is None:
-        background = ndshape(d).alloc() # zeros -- keep life easy!
+        background = ndshape(d).alloc()  # zeros -- keep life easy!
         background.copy_axes(d)
     else:
         # {{{ set up the background
         background = background.C
         background -= background[fieldaxis, -100:].data.mean()
-        background.setaxis(fieldaxis,
-                lambda x: x-background.get_prop('MWFQ')/gammabar_e*1e4)
+        background.setaxis(
+            fieldaxis,
+            lambda x: x - background.get_prop("MWFQ") / gammabar_e * 1e4,
+        )
         # }}}
     background = background.interp(fieldaxis, d.getaxis(fieldaxis))
     # {{{ configure all the plots -- I like to do this in one place so I
     #     can easily control placement in the PDF
     thisfig = plt.figure()
-    gs = plt.GridSpec(4,1, figure=thisfig)
-    fl.text(r"\textbf{\texttt{%s}}\par"%file_name)
-    ax = thisfig.add_subplot(gs[:3,0])
+    gs = plt.GridSpec(4, 1, figure=thisfig)
+    fl.text(r"\textbf{\texttt{%s}}\par" % file_name)
+    ax = thisfig.add_subplot(gs[:3, 0])
     fl.next(f"{which_plot} absorption, bg. no bl.", ax=ax, fig=thisfig)
     ax.set_title("abs mode:\nbackground subtracted, show baseline")
-    fl.text(r'\par')
+    fl.text(r"\par")
     fl.next(f"{which_plot} baseline diagnostic")
-    gca().set_title("zoomed-in baseline diagnostic\nshowing only baseline\n(data and fit)")
-    ax_dblint = thisfig.add_subplot(gs[3,0])
-    ax_dblint.set_title(r"$\left(\frac{dblint}{denom}\right)(calibration\ \rightarrow %s)$"%calibration_name)
+    gca().set_title(
+        "zoomed-in baseline diagnostic\nshowing only baseline\n(data and fit)"
+    )
+    ax_dblint = thisfig.add_subplot(gs[3, 0])
+    ax_dblint.set_title(
+        r"$\left(\frac{dblint}{denom}\right)(calibration\ \rightarrow %s)$"
+        % calibration_name
+    )
     # }}}
     if color is None:
         color = next(colors)["color"]
@@ -124,19 +134,26 @@ def QESR(file_name,
     fl.next(f"{which_plot} absorption, bg. no bl.")
     forplot = d.C.integrate(fieldaxis, cumulative=True)
     # show the same thing on the main plot as a faint line
-    fl.plot(forplot, alpha=0.2, label="%s - no background spec. subtracted"%label)
-    d -= background # subtract background in derivative mode
+    fl.plot(
+        forplot, alpha=0.2, label="%s - no background spec. subtracted" % label
+    )
+    d -= background  # subtract background in derivative mode
     d_abs = d.C.integrate(fieldaxis, cumulative=True)
     fl.plot(d_abs, alpha=0.5, label=label)
-    # for a protein, we assume we have 1 broad peak that's always over threshold*max
+    # for a protein, we assume we have 1 broad peak that's always over
+    # threshold*max
     right_side = abs(d_abs.data)[-1]
     # define a peak as anything that rises above threshold*max
     peaklist = d_abs.contiguous(
-        lambda x: abs(x) > right_side + (abs(x).data.max() - right_side) * threshold
+        lambda x: abs(x)
+        > right_side + (abs(x).data.max() - right_side) * threshold
     )
-    # pull the furthest left and furthest right boundaries of any peaks that we find
+    # pull the furthest left and furthest right boundaries of any peaks that we
+    # find
     specrange = (peaklist.ravel().min(), peaklist.ravel().max())
-    generous_limits = specrange + np.diff(specrange).item() * r_[-pushout, +pushout]
+    generous_limits = (
+        specrange + np.diff(specrange).item() * r_[-pushout, +pushout]
+    )
     for j in r_[np.array(specrange)]:
         # show where the peaks were identified
         axvline(x=j, alpha=0.5, color=d.get_plot_color(), ls=":")
@@ -145,7 +162,9 @@ def QESR(file_name,
         axvline(x=j, alpha=0.5, color=d.get_plot_color(), ls="-")
     d_baseline = d_abs[
         fieldaxis,
-        lambda x: np.logical_or(x < generous_limits[0], x > generous_limits[1]),
+        lambda x: np.logical_or(
+            x < generous_limits[0], x > generous_limits[1]
+        ),
     ]
     fl.next(f"{which_plot} baseline diagnostic")
     fl.plot(d_baseline, ".", alpha=0.3, label=label, human_units=False)
@@ -156,18 +175,25 @@ def QESR(file_name,
     # }}}
     fl.plot(polybaseline, alpha=0.5, human_units=False)
     fl.next(f"{which_plot} absorption, bg. no bl.")
-    fl.plot(polybaseline, alpha=0.5, label="spline/poly baseline\n(integrate between this and other curve)", lw=1)
-    d_abs -= polybaseline #background subtraction
+    fl.plot(
+        polybaseline,
+        alpha=0.5,
+        label="spline/poly baseline\n(integrate between this and other curve)",
+        lw=1,
+    )
+    d_abs -= polybaseline  # background subtraction
     d_abs.integrate(fieldaxis, cumulative=True)
-    d_abs /= QESR_scalefactor(d, calibration_name=calibration_name,
-            diameter_name=diameter_name)
+    d_abs /= QESR_scalefactor(
+        d, calibration_name=calibration_name, diameter_name=diameter_name
+    )
     final_conc = (
         d_abs[fieldaxis : (generous_limits[-1], None)].mean(fieldaxis).item()
     ).real
     d_abs.name("conc").set_units("μM")
-    fl.plot(d_abs, alpha=0.5, label=f"{label}, %0.4f μM" % final_conc,
-            ax=ax_dblint)
-    gridandtick(ax_dblint) 
+    fl.plot(
+        d_abs, alpha=0.5, label=f"{label}, %0.4f μM" % final_conc, ax=ax_dblint
+    )
+    gridandtick(ax_dblint)
     if pickle_file is not None:
         pickle_vars[label] = final_conc
         with open(pickle_file, "wb") as fp:
