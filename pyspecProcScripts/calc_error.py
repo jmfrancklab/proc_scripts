4681780c1a9df0b4b803d89b8a279517ca258168
Alex Guinness <aguinnes@syr.edu>
2025-03-17 15:04:52 -0400

    add other mask way

--- pyspecProcScripts/calc_error.py
+++ pyspecProcScripts/calc_error.py
@@ -1,7 +1,34 @@
 import pyspecdata as psd
 from .simple_functions import select_pathway
+import numpy as np
 
 
+# {{{ Functions to calculate var and mean of nan-masked data
+#     Pulled from time_domain_noise.py example
+def masked_mean_multi(x, axis=None):
+    "Calculate the mean of nan-masked data on a 1D axis"
+    assert axis is not None
+
+    def masked_mean(x):
+        return np.mean(x[np.isfinite(x)])
+
+    return np.apply_along_axis(masked_mean, axis, x)
+
+
+def masked_var_multi(x, var_has_imag=False, axis=None):
+    "Calculates the variance along a 1D axis"
+    assert axis is not None
+
+    def masked_var(x):
+        if var_has_imag:  # take average of variance along real and imag
+            return np.var(x[np.isfinite(x)], ddov=1) / 2
+        else:
+            return np.var(x[np.isfinite(x)], ddof=1)
+
+    return np.apply_along_axis(masked_var, axis, x)
+
+
+# }}}
 def calc_error(
     s,
     frq_slice,
@@ -55,6 +82,7 @@ def calc_error(
 
     s = s[direct:frq_slice]
     df = s.get_ft_prop(direct, "df")
+    N = len(s[direct])
     collected_variance = (
         psd.ndshape(
             [psd.ndshape(s)[indirect], len(error_path)], [indirect, "pathways"]
@@ -72,11 +100,8 @@ def calc_error(
             # (by error propagation) where N₂ is the number of points in
             # the indirect dimension
             s_forerror = select_pathway(s, error_path[j])
-            # previous line wipes everything out and starts over -- why not use
-            # collected_variance above, as I had originally set up --> part of
-            # issue #44
             if j == 0:
-                N2 = psd.ndshape(s_forerror)[direct]
+                Nshape = psd.ndshape(s_forerror)[direct]
             # mean divides by N₁ (indirect), integrate multiplies by Δf, and
             # the mean sums all elements (there are N₁N₂ elements)
             s_forerror -= s_forerror.C.mean_all_but([indirect, direct]).mean(
@@ -86,9 +111,21 @@ def calc_error(
                 [direct, indirect]
             ).mean(direct)
             s_forerror *= df**2  # Δf
-            s_forerror *= N2
+            s_forerror *= Nshape
             collected_variance["pathways", j] = s_forerror
         collected_variance = psd.sqrt(
             collected_variance.sum("pathways") / len(error_path)
         )
-        return collected_variance
+    else:
+        collected_variance = s.C  # so we don't alter s
+        collected_variance[direct:frq_slice] = np.nan
+        temp = select_pathway(collected_variance, signal_pathway)
+        temp.data[:] = np.nan
+        if fl is not None:
+            fl.next("Frequency Noise")
+            fl.image(collected_variance)
+        collected_variance.run(masked_var_multi, direct)
+        for j in [k for k in s.dimlabels if k.startswith("ph")]:
+            collected_variance.run(masked_mean_multi, j)
+        collected_variance = psd.sqrt(collected_variance * df**2 * N)
+    return collected_variance
