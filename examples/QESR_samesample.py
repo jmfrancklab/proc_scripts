e6df95966ccbc8344d691f935b3e155d116cab7a
John Franck <jmfranck@jmfDebian.syr.edu>
2025-03-25 23:10:17 -0400

    flake happy

--- examples/QESR_samesample.py
+++ examples/QESR_samesample.py
@@ -18,12 +18,11 @@ See the `QESR.py` example for information about setting your pyspecdata
 config so that this works correctly!
 """
 
-from matplotlib.pyplot import axvline, axhline, gca, title
-from pint import UnitRegistry
+from matplotlib.pyplot import title
 from pyspecdata import find_file, figlist_var
-from itertools import cycle
-from pyspecProcScripts import QESR_scalefactor, QESR, align_esr
+from pyspecProcScripts import QESR, align_esr
 from collections import OrderedDict
+
 fieldaxis = "$B_0$"
 plot_rescaled = False
 water_nddata = find_file("230511_water.DSC", exp_type="francklab_esr/romana")[
@@ -42,13 +41,14 @@ for file_searchstring, thislabel, exp_type in [
     ("250321_OHT_morepoint", "more points", "francklab_esr/Warren"),
     ("250321_OHT_lowpower", "low power", "francklab_esr/Warren"),
 ]:
-    d[thislabel] = find_file(file_searchstring,
-                             exp_type=exp_type).chunk_auto('harmonic')['harmonic',0]['phase',0]
+    d[thislabel] = find_file(file_searchstring, exp_type=exp_type).chunk_auto(
+        "harmonic"
+    )["harmonic", 0]["phase", 0]
 with figlist_var() as fl:
     fl.next("Raw and unscaled")
-    for k,v in d.items():
+    for k, v in d.items():
         fl.plot(v, label=k, alpha=0.7)
-    for k,v in d.items():
+    for k, v in d.items():
         c = QESR(
             v,
             label=k,
@@ -56,5 +56,7 @@ with figlist_var() as fl:
         )
         print(f"{c:~P}")
     d = align_esr(d, fl=fl)
-    title("This plot shows we know how to scale the amplitude\n"
-        "correctly for a range of different experimental parameters")
+    title(
+        "This plot shows we know how to scale the amplitude\n"
+        "correctly for a range of different experimental parameters"
+    )
