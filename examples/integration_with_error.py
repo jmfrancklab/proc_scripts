133274982579689227452466ed507a2772eb72c7
Alex Guinness <aguinnes@syr.edu>
2025-05-08 14:15:06 -0400

    ntegration with error not working A

--- examples/integration_with_error.py
+++ examples/integration_with_error.py
@@ -19,7 +19,6 @@ seed(
     2021
 )  # so the same random result is generated every time -- 2021 is meaningless
 logger = init_logging(level="debug")
-fl = figlist_var()
 t2 = nddata(r_[0:1:1024j], "t2")
 vd = nddata(r_[0:1:40j], "vd")
 ph1 = nddata(r_[0, 2] / 4.0, "ph1")
@@ -45,113 +44,112 @@ data.add_noise(fake_data_noise_std)
 data.reorder(["ph1", "ph2", "vd"])
 # at this point, the fake data has been generated
 data.ft(["ph1", "ph2"])
-fl.next("what does a usual error bar look like?")
-just_noise = nddata(r_[0:1:50j], "t")
-just_noise.data *= 0
-just_noise.add_noise(fake_data_noise_std)
-just_noise.set_error(fake_data_noise_std)
-fl.plot(just_noise, ".", capsize=6)
-# }}}
-data.ft("t2", shift=True)
-# TODO ☐: the goal of the following is essentially to turn the ft into a
-#         unitary FT, which this does.
-#         **But** what I was saying in slack is that I think it's better
-#         to remove this, and check that everything still works.
-#         That's because it makes more sense to take the view that we
-#         always calculate σ_ν (or σ²_ν) first, and then cnovert it to
-#         σ_t as needed.  `time_domain_noise` shows how to do this.
-#     i.e. vector-normalize the FTs above
-for thisdim in ["t2", "ph1", "ph2"]:
-    dt = data.get_ft_prop(thisdim, "dt")
-    data /= sqrt(ndshape(data)[thisdim]) * dt
-# }}}
-# {{{ First, run the code that automatically chooses integration bounds
-# and also assigns error
-s_int, returned_frq_slice = frequency_domain_integral(
-    data,
-    signal_pathway,
-    [manual_slice],
-    excluded_pathways,
-    fl=fl,
-    return_frq_slice=True,
-)
-fl.next("compare manual vs. automatic", legend=True)
-fl.plot(s_int, ".", label="fully auto: real", capsize=6)
-fl.plot(s_int.imag, ".", label="fully auto: imaginary", capsize=6)
-# }}}
-logger.debug(
-    strm("check the std after FT", std(data["ph1", 0]["ph2", 0].data.real))
-)
-# the sqrt on the next line accounts for the var(real)+var(imag)
-fl.next("raw data")
-fl.image(data, alpha=0.5)
-fl.next("real part of raw data")
-fl.image(data.real, alpha=0.5)
-fl.next("compare manual vs. automatic")
-# run a controlled comparison between manually chosen integration bounds and
-# compare against automatically generated
-# as noted in issue #44 , manually chosen bounds underperform
-for bounds, thislabel in [
-    (
-        manual_slice,
-        "manual bounds",
-    ),  # leave this as a loop so user can experiment with different bounds
-    (
-        tuple(returned_frq_slice),
-        "auto bounds",
-    ),  # leave this as a loop so user can experiment with different bounds
-]:
-    manual_bounds = data["ph1", 0]["ph2", 1]["t2":bounds]
-    assert manual_bounds.get_ft_prop("t2")
-    std_from_00 = (
-        data["ph1", 0]["ph2", 0]["t2":bounds]
-        .C.run(lambda x: abs(x) ** 2 / 2)
-        .mean_all_but(["t2", "vd"])
-        .mean("t2")
-        .run(sqrt)
+with figlist_var() as fl:
+    fl.next("what does a usual error bar look like?")
+    just_noise = nddata(r_[0:1:50j], "t")
+    just_noise.data *= 0
+    just_noise.add_noise(fake_data_noise_std)
+    just_noise.set_error(fake_data_noise_std)
+    fl.plot(just_noise, ".", capsize=6)
+    # }}}
+    data.ft("t2", shift=True)
+    # TODO ☐: the goal of the following is essentially to turn the ft into a
+    #         unitary FT, which this does.
+    #         **But** what I was saying in slack is that I think it's better
+    #         to remove this, and check that everything still works.
+    #         That's because it makes more sense to take the view that we
+    #         always calculate σ_ν (or σ²_ν) first, and then cnovert it to
+    #         σ_t as needed.  `time_domain_noise` shows how to do this.
+    #     i.e. vector-normalize the FTs above
+    #for thisdim in ["t2", "ph1", "ph2"]:
+    #    dt = data.get_ft_prop(thisdim, "dt")
+    #    data /= sqrt(ndshape(data)[thisdim]) * dt
+    # }}}
+    # {{{ First, run the code that automatically chooses integration bounds
+    # and also assigns error
+    s_int, returned_frq_slice = frequency_domain_integral(
+        data,
+        signal_pathway,
+        [manual_slice],
+        excluded_pathways,
+        fl=fl,
+        return_frq_slice=True,
     )
+    fl.next("compare manual vs. automatic", legend=True)
+    fl.plot(s_int, ".", label="fully auto: real", capsize=6)
+    fl.plot(s_int.imag, ".", label="fully auto: imaginary", capsize=6)
+    # }}}
     logger.debug(
-        strm(
-            "here is the std calculated from an off pathway",
-            std_from_00,
-            "does it match",
-            fake_data_noise_std,
-            "?",
+        strm("check the std after FT", std(data["ph1", 0]["ph2", 0].data.real))
+    )
+    # the sqrt on the next line accounts for the var(real)+var(imag)
+    fl.next("raw data")
+    fl.image(data, alpha=0.5)
+    fl.next("real part of raw data")
+    fl.image(data.real, alpha=0.5)
+    fl.next("compare manual vs. automatic")
+    # run a controlled comparison between manually chosen integration bounds and
+    # compare against automatically generated
+    # as noted in issue #44 , manually chosen bounds underperform
+    for bounds, thislabel in [
+        (
+            manual_slice,
+            "manual bounds",
+        ),  # leave this as a loop so user can experiment with different bounds
+        (
+            tuple(returned_frq_slice),
+            "auto bounds",
+        ),  # leave this as a loop so user can experiment with different bounds
+    ]:
+        manual_bounds = data["ph1", 0]["ph2", 1]["t2":bounds]
+        assert manual_bounds.get_ft_prop("t2")
+        std_from_00 = (
+            data["ph1", 0]["ph2", 0]["t2":bounds]
+            .C.run(lambda x: abs(x) ** 2 / 2)
+            .mean_all_but(["t2", "vd"])
+            .mean("t2")
+            .run(sqrt)
         )
-    )
-    N = ndshape(manual_bounds)["t2"]
-    df = manual_bounds.get_ft_prop("t2", "df")
-    logger.debug(
-        strm(ndshape(manual_bounds), "df is", df, "N is", N, "N*df is", N * df)
-    )
-    manual_bounds.integrate("t2")
-    # N terms that have variance given by fake_data_noise_std**2 each
-    # multiplied by df
-    # the 2 has to do w/ real/imag/abs -- see check_integration_error
-    propagated_variance = N * df**2 * fake_data_noise_std**2
-    propagated_variance_from_inactive = N * df**2 * std_from_00**2
-    logger.debug(
-        strm(
-            "manually calculated integral error is", sqrt(propagated_variance)
+        logger.debug(
+            strm(
+                "here is the std calculated from an off pathway",
+                std_from_00,
+                "does it match",
+                fake_data_noise_std,
+                "?",
+            )
         )
-    )
-    manual_bounds.set_error(sqrt(propagated_variance))
-    fl.plot(
-        manual_bounds,
-        ".",
-        capsize=6,
-        label="%s (programmed σ)\n$%4g\\rightarrow%4g$"
-        % ((thislabel,) + bounds),
-        alpha=0.5,
-    )
-    manual_bounds.set_error(sqrt(propagated_variance_from_inactive.data))
-    fl.plot(
-        manual_bounds,
-        ".",
-        capsize=6,
-        label="%s (inactive CT σ)\n$%4g\\rightarrow%4g$"
-        % ((thislabel,) + bounds),
-        alpha=0.5,
-    )
-# TODO ☐: change to context block
-fl.show()
+        N = ndshape(manual_bounds)["t2"]
+        df = manual_bounds.get_ft_prop("t2", "df")
+        logger.debug(
+            strm(ndshape(manual_bounds), "df is", df, "N is", N, "N*df is", N * df)
+        )
+        manual_bounds.integrate("t2")
+        # N terms that have variance given by fake_data_noise_std**2 each
+        # multiplied by df
+        # the 2 has to do w/ real/imag/abs -- see check_integration_error
+        propagated_variance = N * df**2 * fake_data_noise_std**2
+        propagated_variance_from_inactive = N * df**2 * std_from_00**2
+        logger.debug(
+            strm(
+                "manually calculated integral error is", sqrt(propagated_variance)
+            )
+        )
+        manual_bounds.set_error(sqrt(propagated_variance))
+        fl.plot(
+            manual_bounds,
+            ".",
+            capsize=6,
+            label="%s (programmed σ)\n$%4g\\rightarrow%4g$"
+            % ((thislabel,) + bounds),
+            alpha=0.5,
+        )
+        manual_bounds.set_error(sqrt(propagated_variance_from_inactive.data))
+        fl.plot(
+            manual_bounds,
+            ".",
+            capsize=6,
+            label="%s (inactive CT σ)\n$%4g\\rightarrow%4g$"
+            % ((thislabel,) + bounds),
+            alpha=0.5,
+        )
