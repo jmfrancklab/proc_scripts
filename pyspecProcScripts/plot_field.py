2716159b25261c17e45aa5d5c7d27c60e75dca3e
atahangarip <ag2639@cornell.edu>
2026-03-20 12:24:47 -0400

    finalized script

--- pyspecProcScripts/plot_field.py
+++ pyspecProcScripts/plot_field.py
@@ -48,7 +48,9 @@ def plot_field(
         thislog = logobj.from_group(f[node_name])
         log_array = np.array(thislog.total_log)
 
-    assert "field" in log_array.dtype.names, str(log_array.dtype.names)
+    assert all(name in log_array.dtype.names for name in ("time", "field")), str(
+        log_array.dtype.names
+    )
     assert s["indirect"].dtype.names == (
         "start_times",
         "stop_times",
@@ -60,9 +62,8 @@ def plot_field(
     log_vs_time = (
         psd.nddata(
             log_array["field"],  # Create an nddata object from the field data
-            [
-                -1
-            ],  # The shape of the data is 1D, so we use -1 to indicate that the length should be inferred
+            [-1],  # The shape of the data is 1D, so we use -1 to indicate that
+            # the length should be inferred
             ["time"],  # The axis is named "time"
         )
         .setaxis("time", log_array["time"])
@@ -70,25 +71,6 @@ def plot_field(
     )
     log_vs_time.set_units("G")
 
-    if fl:  # Check that fl is not None before plotting
-        fl.next("field log")
-        fl.plot(
-            log_vs_time,
-            ".",
-            human_units=False,
-        )  # Plot the field log data as points
-        ax = plt.gca()
-        # {{{ this is just matplotlib time formatting
-        ax.xaxis.set_major_formatter(
-            plt.FuncFormatter(
-                lambda x, _: (
-                    str(datetime.timedelta(seconds=x)).lstrip("0:").lstrip(":")
-                    if x > 0
-                    else "0:00"
-                )
-            )
-        )
-        # }}}
     log_vs_time.set_units("time", "s")
     mean_field_vs_time = (
         psd.ndshape([("time", len(s["indirect"]))])
@@ -97,6 +79,7 @@ def plot_field(
         .set_units("time", "s")
         .setaxis("time", np.zeros(len(s["indirect"])))
     )
+    time_midpoints = np.zeros(len(s["indirect"]))
     # {{{ we need to convert these to relative time up front, so that things
     #     don't get complicated!
     s["indirect"]["start_times"] -= zero_time
@@ -111,27 +94,57 @@ def plot_field(
         mean_field_vs_time["time", j] = log_vs_time[
             "time" : (time_start, time_stop)
         ].mean("time", std=True)
-        mean_field_vs_time["time"][j] = (time_start + time_stop) / 2
-        plt.axvspan(
-            time_start,
-            time_stop,
-            facecolor="none",
-            edgecolor="k",
-            hatch="XXXXXX",
-            alpha=0.1,
-        )
+        time_midpoints[j] = (time_start + time_stop) / 2
+        mean_field_vs_time["time"][j] = time_midpoints[j]
         mean_field_vs_time.set_units("G")
-    # {{{ Plot mean field vs. time with error bars
-    if fl:
-        fl.plot(
-            mean_field_vs_time,
-            "o",
-            human_units=False,
-        )
 
     s.setaxis("indirect", mean_field_vs_time.data).set_error(
         "indirect", mean_field_vs_time.get_error()
     ).set_units("indirect", "G")
     s.rename("indirect", "field")
+    if fl:
+        fl.next("B field and peak shift vs. time")
+        ax_field = plt.gca()
+        fl.plot(
+            log_vs_time,
+            ".",
+            human_units=False,
+            ax=ax_field,
+            label="field log",
+        )
+        ax_field.set_xlabel("time / s")
+        ax_field.set_ylabel("magnetic field / G")
+        ax_field.xaxis.set_major_formatter(
+            plt.FuncFormatter(
+                lambda x, _: (
+                    str(datetime.timedelta(seconds=x)).lstrip("0:").lstrip(":")
+                    if x > 0
+                    else "0:00"
+                )
+            )
+        )
+        if "t2" in s.dimlabels:
+            s_for_peak = s.C
+            if "ph1" in s_for_peak.dimlabels:
+                s_for_peak = s_for_peak["ph1", 1]
+            if not s_for_peak.get_ft_prop("t2"):
+                s_for_peak.ft("t2")
+            peak_shift_vs_time = s_for_peak.run(abs).argmax("t2")
+            source_axis = (
+                "field" if "field" in peak_shift_vs_time.dimlabels else "indirect"
+            )
+            peak_shift_vs_time.rename(source_axis, "time")
+            peak_shift_vs_time.setaxis("time", time_midpoints).set_units("time", "s")
+            ax_shift = ax_field.twinx()
+            fl.plot(
+                peak_shift_vs_time,
+                human_units=False,
+                ax=ax_shift,
+                color="orange",
+                label="peak shift",
+            )
+            ax_shift.set_ylabel("peak position shift / Hz")
+            ax_shift.legend(loc="upper right")
+        ax_field.legend(loc="upper left")
     # }}}
     return s
