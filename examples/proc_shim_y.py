58860a440c34d948b124ae47b355ea3bb4377515
atahangarip <ag2639@cornell.edu>
2026-03-25 13:42:58 -0400

    JF edits

--- examples/proc_shim_y.py
+++ examples/proc_shim_y.py
@@ -1,94 +1,83 @@
-from pyspecdata import find_file, DCCT
+from pyspecdata import find_file, image, DCCT, gammabar_H
+
+# from pyspecProcScripts import lookup_table
 import matplotlib.pyplot as plt
-from numpy import r_
+from numpy import r_, pi
 import matplotlib as mpl
 import numpy as np
 import pyspecProcScripts as prscr
+from pyspecProcScripts.load_data import proc_spincore_generalproc_v1
 import pyspecdata as psd
+
+plt.rcParams["figure.figsize"] = (10, 10)
 
 apply_apod = False
 center_echo = False
 slicing = True
-lims = (-10.7e3, -6.61e3)
+lims = (-7.1e3, -1.5e3)
 
-
-def my_preproc(x):
-    if center_echo:
-        x["t2"] -= x.get_prop("acq_params")["tau_us"] * 1e-6
-    x.setaxis(
-        "ph1", r_[0:4] / 4
-    )  # this appears to be set, but not correctly, which is bad
-    x.ft("ph1")
-    x.run(np.conj)  # so that offset goes up when field goes up
-    x.ft("t2", shift=True)
-    x.set_units("indirect", "G")  # this should have been done when saving!
-    x.rename("indirect", "field")
-    return x
-
-
-my_lookup = dict(prscr.lookup_table)
-
-this_expno = "shim_y_0"
-d = find_file(
-    f"260324_hydroxytempo_{this_expno}.*h5",
-    exp_type="ODNP_NMR_comp/Echoes",
-    expno=this_expno,  # when I leave out expno, it gives me an error that
-    # lists all the nodes
-    lookup=my_lookup,
-)
-nscans = d.get_prop("acq_params")["nScans"]
-if nscans > 1:
-    d = d.mean("nScans")
-d.ift("t2")
-fig = plt.figure(figsize=(6, 4))
-gs = mpl.gridspec.GridSpec(1, 2, left=0.05, right=0.95)
-d = d[
-    "t2" : (None, 6.5e-3 - center_echo * 3.5e-3)
-]  # just slice all the data since we're not filtering
-DCCT(
-    d,
-    fig=fig,
-    title=("Y current vs. Signal"),
-    bbox=gs[0, 0],
-)
-d = d["t2", 1:]  # remove first point, which has ringdown
-if apply_apod:
-    d *= np.exp(
-        -abs(d.fromaxis("t2")) / 0.5e-3
-    )  # apply a filter to clean up signal
-d.ft("t2")
-DCCT(
-    d,
-    fig=fig,
-    title=("Y current vs. Signal"),
-    interpolation="nearest",
-    bbox=gs[0, 1],
-)
-fig2 = plt.figure()
-if slicing:
-    d = d["t2":lims]
-DCCT(
-    d,
-    fig=fig2,
-    title=("Y current vs. Signal, sliced"),
-    interpolation="nearest",
-    bbox=gs[0, 0],
-)
-fig3 = plt.figure()
-d = prscr.select_pathway(d, d.get_prop("coherence_pathway"))
-d.ift("t2")
-psd.image(d, ax=fig3.add_subplot(gs[0, 0]))
-psd.plot(
-    d.C.run(lambda x: abs(x) ** 2).integrate("t2"),
-    "o-",
-    ax=fig3.add_subplot(gs[0, 1]),
-)
-d.ft("t2")
-d = abs(d)
-d.pcolor(ax=fig2.add_subplot(gs[0, 1]))
-d.run(lambda x: x**2).integrate("t2")
-fig4 = plt.figure()
-psd.plot(d, "o")
-fig4.suptitle("Energy vs. Y current")
-fig4.axes[0].set_ylabel("Energy / a.u.")
+for whichexp in [2, 3]:
+    this_expno = f"shim_y_{whichexp}"
+    d = find_file(
+        rf"260323.*{whichexp}\.",
+        exp_type="B27/Echoes",
+        expno=this_expno,  # when I leave out expno, it gives me an error that lists all the nodes
+        lookup=prscr.lookup_table,
+    )
+    beta = d.get_prop("acq_params")["beta_90_s_sqrtW"]
+    freq = d.get_prop("acq_params")["carrierFreq_MHz"]
+    ind_pts = d.get_prop("acq_params")["indirect_pts"]
+    d.ift("t2")
+    # {{{ first figure covers data selection
+    fig = plt.figure(f"{this_expno} data slicing")
+    gs = mpl.gridspec.GridSpec(1, 2, left=0.1, right=0.95, top=0.9, bottom=0.2)
+    DCCT(
+        d,
+        fig=fig,
+        title=(
+            f"{this_expno}\n β={beta} time domain \n f = {freq} ind_pts = {ind_pts}"
+        ),
+        bbox=gs[0, 0],
+    )
+    d = d["t2", 1:]  # remove first point, which has ringdown
+    if apply_apod:
+        d *= np.exp(
+            -abs(d.fromaxis("t2")) / 0.5e-3
+        )  # apply a filter to clean up signal
+    d.ft("t2")
+    DCCT(
+        d,
+        fig=fig,
+        title=(
+            f"{this_expno}\n β={beta} frequency domain \n f = {freq} ind_pts ="
+            f" {ind_pts}"
+        ),
+        interpolation="nearest",
+        bbox=gs[0, 1],
+    )
+    plt.tight_layout()
+    # }}}
+    # {{{ carry through to energy
+    fig = plt.figure(f"{this_expno} determine energy")
+    gs = mpl.gridspec.GridSpec(1, 3, left=0.1, right=0.95, top=0.9, bottom=0.15)
+    if slicing:
+        d = d["t2":lims]
+    DCCT(
+        d,
+        fig=fig,
+        title=(
+            f"{this_expno}\n β={beta} frequency domain \n f = {freq} ind_pts ="
+            f" {ind_pts}"
+        ),
+        interpolation="nearest",
+        bbox=gs[0, 0],
+    )
+    d = abs(prscr.select_pathway(d, d.get_prop("coherence_pathway")))
+    d.pcolor(ax=fig.add_subplot(gs[0, 1]))
+    d.run(lambda x: abs(x) ** 2).integrate("t2")
+    plt.tight_layout()
+    psd.plot(d, "o", ax=fig.add_subplot(gs[0, 2]))
+    plt.title(this_expno)
+    plt.tight_layout()
+    # }}}
 plt.show()
