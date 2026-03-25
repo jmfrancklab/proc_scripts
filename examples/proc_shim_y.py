19f9fef7f95e40efc29ed9e7e2b989175ea167aa
atahangarip <ag2639@cornell.edu>
2026-03-25 13:44:15 -0400

    AG adds nscans and plots new data

--- examples/proc_shim_y.py
+++ examples/proc_shim_y.py
@@ -2,11 +2,9 @@ from pyspecdata import find_file, image, DCCT, gammabar_H
 
 # from pyspecProcScripts import lookup_table
 import matplotlib.pyplot as plt
-from numpy import r_, pi
 import matplotlib as mpl
 import numpy as np
 import pyspecProcScripts as prscr
-from pyspecProcScripts.load_data import proc_spincore_generalproc_v1
 import pyspecdata as psd
 
 plt.rcParams["figure.figsize"] = (10, 10)
@@ -14,19 +12,22 @@ plt.rcParams["figure.figsize"] = (10, 10)
 apply_apod = False
 center_echo = False
 slicing = True
-lims = (-7.1e3, -1.5e3)
-
-for whichexp in [2, 3]:
+lims = [(-7.5e3, -2.37e3), (-3.33e3, 0.25e3), (-9.1e3, -6.8e3)]
+date = ["260323", "260324", "260324"]
+this_lim = 0
+for whichexp in [3, 4, 6]:
     this_expno = f"shim_y_{whichexp}"
     d = find_file(
-        rf"260323.*{whichexp}\.",
+        rf"{date[this_lim]}.*{whichexp}\.",
         exp_type="B27/Echoes",
         expno=this_expno,  # when I leave out expno, it gives me an error that lists all the nodes
         lookup=prscr.lookup_table,
     )
     beta = d.get_prop("acq_params")["beta_90_s_sqrtW"]
     freq = d.get_prop("acq_params")["carrierFreq_MHz"]
-    ind_pts = d.get_prop("acq_params")["indirect_pts"]
+    nscans = d.get_prop("acq_params")["nScans"]
+    if nscans > 1:
+        d = d.mean("nScans")
     d.ift("t2")
     # {{{ first figure covers data selection
     fig = plt.figure(f"{this_expno} data slicing")
@@ -34,9 +35,7 @@ for whichexp in [2, 3]:
     DCCT(
         d,
         fig=fig,
-        title=(
-            f"{this_expno}\n β={beta} time domain \n f = {freq} ind_pts = {ind_pts}"
-        ),
+        title=(f"{this_expno}\n β={beta} time domain \n f = {freq} nscans = {nscans}"),
         bbox=gs[0, 0],
     )
     d = d["t2", 1:]  # remove first point, which has ringdown
@@ -49,8 +48,7 @@ for whichexp in [2, 3]:
         d,
         fig=fig,
         title=(
-            f"{this_expno}\n β={beta} frequency domain \n f = {freq} ind_pts ="
-            f" {ind_pts}"
+            f"{this_expno}\n β={beta} frequency domain \n f = {freq} nscans = {nscans}"
         ),
         interpolation="nearest",
         bbox=gs[0, 1],
@@ -61,13 +59,12 @@ for whichexp in [2, 3]:
     fig = plt.figure(f"{this_expno} determine energy")
     gs = mpl.gridspec.GridSpec(1, 3, left=0.1, right=0.95, top=0.9, bottom=0.15)
     if slicing:
-        d = d["t2":lims]
+        d = d["t2" : lims[this_lim]]
     DCCT(
         d,
         fig=fig,
         title=(
-            f"{this_expno}\n β={beta} frequency domain \n f = {freq} ind_pts ="
-            f" {ind_pts}"
+            f"{this_expno}\n β={beta} frequency domain \n f = {freq} nscans = {nscans}"
         ),
         interpolation="nearest",
         bbox=gs[0, 0],
@@ -79,5 +76,6 @@ for whichexp in [2, 3]:
     psd.plot(d, "o", ax=fig.add_subplot(gs[0, 2]))
     plt.title(this_expno)
     plt.tight_layout()
+    this_lim += 1
     # }}}
 plt.show()
