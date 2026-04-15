bfd7af645378fbe3b627830b9d61397e6cdc306f
John Franck <jmfranck@jmfDebian.syr.edu>
2026-04-15 14:38:54 -0400

    stylistic tweaks and added comments -- still runs

--- examples/plot_field.py
+++ examples/plot_field.py
@@ -1,5 +1,3 @@
-# TODO ☐: not yet reviewed
-
 import pyspecdata as psd
 import pyspecProcScripts as prscr
 import matplotlib.pyplot as plt
@@ -7,21 +5,20 @@ import matplotlib.pyplot as plt
 
 # {{{ the HDF used in this particular example is broken, so we need to
 #     patch it
-def _decode_list_node(h5group):
-    item_names = sorted(
-        (name for name in h5group.attrs if name.startswith("ITEM")),
-        key=lambda name: int(name[4:]),
-    )
-    values = []
-    for name in item_names:
-        value = h5group.attrs[name]
-        if isinstance(value, bytes):
-            value = value.decode("utf-8")
-        values.append(value)
-    return values
-
-
 def fix_broken_hdf(log_group):
+    def _decode_list_node(h5group):
+        item_names = sorted(
+            (name for name in h5group.attrs if name.startswith("ITEM")),
+            key=lambda name: int(name[4:]),
+        )
+        values = []
+        for name in item_names:
+            value = h5group.attrs[name]
+            if isinstance(value, bytes):
+                value = value.decode("utf-8")
+            values.append(value)
+        return values
+
     # {{{ because this is a hack, let's just create our classes inline,
     #     to keep it simple
     array_node_cls = type(
@@ -57,9 +54,9 @@ def fix_broken_hdf(log_group):
 
 # }}}
 
-
 filename = "260406_hydroxytempo_ODNP_1.h5"
 exp_type = "B27/ODNP"
+frq_range = (-5e3, 5e3)
 # TODO: I'm not stoked about this -- can't we just search_filename, and
 #       then use the load_indiv? or, alternately (would be better for
 #       this log stuff), should we modify
@@ -67,6 +64,8 @@ exp_type = "B27/ODNP"
 #       Even better, should we modify the functions in the lookup table
 #       so that, for the right postproc types, it goes and grabs the log
 #       and attaches it as a property.
+#       Note -- this is covered in a different PR, and there we should just
+#       update this file, as well.
 log_array = prscr.load_log_data(filename, exp_type, hdf_repair=fix_broken_hdf)
 s = psd.find_file(
     filename,
@@ -74,13 +73,25 @@ s = psd.find_file(
     expno="ODNP",
     lookup=prscr.lookup_table,
 )
-zero_time = log_array["time"][0].item()
-log_array["time"] -= zero_time
-carrier_Hz = s.get_prop("acq_params")["carrierFreq_MHz"] * 1e6
-gamma_eff_Hz_G = s.get_prop("acq_params")["gamma_eff_MHz_G"] * 1e6
-field_drift_Hz = log_array["field"] * gamma_eff_Hz_G - carrier_Hz
+log_start_time = log_array["time"][0].item()
+log_array["time"] -= log_start_time
+Hall_drift_Hz = (
+    psd.nddata(
+        1e6
+        * (
+            log_array["field"] * s.get_prop("acq_params")["gamma_eff_MHz_G"]
+            - s.get_prop("acq_params")["carrierFreq_MHz"]
+        ),
+        [-1],
+        ["t"]
+    )
+    .setaxis("t", log_array["time"])
+    .set_units("t", "s")
+)
 s.rename("indirect", "t").set_units("t", "s")
-s["t"] = 0.5 * (s["t"]["start_times"] + s["t"]["stop_times"]) - zero_time
+# the time axis for the signal is originally a structured array that gives both
+# experiment start and stop times.  Convert to a normal array
+s["t"] = 0.5 * (s["t"]["start_times"] + s["t"]["stop_times"]) - log_start_time
 # {{{ rather than averaging over nScans, I create an appropriate time
 #     axis for it, and then smoosh to create a dimension with all my scans
 s["nScans"] = s["nScans"] * (
@@ -88,16 +99,11 @@ s["nScans"] = s["nScans"] * (
     + s.get_prop("acq_params")["repetition_us"] * 1e-6
 )
 s.smoosh(["t", "nScans"], dimname="t")
-# smoosh makes a record array
+# smoosh makes a structured array, which I convert to a normal array here:
 s["t"] = s["t"]["nScans"] + s["t"]["t"]
 s.set_units("t", "s")
 # }}}
 s.reorder("t2", first=False)
-field_drift_Hz = (
-    psd.nddata(field_drift_Hz, [-1], ["t"])
-    .setaxis("t", log_array["time"])
-    .set_units("t", "s")
-)
 with psd.figlist_var() as fl:
     fl.next("NMR signal - $\\varphi_0$ only")
     s /= prscr.zeroth_order_ph(s)
@@ -105,9 +111,9 @@ with psd.figlist_var() as fl:
     fl.next("slice FID")
     s = prscr.fid_from_echo(s, signal_pathway=s.get_prop("coherence_pathway"))
     s = prscr.select_pathway(s, s.get_prop("coherence_pathway"))
-    frq_range = (-5e3, 5e3)
     fl.plot(s.real)
     fl.next("NMR signal - with zf and conv (tdom)")
+    # the linewidth is about 1 kHz, so in the following, we're shooting for a matched filter
     s.ift("t2").ft("t2", pad=s.shape["t2"] * 20).convolve("t2", 1e3)
     fl.plot(s)
     fl.next("normalized (zoomed)")
@@ -120,6 +126,6 @@ with psd.figlist_var() as fl:
     s.integrate("t2")
     # }}}
     fl.next("B field and peak shift vs. time")
-    fl.plot(field_drift_Hz, ".", label="Hall probe")
+    fl.plot(Hall_drift_Hz, ".", label="Hall probe")
     fl.plot(s, "o", label="NMR")
     plt.ylim(frq_range)
